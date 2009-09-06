e09760cb281b4d830794d66df8ff3380c4e3aacb
nicove <WPCleaner@users.noreply.github.com>
2009-09-06 11:29:54 +0000

    Check Wikipedia

---
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/CheckErrorAlgorithm39.java
@@ -0,0 +1,54 @@
+/*
+ *  WikipediaCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2008  Nicolas Vervelle
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.wikipediacleaner.api.check;
+
+import java.util.ArrayList;
+
+import org.wikipediacleaner.api.data.Page;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Algorithm for analyzing error 39 of check wikipedia project.
+ * Error 39: HTML text style element &lt;p&gt;
+ */
+public class CheckErrorAlgorithm39 extends CheckErrorAlgorithmBase {
+
+  /* (non-Javadoc)
+   * @see org.wikipediacleaner.api.check.CheckErrorAlgorithm#getErrorDescription()
+   */
+  public String getErrorDescription() {
+    return GT._("HTML text style element <p>");
+  }
+
+  /* (non-Javadoc)
+   * @see org.wikipediacleaner.api.check.CheckErrorAlgorithm#analyze(org.wikipediacleaner.api.data.Page, java.lang.String, java.util.ArrayList)
+   */
+  public boolean analyze(Page page, String contents, ArrayList<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+    boolean result = false;
+    result |= simpleTextSearch(page, contents, errors, "<p>");
+    result |= simpleTextSearch(page, contents, errors, "</p>");
+    result |= simpleTextSearch(page, contents, errors, "<P>");
+    result |= simpleTextSearch(page, contents, errors, "</P>");
+    return result;
+  }
+}
