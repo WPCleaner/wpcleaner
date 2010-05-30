f10ab694db9b87d47dff3b9f378de635b27ab5c1
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2010-05-30 14:20:50 +0000

    Online configuration

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiFr.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiFr.java
@@ -61,7 +61,6 @@ class WikiFr {
     "Lien vers un homonyme",
   };
 
-  final static String[] dabList = { "Projet:Liens vers les pages d'homonymie/Export à traiter" };
   final static TemplateMatch[] dabMatches = new TemplateMatch[] {
     new TemplateMatch("A", "1", "", false, false),
     new TemplateMatch("Article détaillé", "1,2,3,4,5,6,7,8,9,10", "", false, false),
