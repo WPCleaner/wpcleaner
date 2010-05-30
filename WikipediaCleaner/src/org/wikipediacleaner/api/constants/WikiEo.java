23c73a6ece19ea98e55cc7e0bc6eea6834bd6ee5
nicove <WPCleaner@users.noreply.github.com>
2010-05-30 20:37:52 +0000

    Online configuration

--- WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiEo.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiEo.java
@@ -37,12 +37,9 @@ class WikiEo {
   final static String queryUrl = baseUrl + "query.php";
   final static String indexUrl = baseUrl + "index.php";
 
-  final static String helpUrl  = WikiEn.helpUrl;
-  final static String helpLink = WikiEn.helpLink;
-
   final static ComponentOrientation orientation = ComponentOrientation.LEFT_TO_RIGHT;
 
-  final static String message  = "";
+  final static String configuration = "Vikipediisto:NicoV/WikiCleanerConfiguration";
 
   final static String wikt     = null;
   final static TemplateMatch[] wiktMatches = new TemplateMatch[] {};
@@ -51,9 +48,5 @@ class WikiEo {
   final static String[] needHelpTemplates = null;
   final static String[] helpRequestedTemplates = null;
 
-  final static String[] dabList = null;
   final static TemplateMatch[] dabMatches = new TemplateMatch[] {};
-  
-  final static String checkWikiProject = "Projekto:Check Wikipedia";
-  final static String checkWikiTraduction = "Projekto:Check Wikipedia/Tradukado";
 }
