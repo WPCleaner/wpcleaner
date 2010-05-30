b72d2422c12b96d06e7173d41c18e9ad40414993
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2010-05-30 18:17:43 +0000

    Online configuration

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiCs.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WikiCs.java
@@ -37,12 +37,9 @@ class WikiCs {
   final static String queryUrl = baseUrl + "query.php";
   final static String indexUrl = baseUrl + "index.php";
 
-  final static String helpUrl  = WikiEn.helpUrl;
-  final static String helpLink = WikiEn.helpLink;
-
   final static ComponentOrientation orientation = ComponentOrientation.LEFT_TO_RIGHT;
 
-  final static String message  = "Opraveny odkazy na rozcestníky";
+  final static String configuration = "Wikipedista:NicoV/WikiCleanerConfiguration";
 
   final static String wikt     = null;
   final static TemplateMatch[] wiktMatches = new TemplateMatch[] {};
@@ -51,9 +48,5 @@ class WikiCs {
   final static String[] needHelpTemplates = null;
   final static String[] helpRequestedTemplates = null;
 
-  final static String[] dabList = null;
   final static TemplateMatch[] dabMatches = new TemplateMatch[] {};
-  
-  final static String checkWikiProject = "Wikipedie:WCW";
-  final static String checkWikiTraduction = "Wikipedie:WikiProjekt Check Wikipedia/Translation";
 }
