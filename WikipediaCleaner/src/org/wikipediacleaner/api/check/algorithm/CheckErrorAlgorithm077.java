7b5e97962d2121442304190b9e6143253ba296c1
nicove <WPCleaner@users.noreply.github.com>
2010-08-28 21:26:54 +0000

    CheckWiki - Error 077 Image description with partial <small>

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm077.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm077.java
@@ -18,14 +18,82 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Page;
+import org.wikipediacleaner.api.data.PageContents;
+import org.wikipediacleaner.api.data.PageElementImage;
+import org.wikipediacleaner.api.data.PageElementTag;
+
 
 /**
  * Algorithm for analyzing error 77 of check wikipedia project.
  * Error 77: Image description with partial &lt;small&gt;
  */
-public class CheckErrorAlgorithm077 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm077 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm077() {
     super("Image description with partial <small>");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      Page page, String contents,
+      List<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+
+    // Analyzing the text from the beginning
+    boolean result = false;
+    int startIndex = 0;
+    while (startIndex < contents.length()) {
+      PageElementImage image = PageContents.findNextImage(page, contents, startIndex);
+      if (image != null) {
+        startIndex = image.getEndIndex();
+        String text = image.getDescription();
+        if (text != null) {
+          PageElementTag tag = PageContents.findNextTag(page, text, "small", 0);
+          if (tag != null) {
+            boolean onlySpaces = true;
+            int tmpIndex = 0;
+            while (tmpIndex < tag.getStartTagBeginIndex()) {
+              if (!Character.isWhitespace(text.charAt(tmpIndex))) {
+                onlySpaces = false;
+              }
+              tmpIndex++;
+            }
+            tmpIndex = tag.getEndTagEndIndex() + 1;
+            while (tmpIndex < text.length()) {
+              if (!Character.isWhitespace(text.charAt(tmpIndex))) {
+                onlySpaces = false;
+              }
+              tmpIndex++;
+            }
+            if (!onlySpaces) {
+              if (errors == null) {
+                return true;
+              }
+              result = true;
+              CheckErrorResult errorResult = createCheckErrorResult(
+                  page, image.getBeginIndex(), image.getEndIndex());
+              errors.add(errorResult);
+            }
+          }
+        }
+      } else {
+        startIndex = contents.length();
+      }
+    }
+
+    return result;
   }
 }
