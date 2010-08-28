8b8f791c2fe6f1e56245978be30559f553cf411f
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2010-08-28 15:21:00 +0000

    CheckWiki - Error 076 Link with no space

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm076.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm076.java
@@ -18,14 +18,73 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Page;
+import org.wikipediacleaner.api.data.PageContents;
+import org.wikipediacleaner.api.data.PageElementInternalLink;
+import org.wikipediacleaner.i18n.GT;
+
 
 /**
  * Algorithm for analyzing error 76 of check wikipedia project.
  * Error 76: Link with no space
  */
-public class CheckErrorAlgorithm076 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm076 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm076() {
     super("Link with no space");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      Page page, String contents,
+      List<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+
+    // Analyzing the text from the beginning
+    boolean result = false;
+    int startIndex = 0;
+    while (startIndex < contents.length()) {
+      PageElementInternalLink link = PageContents.findNextInternalLink(page, contents, startIndex);
+      if (link != null) {
+        startIndex = link.getEndIndex();
+        int spaceIndex = link.getFullLink().indexOf("%20");
+        if (spaceIndex >= 0) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          StringBuilder sb = new StringBuilder();
+          sb.append("[[");
+          sb.append(link.getFullLink().replaceAll("\\%20", " "));
+          if (link.getText() != null) {
+            sb.append("|");
+            sb.append(link.getText());
+          }
+          sb.append("]]");
+          CheckErrorResult errorResult = createCheckErrorResult(
+              page, link.getBeginIndex(), link.getEndIndex());
+          errorResult.addReplacement(
+              sb.toString(),
+              GT._("Replace %20 by space character"));
+          errors.add(errorResult);
+        }
+      } else {
+        startIndex = contents.length();
+      }
+    }
+
+    return result;
   }
 }
