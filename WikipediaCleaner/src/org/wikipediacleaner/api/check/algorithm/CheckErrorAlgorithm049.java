fdca98306c0f5fc0642fa33f7012eaf5522c69dc
nicove <WPCleaner@users.noreply.github.com>
2010-08-29 15:20:25 +0000

    CheckWiki - Error 049 Headline with HTML

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm049.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm049.java
@@ -18,14 +18,45 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Page;
+
 
 /**
  * Algorithm for analyzing error 49 of check wikipedia project.
  * Error 49: Headline with HTML
  */
-public class CheckErrorAlgorithm049 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm049 extends CheckErrorAlgorithmBase {
+
+  private static final String[] titleTags = { "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9" };
 
   public CheckErrorAlgorithm049() {
     super("Headline with HTML");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      Page page, String contents,
+      List<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+
+    // Analyzing the text from the beginning
+    boolean result = false;
+    for (String tagName : titleTags) {
+      result |= addTags(result, page, contents, errors, tagName);
+    }
+
+    return result;
   }
 }
