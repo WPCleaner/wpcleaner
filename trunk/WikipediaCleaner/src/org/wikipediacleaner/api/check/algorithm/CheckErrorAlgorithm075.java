b51cd6371345d3ddd661579e84796e12e3646c3c
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2010-08-30 20:36:27 +0000

    CheckWiki - Error 075 Indented list

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm075.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm075.java
@@ -18,14 +18,72 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Page;
+
 
 /**
  * Algorithm for analyzing error 75 of check wikipedia project.
  * Error 75: Indented list
  */
-public class CheckErrorAlgorithm075 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm075 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm075() {
     super("Indented list");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      Page page, String contents,
+      List<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+
+    // Analyzing the text from the beginning
+    boolean result = false;
+    int startIndex = -1;
+    while (startIndex < contents.length()) {
+      if (startIndex < 0) {
+        startIndex = 0;
+      } else {
+        startIndex = contents.indexOf('\n', startIndex);
+        if (startIndex >= 0) {
+          startIndex++;
+        }
+      }
+      if (startIndex < 0) {
+        startIndex = contents.length();
+      } else {
+        if ((startIndex + 1 < contents.length()) &&
+            (contents.charAt(startIndex) == ':')) {
+          char nextChar = contents.charAt(startIndex + 1);
+          if ((nextChar == '-') ||
+              (nextChar == '*') ||
+              (nextChar == '#')) {
+            if (errors == null) {
+              return true;
+            }
+            result = true;
+            int nextLine = contents.indexOf('\n', startIndex);
+            if (nextLine < 0) {
+              nextLine = contents.length();
+            }
+            CheckErrorResult errorResult = createCheckErrorResult(page, startIndex, nextLine);
+            errors.add(errorResult);
+          }
+        }
+      }
+    }
+    return result;
   }
 }
