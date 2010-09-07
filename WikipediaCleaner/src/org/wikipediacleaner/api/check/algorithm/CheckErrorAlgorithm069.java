f11cd1834f9800f1ce86a78d73c43c11a76f509d
nicove <WPCleaner@users.noreply.github.com>
2010-09-07 19:47:07 +0000

    CheckWiki - Error 069 ISBN wrong syntax

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm069.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm069.java
@@ -18,14 +18,67 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Page;
+
 
 /**
  * Algorithm for analyzing error 69 of check wikipedia project.
  * Error 69: ISBN wrong syntax
  */
-public class CheckErrorAlgorithm069 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm069 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm069() {
     super("ISBN wrong syntax");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(Page page, String contents, List<CheckErrorResult> errors) {
+    if ((page == null) || (contents == null)) {
+      return false;
+    }
+
+    // Analyze contents from the beginning
+    int startIndex = 0;
+    boolean result = false;
+    while (startIndex < contents.length()) {
+      startIndex = contents.indexOf("ISBN", startIndex);
+      if (startIndex < 0) {
+        startIndex = contents.length();
+      } else {
+        // Check following characters
+        int tmpIndex = startIndex + 4;
+        if (tmpIndex < contents.length()) {
+          if ((contents.startsWith("-10", tmpIndex)) ||
+              (contents.startsWith("-13", tmpIndex))) {
+            int tmpIndex2 = startIndex - 1;
+            while ((tmpIndex2 >= 0) && (contents.charAt(tmpIndex2) == ' ')) {
+              tmpIndex2--;
+            }
+            if ((tmpIndex2 < 1) ||
+                (!contents.startsWith("{{", tmpIndex2 - 1))) {
+              if (errors == null) {
+                return true;
+              }
+              CheckErrorResult errorResult = createCheckErrorResult(
+                  page, startIndex, tmpIndex + 3);
+              errors.add(errorResult);
+              result = true;
+            }
+          }
+        }
+        startIndex = tmpIndex;
+      }
+    }
+    return result;
   }
 }
