4146b24841772d5515f2a433ebd1629812059789
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2010-09-11 14:20:11 +0000

    CheckWiki - Error 037 Title with special letters and no DEFAULTSORT

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm037.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm037.java
@@ -22,15 +22,25 @@ import java.util.List;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.SpecialCharacters;
+import org.wikipediacleaner.api.data.PageElementCategory;
 import org.wikipediacleaner.api.data.PageElementDefaultsort;
 import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageContents;
+import org.wikipediacleaner.api.data.PageElementLanguageLink;
+import org.wikipediacleaner.i18n.GT;
 
 /**
  * Algorithm for analyzing error 37 of check wikipedia project.
  * Error 37: Title with special letters and no DEFAULTSORT
  */
 public class CheckErrorAlgorithm037 extends CheckErrorAlgorithmBase {
+
+  /**
+   * Possible global fixes.
+   */
+  private final static String[] globalFixes = new String[] {
+    GT._("Add DEFAULTSORT"),
+  };
 
   public CheckErrorAlgorithm037() {
     super("Title with special letters and no DEFAULTSORT");
@@ -85,9 +95,73 @@ public class CheckErrorAlgorithm037 extends CheckErrorAlgorithmBase {
     }
 
     // Searching for Categories without a sort key
-    // TODO
+    boolean categoriesWithoutSort = false;
+    int currentIndex = 0;
+    while (currentIndex < contents.length()) {
+      PageElementCategory category = PageContents.findNextCategory(page, contents, currentIndex);
+      if (category != null) {
+        currentIndex = category.getEndIndex();
+        if ((category.getSort() == null) ||
+            (category.getSort().trim().length() == 0)) {
+          categoriesWithoutSort = true;
+        }
+      } else {
+        currentIndex = contents.length();
+      }
+    }
+    if (!categoriesWithoutSort) {
+      return false;
+    }
 
     // Reporting error
     return true;
+  }
+
+  /**
+   * @return List of possible global fixes.
+   */
+  @Override
+  public String[] getGlobalFixes() {
+    return globalFixes;
+  }
+
+  /**
+   * Fix all the errors in the page.
+   * 
+   * @param fixName Fix name (extracted from getGlobalFixes()).
+   * @param page Page.
+   * @param contents Page contents (may be different from page.getContents()).
+   * @return Page contents after fix.
+   */
+  @Override
+  public String fix(String fixName, Page page, String contents) {
+    int index = contents.length();
+    PageElementCategory category = PageContents.findNextCategory(page, contents, 0);
+    if (category != null) {
+      index = category.getBeginIndex();
+    } else {
+      PageElementLanguageLink language = PageContents.findNextLanguageLink(page, contents, 0);
+      if (language != null) {
+        index = language.getBeginIndex();
+      }
+    }
+    StringBuilder tmp = new StringBuilder();
+    if (index > 0) {
+      tmp.append(contents.substring(0, index));
+    }
+    tmp.append("{{DEFAULTSORT:");
+    for (int i = 0; i < page.getTitle().length(); i++) {
+      char character = page.getTitle().charAt(i);
+      if (SpecialCharacters.isAuthorized(character, page.getWikipedia())) {
+        tmp.append(character);
+      } else {
+        tmp.append(SpecialCharacters.proposeReplacement(character));
+      }
+    }
+    tmp.append("}}\n");
+    if (index < contents.length()) {
+      tmp.append(contents.substring(index));
+    }
+    return tmp.toString();
   }
 }
