150c8dc8eed386cc2b7367b2b2aa1a4f65bcf024
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2011-04-07 11:52:33 +0000

    Error 068: Link to other language

---
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/BasicActionProvider.java
@@ -0,0 +1,49 @@
+/*
+ *  WikipediaCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2008  Nicolas Vervelle
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.wikipediacleaner.api.check;
+
+import javax.swing.Action;
+import javax.swing.JTextPane;
+import javax.swing.text.Element;
+
+
+/**
+ * A basic ActionProvider created with an action
+ */
+public class BasicActionProvider implements ActionProvider {
+
+  private final Action action;
+
+  /**
+   * Constructor.
+   */
+  public BasicActionProvider(Action action) {
+    this.action = action;
+  }
+
+  /* (non-Javadoc)
+   * @see org.wikipediacleaner.api.check.ActionProvider#getAction(javax.swing.text.Element, javax.swing.JTextPane)
+   */
+  public Action getAction(
+      @SuppressWarnings("unused") Element element,
+      @SuppressWarnings("unused") JTextPane textPane) {
+    return action;
+  }
+
+}
