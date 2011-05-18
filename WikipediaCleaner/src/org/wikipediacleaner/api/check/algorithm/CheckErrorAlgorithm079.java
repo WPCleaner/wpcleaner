b27c833957423fd12cd5209159ad3e11ee99a2f5
nicove <WPCleaner@users.noreply.github.com>
2011-05-18 20:01:42 +0000

    Error 079 (External link without description)

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm079.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm079.java
@@ -25,6 +25,7 @@ import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.SimpleAction;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageContents;
+import org.wikipediacleaner.api.data.PageElementExternalLink;
 import org.wikipediacleaner.api.data.PageElementTagData;
 import org.wikipediacleaner.gui.swing.action.PageViewAction;
 import org.wikipediacleaner.i18n.GT;
@@ -50,93 +51,68 @@ public class CheckErrorAlgorithm079 extends CheckErrorAlgorithmBase {
   public boolean analyze(
       PageAnalysis pageAnalysis,
       Collection<CheckErrorResult> errors) {
-    boolean result = false;
-    result |= analyzeProtocol("[http://", pageAnalysis, errors);
-    result |= analyzeProtocol("[ftp://", pageAnalysis, errors);
-    result |= analyzeProtocol("[https://", pageAnalysis, errors);
-    return result;
-  }
-
-  /**
-   * Check for errors for on protocol.
-   * 
-   * @param protocol Protocol.
-   * @param pageAnalysis Page analysis.
-   * @return
-   */
-  private boolean analyzeProtocol(
-      String protocol, PageAnalysis pageAnalysis,
-      Collection<CheckErrorResult> errors) {
     if (pageAnalysis == null) {
       return false;
     }
-    int startIndex = 0;
+
     boolean result = false;
     String contents = pageAnalysis.getContents();
-    while (startIndex < contents.length()) {
-      startIndex = contents.indexOf(protocol, startIndex);
-      if (startIndex >= 0) {
-        int endIndex = contents.indexOf("]", startIndex);
-        int spaceIndex = contents.indexOf(" ", startIndex);
-        if (endIndex < 0) {
-          startIndex = contents.length();
-        } else {
-          if ((spaceIndex < 0) || (spaceIndex > endIndex)) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            CheckErrorResult errorResult = createCheckErrorResult(
-                pageAnalysis.getPage(), startIndex, endIndex + 1); 
-            boolean isInRef = false;
-            PageElementTagData previousStartRef = PageContents.findPreviousStartTag(
-                pageAnalysis.getPage(), contents, "ref", startIndex);
-            if (previousStartRef != null) {
-              PageElementTagData previousEndRef = PageContents.findPreviousEndTag(
-                  pageAnalysis.getPage(), contents, "ref", startIndex);
-              if ((previousEndRef == null) ||
-                  (previousEndRef.getEndIndex() < previousStartRef.getEndIndex())) {
-                PageElementTagData nextEndRef = PageContents.findNextEndTag(
-                    pageAnalysis.getPage(), contents, "ref", endIndex);
-                if (nextEndRef != null) {
-                  PageElementTagData nextStartRef = PageContents.findNextStartTag(
-                      pageAnalysis.getPage(), contents, "ref", startIndex);
-                  if ((nextStartRef == null) ||
-                      (nextEndRef.getBeginIndex() < nextStartRef.getBeginIndex())) {
-                    isInRef = true;
-                  }
-                }
+    Collection<PageElementExternalLink> links = pageAnalysis.getExternalLinks();
+    for (PageElementExternalLink link : links) {
+      String text = link.getText();
+      if ((text == null) || (text.trim().length() == 0)) {
+        if (errors == null) {
+          return true;
+        }
+        result = true;
+        int beginIndex = link.getBeginIndex();
+        int endIndex = link.getEndIndex();
+        CheckErrorResult errorResult = createCheckErrorResult(
+            pageAnalysis.getPage(), beginIndex, endIndex); 
+        boolean isInRef = false;
+        PageElementTagData previousStartRef = PageContents.findPreviousStartTag(
+            pageAnalysis.getPage(), contents, "ref", beginIndex);
+        if (previousStartRef != null) {
+          PageElementTagData previousEndRef = PageContents.findPreviousEndTag(
+              pageAnalysis.getPage(), contents, "ref", beginIndex);
+          if ((previousEndRef == null) ||
+              (previousEndRef.getEndIndex() < previousStartRef.getEndIndex())) {
+            PageElementTagData nextEndRef = PageContents.findNextEndTag(
+                pageAnalysis.getPage(), contents, "ref", endIndex);
+            if (nextEndRef != null) {
+              PageElementTagData nextStartRef = PageContents.findNextStartTag(
+                  pageAnalysis.getPage(), contents, "ref", beginIndex);
+              if ((nextStartRef == null) ||
+                  (nextEndRef.getBeginIndex() < nextStartRef.getBeginIndex())) {
+                isInRef = true;
               }
             }
-            String url = contents.substring(startIndex + 1, endIndex).trim();
-            errorResult.addPossibleAction(
-                GT._("Add a description..."),
-                new AddTextActionProvider(
-                    "[" + url + " ", "]", url,
-                    GT._("What description would like to use for the external link ?"),
-                    "[]"));
-            if (!isInRef) {
-              errorResult.addReplacement(
-                  "<ref>" + url + "</ref>",
-                  GT._("Convert into <ref> tag"));
-              errorResult.addPossibleAction(
-                  GT._("Add a description and convert into <ref> tag"),
-                  new AddTextActionProvider(
-                      "<ref>[" + url + " ", "]</ref>", url,
-                      GT._("What description would like to use for the external link ?"),
-                      "[]"));
-            } else {
-              errorResult.addReplacement(url);
-            }
-            errorResult.addPossibleAction(new SimpleAction(GT._("External viewer"), new PageViewAction(url)));
-            errors.add(errorResult);
-            startIndex = endIndex + 1;
-          } else {
-            startIndex = endIndex + 1;
           }
         }
-      } else {
-        startIndex = contents.length();
+        String url = link.getLink();
+        errorResult.addPossibleAction(
+            GT._("Add a description..."),
+            new AddTextActionProvider(
+                "[" + url + " ", "]", url,
+                GT._("What description would like to use for the external link ?"),
+                "[]"));
+        if (!isInRef) {
+          errorResult.addReplacement(
+              "<ref>" + url + "</ref>",
+              GT._("Convert into <ref> tag"));
+          errorResult.addPossibleAction(
+              GT._("Add a description and convert into <ref> tag"),
+              new AddTextActionProvider(
+                  "<ref>[" + url + " ", "]</ref>", url,
+                  GT._("What description would like to use for the external link ?"),
+                  "[]"));
+        } else {
+          errorResult.addReplacement(url);
+        }
+        errorResult.addPossibleAction(
+            new SimpleAction(GT._("External viewer"),
+                new PageViewAction(url)));
+        errors.add(errorResult);
       }
     }
     return result;
