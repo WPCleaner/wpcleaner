9e6b30dd1fc6f3cb24c37ca7fe63fc2d5efb1a11
nicove <WPCleaner@users.noreply.github.com>
2012-03-23 20:11:13 +0000

    Error n°62: Headline alone

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm062.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm062.java
@@ -18,14 +18,75 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementTitle;
+
 
 /**
  * Algorithm for analyzing error 62 of check wikipedia project.
  * Error 62: Headline alone
  */
-public class CheckErrorAlgorithm062 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm062 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm062() {
     super("Headline alone");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param pageAnalysis Page analysis.
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis pageAnalysis,
+      Collection<CheckErrorResult> errors) {
+    if (pageAnalysis == null) {
+      return false;
+    }
+
+    // Retrieving titles
+    boolean result = false;
+    List<PageElementTitle> titles = pageAnalysis.getTitles();
+    if (titles == null) {
+      return false;
+    }
+    if (titles.size() < 5) {
+      return false;
+    }
+
+    // Analyzing titles
+    int previousLevel = 0;
+    for (int i = 0; i < titles.size(); i++) {
+      PageElementTitle title = titles.get(i);
+      if ((title.getFirstLevel() > previousLevel) &&
+          (title.getFirstLevel() > 2)) {
+        int j = i + 1;
+        int count = 0;
+        while ((j < titles.size()) &&
+               (titles.get(j).getFirstLevel() >= title.getFirstLevel())) {
+          if (titles.get(j).getFirstLevel() == title.getFirstLevel()) {
+            count++;
+          }
+          j++;
+        }
+        if (count == 0) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              pageAnalysis.getPage(), title.getBeginIndex(), title.getEndIndex());
+          errors.add(errorResult);
+        }
+      }
+      previousLevel = title.getFirstLevel();
+    }
+    return result;
   }
 }
