b8d3a463b52ce3a6d8bbe471bce2abba742d0e35
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2012-12-02 16:29:43 +0000

    Error n°509: Internal link could be compacted

---
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm509.java
@@ -0,0 +1,122 @@
+/*
+ *  WikipediaCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2008  Nicolas Vervelle
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementInternalLink;
+import org.wikipediacleaner.gui.swing.component.MWPane;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Algorithm for analyzing error 509 of check wikipedia project.
+ * Error 509: Internal link could be compacted
+ */
+public class CheckErrorAlgorithm509 extends CheckErrorAlgorithmBase {
+
+  /**
+   * Possible global fixes.
+   */
+  private final static String[] globalFixes = new String[] {
+    GT._("Compact links"),
+  };
+
+  public CheckErrorAlgorithm509() {
+    super("Internal link could be compacted");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param pageAnalysis Page analysis.
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis pageAnalysis,
+      Collection<CheckErrorResult> errors) {
+    if ((pageAnalysis == null) || (pageAnalysis.getInternalLinks() == null)) {
+      return false;
+    }
+
+    // Analyze each internal link
+    boolean result = false;
+    List<PageElementInternalLink> links = pageAnalysis.getInternalLinks();
+    String contents = pageAnalysis.getContents();
+    int maxLength = contents.length();
+    for (PageElementInternalLink link : links) {
+      if (link.getText() != null) {
+        int endIndex = link.getEndIndex();
+        while ((endIndex < maxLength) && (Character.isLetter(contents.charAt(endIndex)))) {
+          endIndex++;
+        }
+        if (endIndex > link.getEndIndex()) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              pageAnalysis.getPage(), link.getBeginIndex(), endIndex);
+          String replacement = PageElementInternalLink.createInternalLink(
+              link.getFullLink(),
+              link.getDisplayedText() + contents.substring(link.getEndIndex(), endIndex));
+          errorResult.addReplacement(replacement, GT._("Compact link"));
+          errors.add(errorResult);
+        }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Automatic fixing of all the errors in the page.
+   * 
+   * @param analysis Page analysis.
+   * @return Page contents after fix.
+   */
+  @Override
+  public String automaticFix(PageAnalysis analysis) {
+    return fix(globalFixes[0], analysis, null);
+  }
+
+  /**
+   * @return List of possible global fixes.
+   */
+  @Override
+  public String[] getGlobalFixes() {
+    return globalFixes;
+  }
+
+  /**
+   * Fix all the errors in the page.
+   * 
+   * @param fixName Fix name (extracted from getGlobalFixes()).
+   * @param analysis Page analysis.
+   * @param textPane Text pane.
+   * @return Page contents after fix.
+   */
+  @Override
+  public String fix(String fixName, PageAnalysis analysis, MWPane textPane) {
+    return fixUsingFirstReplacement(fixName, analysis);
+  }
+}
