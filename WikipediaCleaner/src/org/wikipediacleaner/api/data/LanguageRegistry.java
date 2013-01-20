063e4bac8229e16e015b88344ea2f7e74239b4fb
nicove <WPCleaner@users.noreply.github.com>
2013-01-20 19:39:22 +0000

    Mark text as being in a foreign language

--- WikipediaCleaner/src/org/wikipediacleaner/api/data/LanguageRegistry.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/data/LanguageRegistry.java
@@ -48,6 +48,21 @@ public class LanguageRegistry {
   private static List<LanguageRegistry.Language> languages;
 
   /**
+   * List of scripts.
+   */
+  private static List<LanguageRegistry.Script> scripts;
+
+  /**
+   * List of regions.
+   */
+  private static List<LanguageRegistry.Region> regions;
+
+  /**
+   * List of variants.
+   */
+  private static List<LanguageRegistry.Variant> variants;
+
+  /**
    * Constructor.
    */
   public LanguageRegistry() {
@@ -64,7 +79,28 @@ public class LanguageRegistry {
   public List<LanguageRegistry.Language> getLanguages() {
     return languages;
   }
-  
+
+  /**
+   * @return List of all scripts.
+   */
+  public List<LanguageRegistry.Script> getScripts() {
+    return scripts;
+  }
+
+  /**
+   * @return List of all regions.
+   */
+  public List<LanguageRegistry.Region> getRegions() {
+    return regions;
+  }
+
+  /**
+   * @return List of all variants.
+   */
+  public List<LanguageRegistry.Variant> getVariants() {
+    return variants;
+  }
+
   /**
    * Initialize registry.
    * @throws IOException 
@@ -82,6 +118,9 @@ public class LanguageRegistry {
             new GZIPInputStream(url.openStream())));
 
         List<LanguageRegistry.Language> tmpLanguages = new ArrayList<LanguageRegistry.Language>();
+        List<LanguageRegistry.Script> tmpScripts = new ArrayList<LanguageRegistry.Script>();
+        List<LanguageRegistry.Region> tmpRegions = new ArrayList<LanguageRegistry.Region>();
+        List<LanguageRegistry.Variant> tmpVariants = new ArrayList<LanguageRegistry.Variant>();
         String line = null;
         while ((line = reader.readLine()) != null) {
           List<String> lines = new ArrayList<String>();
@@ -91,20 +130,24 @@ public class LanguageRegistry {
           }
           if ("Type".equals(getElementName(0, lines))) {
             String type = getElementValue(0, lines);
-            if ("language".equals(type)) {
-              if ("Subtag".equals(getElementName(1, lines))) {
+            if ("language".equalsIgnoreCase(type)) {
+              if ("Subtag".equalsIgnoreCase(getElementName(1, lines))) {
                 Language language = new Language(getElementValue(1, lines));
                 boolean shouldKeep = true;
                 for (int lineNum = 2; lineNum < lines.size(); lineNum++) {
                   String name = getElementName(lineNum, lines);
                   String value = getElementValue(lineNum, lines);
-                  if ("Description".equals(name)) {
+                  if ("Description".equalsIgnoreCase(name)) {
                     language.addDescription(value);
-                  } else if ("Scope".equals(name)) {
-                    if ("special".equals(value)) {
+                  } else if ("Comments".equalsIgnoreCase(name)) {
+                    language.addComments(value);
+                  } else if ("Suppress-Script".equalsIgnoreCase(name)) {
+                    language.setSuppressScript(value);
+                  } else if ("Scope".equalsIgnoreCase(name)) {
+                    if ("special".equalsIgnoreCase(value)) {
                       shouldKeep = false;
                     }
-                  } else if ("Deprecated".equals(name)) {
+                  } else if ("Deprecated".equalsIgnoreCase(name)) {
                     shouldKeep = false;
                   }
                 }
@@ -112,11 +155,74 @@ public class LanguageRegistry {
                   tmpLanguages.add(language);
                 }
               }
+            } else if ("script".equalsIgnoreCase(type)) {
+              if ("Subtag".equalsIgnoreCase(getElementName(1, lines))) {
+                Script script = new Script(getElementValue(1, lines));
+                boolean shouldKeep = true;
+                for (int lineNum = 2; lineNum < lines.size(); lineNum++) {
+                  String name = getElementName(lineNum, lines);
+                  String value = getElementValue(lineNum, lines);
+                  if ("Description".equalsIgnoreCase(name)) {
+                    script.addDescription(value);
+                  } else if("Comments".equalsIgnoreCase(name)) {
+                    script.addComments(value);
+                  }
+                }
+                if (shouldKeep) {
+                  tmpScripts.add(script);
+                }
+              }
+            } else if ("region".equalsIgnoreCase(type)) {
+              if ("Subtag".equalsIgnoreCase(getElementName(1, lines))) {
+                Region region = new Region(getElementValue(1, lines));
+                boolean shouldKeep = true;
+                for (int lineNum = 2; lineNum < lines.size(); lineNum++) {
+                  String name = getElementName(lineNum, lines);
+                  String value = getElementValue(lineNum, lines);
+                  if ("Description".equalsIgnoreCase(name)) {
+                    region.addDescription(value);
+                  } else if ("Comments".equalsIgnoreCase(name)) {
+                    region.addComments(value);
+                  } else if ("Deprecated".equalsIgnoreCase(name)) {
+                    shouldKeep = false;
+                  }
+                }
+                if (shouldKeep) {
+                  tmpRegions.add(region);
+                }
+              }
+            } else if ("variant".equalsIgnoreCase(type)) {
+              if ("Subtag".equalsIgnoreCase(getElementName(1, lines))) {
+                Variant variant = new Variant(getElementValue(1, lines));
+                boolean shouldKeep = true;
+                for (int lineNum = 2; lineNum < lines.size(); lineNum++) {
+                  String name = getElementName(lineNum, lines);
+                  String value = getElementValue(lineNum, lines);
+                  if ("Description".equalsIgnoreCase(name)) {
+                    variant.addDescription(value);
+                  } else if ("Comments".equalsIgnoreCase(name)) {
+                    variant.addComments(value);
+                  } else if ("Prefix".equalsIgnoreCase(name)) {
+                    variant.addPrefix(value);
+                  } else if ("Deprecated".equalsIgnoreCase(name)) {
+                    shouldKeep = false;
+                  }
+                }
+                if (shouldKeep) {
+                  tmpVariants.add(variant);
+                }
+              }
             }
           }
         }
         Collections.sort(tmpLanguages);
         languages = Collections.unmodifiableList(tmpLanguages);
+        Collections.sort(tmpScripts);
+        scripts = Collections.unmodifiableList(tmpScripts);
+        Collections.sort(tmpRegions);
+        regions = Collections.unmodifiableList(tmpRegions);
+        Collections.sort(tmpVariants);
+        variants = Collections.unmodifiableList(tmpVariants);
       }
     }
   }
@@ -159,6 +265,11 @@ public class LanguageRegistry {
       return null;
     }
     String value = line.substring(colonIndex + 1).trim();
+    while ((lineNum + 1 < lines.size()) &&
+           (lines.get(lineNum + 1).startsWith(" "))) {
+      lineNum++;
+      value += " " + lines.get(lineNum).trim();
+    }
     // TODO: Manage values on several lines
     return value;
   }
@@ -177,6 +288,16 @@ public class LanguageRegistry {
      * Description.
      */
     private String description;
+
+    /**
+     * Comments.
+     */
+    private String comments;
+
+    /**
+     * Implicit script, non necessary.
+     */
+    private String suppressScript;
 
     /**
      * @param code Language code.
@@ -211,6 +332,38 @@ public class LanguageRegistry {
     }
 
     /**
+     * @param commentary Comments
+     */
+    void addComments(String commentary) {
+      if (this.comments == null) {
+        this.comments = commentary;
+      } else {
+        this.comments += ", " + commentary;
+      }
+    }
+
+    /**
+     * @return Comments
+     */
+    public String getComments() {
+      return comments;
+    }
+
+    /**
+     * @param script Implicit script.
+     */
+    void setSuppressScript(String script) {
+      this.suppressScript = script;
+    }
+
+    /**
+     * @return Implicit script.
+     */
+    String getSuppressScript() {
+      return this.suppressScript;
+    }
+
+    /**
      * @param o Other language.
      * @return  a negative integer, zero, or a positive integer as the language code
      *    is less than, equal to, or greater than the code of the specified language.
@@ -221,7 +374,300 @@ public class LanguageRegistry {
     }
 
     /**
-     * @return String representation of the object.
+     * @return String representation of the language.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+      if (description != null) {
+        return code + " - " + description;
+      }
+      return code;
+    }
+  }
+
+  /**
+   * Bean for holding information about a language.
+   */
+  public static class Script implements Comparable<Script> {
+
+    /**
+     * Script code.
+     */
+    private final String code;
+
+    /**
+     * Description.
+     */
+    private String description;
+
+    /**
+     * Comments.
+     */
+    private String comments;
+
+    /**
+     * @param code Script code.
+     */
+    Script(String code) {
+      this.code = code;
+    }
+
+    /**
+     * @return Script code.
+     */
+    public String getCode() {
+      return code;
+    }
+
+    /**
+     * @param desc Description.
+     */
+    void addDescription(String desc) {
+      if (this.description == null) {
+        this.description = desc;
+      } else {
+        this.description += ", " + desc;
+      }
+    }
+
+    /**
+     * @return Description.
+     */
+    public String getDescription() {
+      return description;
+    }
+
+    /**
+     * @param commentary Comments.
+     * @return
+     */
+    void addComments(String commentary) {
+      if (this.comments == null) {
+        this.comments = commentary;
+      } else {
+        this.comments += ", " + commentary;
+      }
+    }
+
+    /**
+     * @return Comments.
+     */
+    public String getComments() {
+      return comments;
+    }
+    /**
+     * @param o Other script.
+     * @return  a negative integer, zero, or a positive integer as the script code
+     *    is less than, equal to, or greater than the code of the specified script.
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(Script o) {
+      return code.compareTo(o.code);
+    }
+
+    /**
+     * @return String representation of the script.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+      if (description != null) {
+        return code + " - " + description;
+      }
+      return code;
+    }
+  }
+
+  /**
+   * Bean for holding information about a region.
+   */
+  public static class Region implements Comparable<Region> {
+
+    /**
+     * Region code.
+     */
+    private final String code;
+
+    /**
+     * Description.
+     */
+    private String description;
+
+    /**
+     * Comments.
+     */
+    private String comments;
+
+    /**
+     * @param code Region code.
+     */
+    Region(String code) {
+      this.code = code;
+    }
+
+    /**
+     * @return Region code.
+     */
+    public String getCode() {
+      return code;
+    }
+
+    /**
+     * @param desc Description.
+     */
+    void addDescription(String desc) {
+      if (this.description == null) {
+        this.description = desc;
+      } else {
+        this.description += ", " + desc;
+      }
+    }
+
+    /**
+     * @return Description.
+     */
+    public String getDescription() {
+      return description;
+    }
+
+    /**
+     * @param commentary Comments.
+     */
+    void addComments(String commentary) {
+      if (this.comments == null) {
+        this.comments = commentary;
+      } else {
+        this.comments += ", " + commentary;
+      }
+    }
+
+    /**
+     * @return Comments.
+     */
+    public String getComments() {
+      return comments;
+    }
+
+    /**
+     * @param o Other region.
+     * @return  a negative integer, zero, or a positive integer as the region code
+     *    is less than, equal to, or greater than the code of the specified region.
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(Region o) {
+      return code.compareTo(o.code);
+    }
+
+    /**
+     * @return String representation of the region.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+      if (description != null) {
+        return code + " - " + description;
+      }
+      return code;
+    }
+  }
+
+  /**
+   * Bean for holding information about a variant.
+   */
+  public static class Variant implements Comparable<Variant> {
+
+    /**
+     * Variant code.
+     */
+    private final String code;
+
+    /**
+     * List of possible prefixes.
+     */
+    private final List<String> prefixes;
+
+    /**
+     * Description.
+     */
+    private String description;
+
+    /**
+     * Comments.
+     */
+    private String comments;
+
+    /**
+     * @param code Variant code.
+     */
+    Variant(String code) {
+      this.code = code;
+      this.prefixes = new ArrayList<String>();
+    }
+
+    /**
+     * @return Variant code.
+     */
+    public String getCode() {
+      return code;
+    }
+
+    /**
+     * @param prefix Prefix.
+     */
+    void addPrefix(String prefix) {
+      if (!prefixes.contains(prefix)) {
+        prefixes.add(prefix);
+      }
+    }
+    /**
+     * @param desc Description.
+     */
+    void addDescription(String desc) {
+      if (this.description == null) {
+        this.description = desc;
+      } else {
+        this.description += ", " + desc;
+      }
+    }
+
+    /**
+     * @return Description.
+     */
+    public String getDescription() {
+      return description;
+    }
+
+    /**
+     * @param commentary Comments.
+     */
+    void addComments(String commentary) {
+      if (this.comments == null) {
+        this.comments = commentary;
+      } else {
+        this.comments += ", " + commentary;
+      }
+    }
+
+    /**
+     * @return Comments.
+     */
+    public String getComments() {
+      return comments;
+    }
+
+    /**
+     * @param o Other variant.
+     * @return  a negative integer, zero, or a positive integer as the variant code
+     *    is less than, equal to, or greater than the code of the specified variant.
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    public int compareTo(Variant o) {
+      return code.compareTo(o.code);
+    }
+
+    /**
+     * @return String representation of the variant.
      * @see java.lang.Object#toString()
      */
     @Override
