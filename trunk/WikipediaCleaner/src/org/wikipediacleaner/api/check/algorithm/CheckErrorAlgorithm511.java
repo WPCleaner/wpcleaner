295381eab85cee124111f18304b296d48364106b
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2013-04-15 18:57:48 +0000

    Error n°511: Internal link written as external link

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm511.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm511.java
@@ -27,6 +27,8 @@ import org.wikipediacleaner.api.constants.WikiConfiguration;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementExternalLink;
 import org.wikipediacleaner.api.data.PageElementInternalLink;
+import org.wikipediacleaner.gui.swing.component.MWPane;
+import org.wikipediacleaner.i18n.GT;
 
 
 /**
@@ -34,6 +36,13 @@ import org.wikipediacleaner.api.data.PageElementInternalLink;
  * Error 511: Internal link written as external link
  */
 public class CheckErrorAlgorithm511 extends CheckErrorAlgorithmBase {
+
+  /**
+   * Possible global fixes.
+   */
+  private final static String[] globalFixes = new String[] {
+    GT._("Convert them to internal links"),
+  };
 
   public CheckErrorAlgorithm511() {
     super("Internal link written as external link");
@@ -84,5 +93,37 @@ public class CheckErrorAlgorithm511 extends CheckErrorAlgorithmBase {
     }
 
     return result;
+  }
+
+  /**
+   * Bot fixing of all the errors in the page.
+   * 
+   * @param analysis Page analysis.
+   * @return Page contents after fix.
+   */
+  @Override
+  public String botFix(PageAnalysis analysis) {
+    return fix(globalFixes[0], analysis, null);
+  }
+
+  /**
+   * @return List of possible global fixes.
+   */
+  @Override
+  public String[] getGlobalFixes() {
+    return globalFixes;
+  }
+
+  /**
+   * Fix all the errors in the page.
+   * 
+   * @param fixName Fix name (extracted from getGlobalFixes()).
+   * @param analysis Page analysis.
+   * @param textPane Text pane.
+   * @return Page contents after fix.
+   */
+  @Override
+  public String fix(String fixName, PageAnalysis analysis, MWPane textPane) {
+    return fixUsingFirstReplacement(fixName, analysis);
   }
 }
