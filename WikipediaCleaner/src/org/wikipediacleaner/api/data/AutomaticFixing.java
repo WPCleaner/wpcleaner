3f6d59f553b0c5a8330275f341a8231b33b01775
nicove <WPCleaner@users.noreply.github.com>
2013-05-03 19:17:30 +0000

    Automatic fixing

--- WikipediaCleaner/src/org/wikipediacleaner/api/data/AutomaticFixing.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/data/AutomaticFixing.java
@@ -18,6 +18,9 @@
 
 package org.wikipediacleaner.api.data;
 
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 
 /**
  * Utility class to memorize automatic fixing parameters
@@ -39,38 +42,95 @@ public class AutomaticFixing implements Comparable<AutomaticFixing> {
    */
   private boolean regex;
 
+  /**
+   * Default constructor.
+   */
   public AutomaticFixing() {
     //
   }
 
+  /**
+   * Constructor.
+   * 
+   * @param from Original text.
+   * @param to Replacement text.
+   * @param regex Regular expression or not.
+   */
   public AutomaticFixing(String from, String to, boolean regex) {
     originalText = from;
     replacementText = to;
     this.regex = regex;
   }
 
+  /**
+   * @return Original text.
+   */
   public String getOriginalText() {
     return originalText;
   }
 
+  /**
+   * @param text Original text.
+   */
   public void setOriginalText(String text) {
     originalText = text;
   }
 
+  /**
+   * @return Replacement text.
+   */
   public String getReplacementText() {
     return replacementText;
   }
 
+  /**
+   * @param text Replacement text.
+   */
   public void setReplacementText(String text) {
     replacementText = text;
   }
 
-  public boolean isRegex() {
-    return regex;
+  /**
+   * @return Regular expression or not.
+   */
+  public Boolean getRegex() {
+    return Boolean.valueOf(regex);
   }
 
-  public void setRegex(boolean flag) {
-    regex = flag;
+  /**
+   * @param flag Regular expression or not.
+   */
+  public void setRegex(Boolean flag) {
+    regex = Boolean.TRUE.equals(flag);
+  }
+
+  /**
+   * Apply replacement to text.
+   * 
+   * @param text Text.
+   * @return Text with replacement applied.
+   */
+  public String apply(String text) {
+    if (text == null) {
+      return null;
+    }
+
+    try {
+      // Regular expression
+      if (regex) {
+        return text.replaceAll(originalText, replacementText);
+      }
+  
+      // Normal text
+      String to = replacementText;
+      if (to.indexOf('$') >= 0) {
+        to = to.replaceAll(Pattern.quote("$"), "\\\\\\$");
+      }
+      return text.replaceAll(Pattern.quote(originalText), to);
+    } catch (PatternSyntaxException e) {
+      System.err.println("Error with " + toString() + ": " + e.getMessage());
+      return text;
+    }
   }
 
   /**
@@ -95,6 +155,6 @@ public class AutomaticFixing implements Comparable<AutomaticFixing> {
    */
   @Override
   public String toString() {
-    return "[" + originalText + "] → [" + replacementText + "]";
+    return originalText + " → " + replacementText;
   }
 }
