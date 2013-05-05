d44d86d1956152f1801c14cdda8ff98cd5640576
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2013-05-05 18:41:30 +0000

    Error n°516: <br /> tags in main namespace

---
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm516.java
@@ -0,0 +1,74 @@
+/*
+ *  WikipediaCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2008  Nicolas Vervelle
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.Namespace;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementTag;
+
+
+/**
+ * Algorithm for analyzing error 516 of check wikipedia project.
+ * Error 516: &lt;br /&gt; tags in main namespace
+ */
+public class CheckErrorAlgorithm516 extends CheckErrorAlgorithmBase {
+
+  public CheckErrorAlgorithm516() {
+    super("<br /> tags in main namespace");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors) {
+    if ((analysis == null) || (analysis.getPage() == null)) {
+      return false;
+    }
+    Integer ns = analysis.getPage().getNamespace();
+    if ((ns == null) || (ns.intValue() != Namespace.MAIN)) {
+      return false;
+    }
+
+    // Check each tag
+    List<PageElementTag> brTags = analysis.getTags(PageElementTag.TAG_HTML_BR);
+    if ((brTags == null) || (brTags.isEmpty())) {
+      return false;
+    }
+    if (errors == null) {
+      return true;
+    }
+    for (PageElementTag brTag : brTags) {
+      CheckErrorResult errorResult = createCheckErrorResult(
+          analysis.getPage(), brTag.getBeginIndex(), brTag.getEndIndex());
+      errors.add(errorResult);
+    }
+
+    return true;
+  }
+}
