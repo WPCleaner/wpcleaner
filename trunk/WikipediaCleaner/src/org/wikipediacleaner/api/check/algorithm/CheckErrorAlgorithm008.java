ab87857022b65f1c529b2a17f4eb4e11bea8387d
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2013-05-15 16:29:42 +0000

    Error n°8: Headline should end with "="

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm008.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm008.java
@@ -67,7 +67,8 @@ public class CheckErrorAlgorithm008 extends CheckErrorAlgorithmBase {
           if ((analysis.getSurroundingTag(PageElementTag.TAG_WIKI_CODE, currentIndex) != null) ||
               (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_MATH, currentIndex) != null) ||
               (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_NOWIKI, currentIndex) != null) ||
-              (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_SOURCE, currentIndex) != null)) {
+              (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_SOURCE, currentIndex) != null) ||
+              (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_PRE, currentIndex) != null)) {
             errorFound = false;
           }
         }
