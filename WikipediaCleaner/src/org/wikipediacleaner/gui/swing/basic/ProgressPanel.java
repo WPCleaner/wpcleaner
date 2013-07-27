a90a42e3511d4a2426cd6d2db3972a847e81f127
nicove <WPCleaner@users.noreply.github.com>
2013-07-27 14:01:59 +0000

    License

--- WikipediaCleaner/src/org/wikipediacleaner/gui/swing/basic/ProgressPanel.java
+++ WikipediaCleaner/src/org/wikipediacleaner/gui/swing/basic/ProgressPanel.java
@@ -1,23 +1,8 @@
 /*
- *  WikipediaCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
  *  Copyright (C) 2013  Nicolas Vervelle
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
- *  NOTE: Part of this code is coming from LockingGlassPane,
- *  a class written by Alexander Potochkin,
- *  see http://weblogs.java.net/blog/alexfromsun/archive/2008/01/disabling_swing.html
+ *  See README.txt file for licensing information.
  */
 
 package org.wikipediacleaner.gui.swing.basic;
