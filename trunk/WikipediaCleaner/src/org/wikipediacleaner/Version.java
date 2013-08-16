d968d2cfe0813f8291f2700cc5cd526a262df9ed
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2013-08-16 18:46:29 +0000

    Version

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/Version.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/Version.java
@@ -19,8 +19,8 @@ import org.wikipediacleaner.i18n.GT;
  */
 public final class Version {
 
-  public final static String VERSION = "1.29β";
-  public final static Date   DATE = new GregorianCalendar(2013, Calendar.AUGUST, 14).getTime();
+  public final static String VERSION = "1.29";
+  public final static Date   DATE = new GregorianCalendar(2013, Calendar.AUGUST, 16).getTime();
 
   public final static String PROGRAM = "WPCleaner";
 
