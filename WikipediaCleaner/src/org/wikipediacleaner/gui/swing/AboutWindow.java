a2a351c97b0b84320a7d51eddd9f9df676eb48c8
nicove <WPCleaner@users.noreply.github.com>
2013-08-18 21:25:58 +0000

    About

--- WikipediaCleaner/src/org/wikipediacleaner/gui/swing/AboutWindow.java
+++ WikipediaCleaner/src/org/wikipediacleaner/gui/swing/AboutWindow.java
@@ -7,7 +7,9 @@
 
 package org.wikipediacleaner.gui.swing;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -17,11 +19,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 
 import org.wikipediacleaner.Version;
@@ -145,12 +151,43 @@ public class AboutWindow extends BasicWindow {
   }
 
   /**
-   * @return Wikipedia Cleaner tab.
+   * Add a file contents to a panel.
+   * 
+   * @param panel Panel Panel in which file contents is added.
+   * @param constraints Constraints for adding the file contents.
+   * @param fileName File name.
    */
-  private Component createWPCleanerTab() {
-    JPanel panel = new JPanel();
+  private void addFile(
+      JPanel panel, GridBagConstraints constraints,
+      String fileName) {
+    String contents = loadFile(fileName);
+    addPresentation(panel, constraints, contents);
+  }
 
-    // Initialize constraints
+  /**
+   * Add a presentation to a panel.
+   * 
+   * @param panel Panel Panel in which presentation is added.
+   * @param constraints Constraints for adding the presentation.
+   * @param text Presentation text.
+   */
+  private void addPresentation(
+      JPanel panel, GridBagConstraints constraints,
+      String text) {
+    if ((text != null) && (!"".equals(text.trim()))) {
+      JLabel label = new JLabel(text);
+      label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+      panel.add(label, constraints);
+      constraints.gridy++;
+    }
+  }
+
+  /**
+   * Initialize GridBagConstraints for a panel.
+   * 
+   * @return GridBagConstraints.
+   */
+  private GridBagConstraints initializeGridBagConstraints() {
     GridBagConstraints constraints = new GridBagConstraints();
     constraints.fill = GridBagConstraints.HORIZONTAL;
     constraints.gridheight = 1;
@@ -162,27 +199,39 @@ public class AboutWindow extends BasicWindow {
     constraints.ipady = 0;
     constraints.weightx = 1;
     constraints.weighty = 0;
+    return constraints;
+  }
 
-    // Short presentation
-    JLabel label = new JLabel();
-    label.setText(
+  /**
+   * Create a JScrollPane.
+   * 
+   * @param component Component inside the JScrollPane.
+   * @return JScrollPane.
+   */
+  private JScrollPane createScrollPane(JComponent component) {
+    JScrollPane scrollPane = new JScrollPane(
+        component,
+        ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    scrollPane.setPreferredSize(new Dimension(500, 400));
+    return scrollPane;
+  }
+
+  /**
+   * @return Wikipedia Cleaner tab.
+   */
+  private Component createWPCleanerTab() {
+    JPanel panel = new JPanel(new GridBagLayout());
+    GridBagConstraints constraints = initializeGridBagConstraints();
+    addPresentation(
+        panel, constraints,
         "<html>" +
         "<b>WPCleaner</b> is a tool designed to help with various maintenance tasks." +
         "<br>" +
         "See <a href='http://en.wikipedia.org/wiki/Wikipedia:WPCleaner'>Wikipedia:WPCleaner</a> for more information." +
         "</html>");
-    panel.add(label, constraints);
-    constraints.gridy++;
-
-    // License
-    String license = loadFile("LICENSE.txt");
-    if ((license != null) && (!"".equals(license.trim()))) {
-      label = new JLabel(license);
-      panel.add(label, constraints);
-      constraints.gridy++;
-    }
-
-    return panel;
+    addFile(panel, constraints, "LICENSE.txt");
+    return createScrollPane(panel);
   }
 
   /**
@@ -190,23 +239,9 @@ public class AboutWindow extends BasicWindow {
    */
   private Component createCommonsLoggingTab() {
     JPanel panel = new JPanel(new GridBagLayout());
-
-    // Initialize constraints
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.gridheight = 1;
-    constraints.gridwidth = 1;
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    constraints.insets = new Insets(2, 2, 2, 2);
-    constraints.ipadx = 0;
-    constraints.ipady = 0;
-    constraints.weightx = 1;
-    constraints.weighty = 0;
-
-    // Short presentation
-    JLabel label = new JLabel();
-    label.setText(
+    GridBagConstraints constraints = initializeGridBagConstraints();
+    addPresentation(
+        panel, constraints,
         "<html>" +
         "<b>Apache Commons Logging</b> is a component of Commons Apache projet." +
         "<br>" +
@@ -214,26 +249,9 @@ public class AboutWindow extends BasicWindow {
         "<br>" +
         "See <a href='http://commons.apache.org/logging/'>http://commons.apache.org/logging/</a> for more information." +
         "</html>");
-    panel.add(label, constraints);
-    constraints.gridy++;
-
-    // Notice
-    String notice = loadFile("NOTICE_commons-logging.txt");
-    if ((notice != null) && (!"".equals(notice.trim()))) {
-      label = new JLabel(notice);
-      panel.add(label, constraints);
-      constraints.gridy++;
-    }
-
-    // License
-    String license = loadFile("LICENSE_commons-logging.txt");
-    if ((license != null) && (!"".equals(license.trim()))) {
-      label = new JLabel(license);
-      panel.add(label, constraints);
-      constraints.gridy++;
-    }
-
-    return panel;
+    addFile(panel, constraints, "NOTICE_commons-logging.txt");
+    addFile(panel, constraints, "LICENSE_commons-logging.txt");
+    return createScrollPane(panel);
   }
 
   /**
@@ -241,23 +259,9 @@ public class AboutWindow extends BasicWindow {
    */
   private Component createCommonsCodecTab() {
     JPanel panel = new JPanel(new GridBagLayout());
-
-    // Initialize constraints
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.gridheight = 1;
-    constraints.gridwidth = 1;
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    constraints.insets = new Insets(2, 2, 2, 2);
-    constraints.ipadx = 0;
-    constraints.ipady = 0;
-    constraints.weightx = 1;
-    constraints.weighty = 0;
-
-    // Short presentation
-    JLabel label = new JLabel();
-    label.setText(
+    GridBagConstraints constraints = initializeGridBagConstraints();
+    addPresentation(
+        panel, constraints,
         "<html>" +
         "<b>Apache Commons Codec</b> is a component of Commons Apache projet." +
         "<br>" +
@@ -265,26 +269,9 @@ public class AboutWindow extends BasicWindow {
         "<br>" +
         "See <a href='http://commons.apache.org/codec/'>http://commons.apache.org/codec/</a> for more information." +
         "</html>");
-    panel.add(label, constraints);
-    constraints.gridy++;
-
-    // Notice
-    String notice = loadFile("NOTICE_commons-codec.txt");
-    if ((notice != null) && (!"".equals(notice.trim()))) {
-      label = new JLabel(notice);
-      panel.add(label, constraints);
-      constraints.gridy++;
-    }
-
-    // License
-    String license = loadFile("LICENSE_commons-codec.txt");
-    if ((license != null) && (!"".equals(license.trim()))) {
-      label = new JLabel(license);
-      panel.add(label, constraints);
-      constraints.gridy++;
-    }
-
-    return panel;
+    addFile(panel, constraints, "NOTICE_commons-codec.txt");
+    addFile(panel, constraints, "LICENSE_commons-codec.txt");
+    return createScrollPane(panel);
   }
 
   /**
@@ -292,34 +279,17 @@ public class AboutWindow extends BasicWindow {
    */
   private Component createCommonsHttpClientTab() {
     JPanel panel = new JPanel(new GridBagLayout());
-
-    // Initialize constraints
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.gridheight = 1;
-    constraints.gridwidth = 1;
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    constraints.insets = new Insets(2, 2, 2, 2);
-    constraints.ipadx = 0;
-    constraints.ipady = 0;
-    constraints.weightx = 1;
-    constraints.weighty = 0;
-
-    // Short presentation
-    JLabel presentation = new JLabel();
-    presentation.setText(
+    GridBagConstraints constraints = initializeGridBagConstraints();
+    addPresentation(
+        panel, constraints,
         "<html>" +
         "<b>Jakarta Commons HttpClient</b> is an Apache projet." +
         "<br>" +
         "See <a href='http://jakarta.apache.org/httpcomponents/httpclient-3.x/'>http://jakarta.apache.org/httpcomponents/httpclient-3.x/</a> for more information." +
         "</html>");
-    panel.add(presentation, constraints);
-    constraints.gridy++;
-
-    //TODO: License
-
-    return panel;
+    addFile(panel, constraints, "NOTICE_commons-httpclient.txt");
+    addFile(panel, constraints, "LICENSE_commons-httpclient.txt");
+    return createScrollPane(panel);
   }
 
   /**
@@ -328,7 +298,7 @@ public class AboutWindow extends BasicWindow {
   private Component createGettextCommonsTab() {
     JPanel panel = new JPanel(new GridBagLayout());
     //TODO
-    return panel;
+    return createScrollPane(panel);
   }
 
   /**
@@ -337,7 +307,7 @@ public class AboutWindow extends BasicWindow {
   private Component createJDomTab() {
     JPanel panel = new JPanel(new GridBagLayout());
     //TODO
-    return panel;
+    return createScrollPane(panel);
   }
 
   /**
@@ -346,7 +316,7 @@ public class AboutWindow extends BasicWindow {
   private Component createJaxenTab() {
     JPanel panel = new JPanel(new GridBagLayout());
     //TODO
-    return panel;
+    return createScrollPane(panel);
   }
 
   /**
@@ -356,7 +326,7 @@ public class AboutWindow extends BasicWindow {
     JPanel panel = new JPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     //TODO
-    return panel;
+    return createScrollPane(panel);
   }
 
   /**
