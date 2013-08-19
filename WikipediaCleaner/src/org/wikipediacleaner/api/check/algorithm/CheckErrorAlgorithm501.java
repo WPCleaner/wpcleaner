b2c5f1b4287731415c508dd3d4baa2e871d7b16a
nicove <WPCleaner@users.noreply.github.com>
2013-08-19 21:54:13 +0000

    Error n°501: Spelling and typography

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm501.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm501.java
@@ -545,6 +545,7 @@ public class CheckErrorAlgorithm501 extends CheckErrorAlgorithmBase {
 
     // Remove tags
     removeCompleteTags(chunks, analysis, PageElementTag.TAG_WIKI_CODE);
+    removeCompleteTags(chunks, analysis, PageElementTag.TAG_WIKI_IMAGEMAP); // TODO: keep descriptions
     removeCompleteTags(chunks, analysis, PageElementTag.TAG_WIKI_MATH);
     removeCompleteTags(chunks, analysis, PageElementTag.TAG_WIKI_SCORE);
     removeCompleteTags(chunks, analysis, PageElementTag.TAG_WIKI_SOURCE);
