22fbcb142915732ca21260a18069ecc1f61df505
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2013-08-24 11:16:44 +0000

    Error n°51 & 53: Interwiki before end

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm053.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm053.java
@@ -14,6 +14,7 @@ import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementCategory;
 import org.wikipediacleaner.api.data.PageElementLanguageLink;
+import org.wikipediacleaner.api.data.PageElementTitle;
 
 
 /**
@@ -29,39 +30,46 @@ public class CheckErrorAlgorithm053 extends CheckErrorAlgorithmBase {
   /**
    * Analyze a page to check if errors are present.
    * 
-   * @param pageAnalysis Page analysis.
+   * @param analysis Page analysis.
    * @param errors Errors found in the page.
    * @return Flag indicating if the error was found.
    */
   public boolean analyze(
-      PageAnalysis pageAnalysis,
+      PageAnalysis analysis,
       Collection<CheckErrorResult> errors) {
-    if (pageAnalysis == null) {
+    if (analysis == null) {
       return false;
     }
 
-    // Retrieving last category
-    List<PageElementCategory> categories = pageAnalysis.getCategories();
+    // Retrieving last category and headline
+    List<PageElementCategory> categories = analysis.getCategories();
     if (categories.size() == 0) {
       return false;
     }
     int lastCategory = categories.get(categories.size() - 1).getEndIndex();
+    List<PageElementTitle> titles = analysis.getTitles();
+    int lastTitle = (titles.size() > 0) ? (titles.get(titles.size() - 1).getEndIndex()) : 0;
 
     // Check every language link
-    List<PageElementLanguageLink> languages = pageAnalysis.getLanguageLinks();
+    List<PageElementLanguageLink> languages = analysis.getLanguageLinks();
+    String contents = analysis.getContents();
     boolean result = false;
     for (PageElementLanguageLink language : languages) {
-      if (language.getBeginIndex() >= lastCategory) {
+      int begin = language.getBeginIndex();
+      if (begin >= lastCategory) {
         return result;
       }
-      if (errors == null) {
-        return true;
+      if (begin > lastTitle) {
+        if (errors == null) {
+          return true;
+        }
+        result = true;
+        int end = language.getEndIndex();
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis.getPage(), begin, end);
+        errorResult.addReplacement("[[:" + contents.substring(begin + 2, end));
+        errors.add(errorResult);
       }
-      result = true;
-      CheckErrorResult errorResult = createCheckErrorResult(
-          pageAnalysis.getPage(),
-          language.getBeginIndex(), language.getEndIndex());
-      errors.add(errorResult);
     }
 
     return result;
