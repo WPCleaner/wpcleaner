1ee01cf03203851499cce6807cea429d3452a62a
nicove <WPCleaner@users.noreply.github.com>
2013-08-31 19:04:29 +0000

    Tidying up articles

--- WikipediaCleaner/src/org/wikipediacleaner/api/data/PageAnalysis.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/data/PageAnalysis.java
@@ -19,6 +19,8 @@ import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithm;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithms;
 import org.wikipediacleaner.api.constants.EnumWikipedia;
 import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.constants.WPCConfigurationBoolean;
+import org.wikipediacleaner.api.constants.WPCConfigurationString;
 import org.wikipediacleaner.api.constants.WikiConfiguration;
 import org.wikipediacleaner.api.constants.wiki.AbstractWikiSettings;
 import org.wikipediacleaner.utils.Configuration;
@@ -1488,12 +1490,14 @@ public class PageAnalysis {
     if ((page == null) || (contents == null)) {
       return contents;
     }
+    EnumWikipedia wiki = page.getWikipedia();
+    WPCConfiguration config = wiki.getConfiguration();
 
     // Fix Check Wiki errors
     if (algorithms != null) {
       for (CheckErrorAlgorithm algorithm : algorithms) {
         if (algorithm.isAvailable() &&
-            CheckErrorAlgorithms.isAlgorithmActive(page.getWikipedia(), algorithm.getErrorNumber())) {
+            CheckErrorAlgorithms.isAlgorithmActive(wiki, algorithm.getErrorNumber())) {
           String currentContents = contents;
           PageAnalysis analysis = page.getAnalysis(currentContents, true);
           contents = algorithm.automaticFix(analysis);
@@ -1504,6 +1508,125 @@ public class PageAnalysis {
       }
     }
 
+    if (!config.getBoolean(WPCConfigurationBoolean.AUTO_ACTIVE)) {
+      return contents;
+    }
+
+    // Auto formatting options: number of carriage returns between default sort and category
+    String option = config.getString(WPCConfigurationString.AUTO_CR_DEFAULTSORT_CATEGORY);
+    if (isValidCrOption(option)) {
+      int min = getMinCrOption(option);
+      int max = getMaxCrOption(option);
+      if ((min > 0) || (max < Integer.MAX_VALUE)) {
+        PageAnalysis analysis = page.getAnalysis(contents, true);
+        for (PageElementFunction function : analysis.getDefaultSorts()) {
+          int nbCr = 0;
+          int index = function.getEndIndex();
+          boolean finished = false;
+          while (!finished && (index < contents.length())) {
+            char currentChar = contents.charAt(index);
+            if (currentChar == '\n') {
+              nbCr++;
+              index++;
+            } else if (currentChar == ' ') {
+              index++;
+            } else {
+              finished = true;
+            }
+          }
+          if (finished && (contents.charAt(index) == '[') &&
+              ((nbCr < min) || (nbCr > max))) {
+            PageElementCategory category = analysis.isInCategory(index);
+            if (category != null) {
+              StringBuilder sb = new StringBuilder(contents.substring(0, function.getEndIndex()));
+              for (int i = normalizeValue(nbCr, min, max); i > 0; i--) {
+                sb.append('\n');
+              }
+              sb.append(contents.substring(category.getBeginIndex()));
+              contents = sb.toString();
+            }
+          }
+        }
+      }
+    }
+
     return contents;
+  }
+
+  /**
+   * @param option Option for number of carriage returns.
+   * @return True if option is valid.
+   */
+  private static boolean isValidCrOption(String option) {
+    if ((option == null) || (option.length() == 0)) {
+      return false;
+    }
+    int minusIndex = option.indexOf('-');
+    try {
+      if (minusIndex < 0) {
+        int value = Integer.parseInt(option);
+        if (value < 0) {
+          return false;
+        }
+      } else {
+        if (minusIndex + 1 >= option.length()) {
+          return false;
+        }
+        int min = Integer.parseInt(option.substring(0, minusIndex));
+        int max = Integer.parseInt(option.substring(minusIndex + 1));
+        if ((min < 0) || (max < min)) {
+          return false;
+        }
+      }
+    } catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * @param option Option for number of carriage returns.
+   * @return Minimum number of carriage returns.
+   */
+  private static int getMinCrOption(String option) {
+    if ((option == null) || (option.length() == 0)) {
+      return 0;
+    }
+    int minusIndex = option.indexOf('-');
+    if (minusIndex < 0) {
+      return Integer.parseInt(option);
+    }
+    return Integer.parseInt(option.substring(0, minusIndex));
+  }
+
+  /**
+   * @param option Option for number of carriage returns.
+   * @return Maximum number of carriage returns.
+   */
+  private static int getMaxCrOption(String option) {
+    if ((option == null) || (option.length() == 0)) {
+      return Integer.MAX_VALUE;
+    }
+    int minusIndex = option.indexOf('-');
+    if (minusIndex < 0) {
+      return Integer.parseInt(option);
+    }
+    return Integer.parseInt(option.substring(minusIndex + 1));
+  }
+
+  /**
+   * @param value Current value.
+   * @param min Minimum possible value.
+   * @param max Maximum possible value.
+   * @return Normalized value.
+   */
+  private static int normalizeValue(int value, int min, int max) {
+    if (value < min) {
+      return min;
+    }
+    if (value > max) {
+      return max;
+    }
+    return value;
   }
 }
