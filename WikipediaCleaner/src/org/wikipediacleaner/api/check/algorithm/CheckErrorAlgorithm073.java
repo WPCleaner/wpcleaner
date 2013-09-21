919bf928545e6f12cde9fe46828c41dee2002a39
nicove <WPCleaner@users.noreply.github.com>
2013-09-21 20:23:35 +0000

    ISBN

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm073.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm073.java
@@ -8,10 +8,15 @@
 package org.wikipediacleaner.api.check.algorithm;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.NullActionProvider;
+import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.constants.WPCConfigurationString;
+import org.wikipediacleaner.api.constants.WPCConfigurationStringList;
 import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementISBN;
 import org.wikipediacleaner.i18n.GT;
 
 
@@ -36,118 +41,74 @@ public class CheckErrorAlgorithm073 extends CheckErrorAlgorithmBase {
   /**
    * Analyze a page to check if errors are present.
    * 
-   * @param pageAnalysis Page analysis.
+   * @param analysis Page analysis.
    * @param errors Errors found in the page.
    * @return Flag indicating if the error was found.
    */
   public boolean analyze(
-      PageAnalysis pageAnalysis,
+      PageAnalysis analysis,
       Collection<CheckErrorResult> errors) {
-    if (pageAnalysis == null) {
+    if (analysis == null) {
       return false;
     }
 
-    // Analyze contents from the beginning
-    int startIndex = -1;
+    // Configuration
+    WPCConfiguration config = analysis.getWPCConfiguration();
+    List<String[]> helpNeededTemplates = config.getStringArrayList(
+        WPCConfigurationStringList.ISBN_HELP_NEEDED_TEMPLATES);
+    String helpNeededComment = config.getString(
+        WPCConfigurationString.ISBN_HELP_NEEDED_COMMENT);
+    String reasonTemplate = getSpecificProperty("reason", true, true, false);
+
+    // Analyze each ISBN
     boolean result = false;
-    String contents = pageAnalysis.getContents();
-    while (startIndex < contents.length()) {
-      startIndex = contents.indexOf("ISBN ", startIndex + 1);
-      if (startIndex < 0) {
-        startIndex = contents.length();
-      } else {
-        // Removing white spaces
-        int tmpIndex = startIndex + 5;
-        while ((tmpIndex < contents.length()) && (contents.charAt(tmpIndex) == ' ')) {
-          tmpIndex++;
-        }
-
-        // Check 12 digits and compute check
-        boolean isISBN = true;
-        int check = 0;
-        if ((tmpIndex < contents.length()) && (contents.charAt(tmpIndex) == '9')) {
-          check += (contents.charAt(tmpIndex) - '0');
-          tmpIndex++;
-        } else {
-          isISBN = false;
-        }
-        if ((tmpIndex < contents.length()) && (contents.charAt(tmpIndex) == '7')) {
-          check += 3 * (contents.charAt(tmpIndex) - '0');
-          tmpIndex++;
-        } else {
-          isISBN = false;
-        }
-        if ((tmpIndex < contents.length()) &&
-            ((contents.charAt(tmpIndex) == '8') ||
-             (contents.charAt(tmpIndex) == '9'))) {
-          check += (contents.charAt(tmpIndex) - '0');
-          tmpIndex++;
-        }
-        if ((tmpIndex < contents.length()) &&
-            ((contents.charAt(tmpIndex) == ' ') || (contents.charAt(tmpIndex) == '-'))) {
-          tmpIndex++;
-        }
-        for (int i = 0; i < 9; i++) {
-          if ((tmpIndex < contents.length()) && Character.isDigit(contents.charAt(tmpIndex))) {
-            check += ((i % 2) == 0 ? 3 : 1) * (contents.charAt(tmpIndex) - '0');
-            tmpIndex++;
-          } else {
-            isISBN = false;
-          }
-          if ((tmpIndex < contents.length()) &&
-              ((contents.charAt(tmpIndex) == ' ') || (contents.charAt(tmpIndex) == '-'))) {
-            tmpIndex++;
-          }
-        }
-        check = check % 10; // Modulus 10
-        check = 10 - check; // Invert
-        check = check % 10; // 10 -> 0
-        char computedCheck = (char) ('0' + check);
-
-        // Verify check
-        boolean checkVerified = false;
-        char checkCharacter = ' ';
-        if ((tmpIndex < contents.length()) &&
-            ((Character.isDigit(contents.charAt(tmpIndex))) ||
-             (contents.charAt(tmpIndex) == 'X') ||
-             (contents.charAt(tmpIndex) == 'x'))) {
-          checkCharacter = contents.charAt(tmpIndex);
-          if (computedCheck == Character.toUpperCase(checkCharacter)) {
-            checkVerified = true;
-          }
-        } else {
-          isISBN = false;
-        }
-        tmpIndex++;
-
-        // Verify end of ISBN
-        while ((tmpIndex < contents.length()) &&
-               ((contents.charAt(tmpIndex) == '-') ||
-                (contents.charAt(tmpIndex) == ' '))) {
-          tmpIndex++;
-        }
-        if ((tmpIndex < contents.length()) &&
-            (Character.isLetterOrDigit(contents.charAt(tmpIndex)))) {
-          isISBN = false;
-        }
-
-        // Check result
-        if (isISBN && !checkVerified) {
+    String contents = analysis.getContents();
+    List<PageElementISBN> isbns = analysis.getISBNs();
+    for (PageElementISBN isbn : isbns) {
+      String number = isbn.getISBN();
+      if ((number != null) && (number.length() == 13)) {
+        char check = Character.toUpperCase(number.charAt(12));
+        char computedCheck = Character.toUpperCase(isbn.getCheck());
+        if ((check != computedCheck) && Character.isDigit(computedCheck)) {
           if (errors == null) {
             return true;
           }
+          result = true;
           CheckErrorResult errorResult = createCheckErrorResult(
-              pageAnalysis.getPage(), startIndex, tmpIndex);
+              analysis.getPage(), isbn.getBeginIndex(), isbn.getEndIndex());
           errorResult.addPossibleAction(
               GT._(
                   "The checksum is {0} instead of {1}",
-                  new Object[] { checkCharacter, computedCheck } ),
+                  new Object[] { check, computedCheck } ),
               new NullActionProvider());
+          String reason = null;
+          if (reasonTemplate != null) {
+            reason = GT._(reasonTemplate, new Object[] { computedCheck, check });
+          }
+          if ((helpNeededTemplates != null) &&
+              (!helpNeededTemplates.isEmpty())) {
+            for (String[] helpNeededTemplate : helpNeededTemplates) {
+              String replacement = isbn.askForHelp(helpNeededTemplate, reason);
+              if (replacement != null) {
+                errorResult.addReplacement(
+                    replacement.toString(),
+                    GT._("Ask for help using {0}", "{{" + helpNeededTemplate[0] + "}}"));
+              }
+            }
+          }
+          if (helpNeededComment != null) {
+            String replacement = isbn.askForHelp(helpNeededComment, reason);
+            if (replacement != null) {
+              errorResult.addReplacement(
+                  contents.substring(isbn.getBeginIndex(), isbn.getEndIndex()) + replacement.toString(),
+                  GT._("Add a comment"));
+            }
+          }
           errors.add(errorResult);
-          result = true;
         }
       }
     }
+
     return result;
   }
 }
