1f38afe7cd10837b23ce038ace114f5bd4c82cc1
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-01-22 21:25:58 +0000

    #042 instead of #517

---
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm042_Old.java
@@ -0,0 +1,87 @@
+/*
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2013  Nicolas Vervelle
+ *
+ *  See README.txt file for licensing information.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.Collection;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementTag;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Algorithm for analyzing error 42 of check wikipedia project.
+ * Error 42: HTML text style element &lt;small&gt;
+ */
+public class CheckErrorAlgorithm042_Old extends CheckErrorAlgorithmBase {
+
+  public CheckErrorAlgorithm042_Old() {
+    super("HTML text style element <small>");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param pageAnalysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis pageAnalysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (pageAnalysis == null) {
+      return false;
+    }
+
+    // Analyzing the text from the beginning
+    Collection<PageElementTag> tags = pageAnalysis.getTags(PageElementTag.TAG_HTML_SMALL);
+    if (tags == null) {
+      return false;
+    }
+    int level = 0;
+    boolean result = false;
+    for (PageElementTag tag : tags) {
+      if (tag.isFullTag()) {
+        // Full tag
+        if (level == 0) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              pageAnalysis.getPage(),
+              tag.getBeginIndex(), tag.getEndIndex());
+          errorResult.addReplacement("", GT._("Delete"));
+          errors.add(errorResult);
+        }
+      } else if (tag.isEndTag()) {
+        // Closing tag
+        level--;
+        if (level < 0) {
+          level = 0;
+        }
+      } else {
+        level++;
+        if (level == 1) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              pageAnalysis.getPage(),
+              tag.getCompleteBeginIndex(), tag.getCompleteEndIndex());
+          errors.add(errorResult);
+        }
+      }
+    }
+
+    return result;
+  }
+}
--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm517.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm517.java
@@ -7,60 +7,12 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.wikipediacleaner.api.check.CheckErrorResult;
-import org.wikipediacleaner.api.data.Namespace;
-import org.wikipediacleaner.api.data.PageAnalysis;
-import org.wikipediacleaner.api.data.PageElementTag;
-
 
 /**
  * Algorithm for analyzing error 517 of check wikipedia project.
  * Error 517: strike tags
  */
-public class CheckErrorAlgorithm517 extends CheckErrorAlgorithmBase {
+public class CheckErrorAlgorithm517 extends CheckErrorAlgorithm042 {
 
-  public CheckErrorAlgorithm517() {
-    super("<strike> tags");
-  }
-
-  /**
-   * Analyze a page to check if errors are present.
-   * 
-   * @param analysis Page analysis.
-   * @param errors Errors found in the page.
-   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
-   * @return Flag indicating if the error was found.
-   */
-  public boolean analyze(
-      PageAnalysis analysis,
-      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
-    if ((analysis == null) || (analysis.getPage() == null)) {
-      return false;
-    }
-    Integer ns = analysis.getPage().getNamespace();
-    if ((ns == null) || (ns.intValue() != Namespace.MAIN)) {
-      return false;
-    }
-
-    // Check each tag
-    List<PageElementTag> tags = analysis.getTags(PageElementTag.TAG_HTML_STRIKE);
-    if ((tags == null) || (tags.isEmpty())) {
-      return false;
-    }
-    if (errors == null) {
-      return true;
-    }
-    for (PageElementTag tag : tags) {
-      CheckErrorResult errorResult = createCheckErrorResult(
-          analysis.getPage(), tag.getBeginIndex(), tag.getEndIndex());
-      errorResult.addReplacement(PageElementTag.createTag(
-          PageElementTag.TAG_HTML_S, tag.isEndTag(), tag.isFullTag()));
-      errors.add(errorResult);
-    }
-
-    return true;
-  }
+  // Renamed as 042
 }
