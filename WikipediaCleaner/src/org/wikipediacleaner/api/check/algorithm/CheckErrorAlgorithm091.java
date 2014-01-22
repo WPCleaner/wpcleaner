dcc73ea00bb6eb45679bcdb541ee61f95a4b588b
nicove <WPCleaner@users.noreply.github.com>
2014-01-22 21:46:53 +0000

    #091 instead of #512

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm091.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm091.java
@@ -9,93 +9,224 @@ package org.wikipediacleaner.api.check.algorithm;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
+import org.wikipediacleaner.api.check.AddTextActionProvider;
 import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.check.CheckLanguageLinkActionProvider;
+import org.wikipediacleaner.api.constants.CWConfigurationError;
+import org.wikipediacleaner.api.constants.EnumWikipedia;
+import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.data.Interwiki;
 import org.wikipediacleaner.api.data.PageAnalysis;
-import org.wikipediacleaner.api.data.PageElementCategory;
-import org.wikipediacleaner.api.data.PageElementFunction;
+import org.wikipediacleaner.api.data.PageElementExternalLink;
+import org.wikipediacleaner.gui.swing.component.MWPane;
 import org.wikipediacleaner.i18n.GT;
+import org.wikipediacleaner.utils.StringChecker;
+import org.wikipediacleaner.utils.StringCheckerUnauthorizedCharacters;
 
 
 /**
- * Algorithm for analyzing error 91 of check wikipedia project.
- * Error 91: DEFAULTSORT is missing and title with lower case letters
+ * Algorithm for analyzing error 091 of check wikipedia project.
+ * Error 091: Interwiki link written as external link
  */
 public class CheckErrorAlgorithm091 extends CheckErrorAlgorithmBase {
 
+  /**
+   * String checker for text inputed by user.
+   */
+  private final StringChecker checker;
+
+  /**
+   * Possible global fixes.
+   */
+  private final static String[] globalFixes = new String[] {
+    GT._("Convert them to internal links"),
+  };
+
   public CheckErrorAlgorithm091() {
-    super("DEFAULTSORT is missing and title with lowercase letters");
+    super("Interwiki link written as external link");
+    checker = new StringCheckerUnauthorizedCharacters("[]\"");
   }
 
   /**
    * Analyze a page to check if errors are present.
    * 
-   * @param pageAnalysis Page analysis.
+   * @param analysis Page analysis.
    * @param errors Errors found in the page.
    * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
    * @return Flag indicating if the error was found.
    */
   public boolean analyze(
-      PageAnalysis pageAnalysis,
+      PageAnalysis analysis,
       Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
-    if (pageAnalysis == null) {
+    if ((analysis == null) || (analysis.getInternalLinks() == null)) {
       return false;
     }
 
-    // Checking if title contains lower case letters
-    boolean lowFound = false;
-    boolean firstLetter = true;
-    String title = pageAnalysis.getPage().getTitle();
-    for (int currentPos = 0; currentPos < title.length(); currentPos++) {
-      if (Character.isLowerCase(title.charAt(currentPos))) {
-        if (firstLetter) {
-          lowFound = true;
+    // Retrieve configuration
+    EnumWikipedia wiki = analysis.getWikipedia();
+    CWConfigurationError error68 = wiki.getCWConfiguration().getErrorConfiguration(68);
+    List<String> templatesList = null;
+    if (error68 != null) {
+      String templatesParam = error68.getSpecificProperty("template", true, false, false);
+      if (templatesParam != null) {
+        templatesList = WPCConfiguration.convertPropertyToStringList(templatesParam);
+      }
+    }
+    boolean onlyLanguage = Boolean.valueOf(getSpecificProperty("only_language", true, false, false));
+    boolean onlyLocal = Boolean.valueOf(getSpecificProperty("only_local", true, false, false));
+
+    // Analyze each external link
+    boolean result = false;
+    List<PageElementExternalLink> links = analysis.getExternalLinks();
+    if (links == null) {
+      return result;
+    }
+    List<Interwiki> interwikis = wiki.getWikiConfiguration().getInterwikis();
+    String contents = analysis.getContents();
+    for (PageElementExternalLink link : links) {
+      if (link.hasSquare()) {
+
+        // Check if this is a external link to an other wiki
+        String article = null;
+        String prefix = null;
+        String language = null;
+        boolean local = false;
+        for (Interwiki interwiki : interwikis) {
+          String tmp = interwiki.isArticleUrl(link.getLink());
+          if (tmp != null) {
+            if ((article == null) || (interwiki.getLanguage() != null)) {
+              article = tmp;
+              prefix = interwiki.getPrefix();
+              language = interwiki.getLanguage();
+              local = interwiki.getLocal();
+            }
+          }
         }
-        firstLetter = false;
-      } else if (Character.isWhitespace(title.charAt(currentPos))) {
-        firstLetter = true;
-      } else {
-        firstLetter = false;
+        EnumWikipedia fromWiki = null;
+        if (prefix != null) {
+          fromWiki = EnumWikipedia.getWikipedia(prefix);
+          if (!prefix.equals(fromWiki.getSettings().getCode())) {
+            fromWiki = null;
+          }
+        }
+
+        // Mark error
+        if ((article != null) &&
+            ((article.length() > 0) || (local && (link.getText() != null))) &&
+            (prefix != null) && (prefix.length() > 0) &&
+            (fromWiki != wiki) &&
+            (!onlyLanguage || (language != null)) &&
+            (!onlyLocal || local)) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          int beginIndex = link.getBeginIndex();
+          int endIndex = link.getEndIndex();
+          if ((beginIndex > 0) && (contents.charAt(beginIndex - 1) == '[') &&
+              (endIndex < contents.length()) && (contents.charAt(endIndex) == ']')) {
+            beginIndex--;
+            endIndex++;
+          }
+
+          // Check language link
+          CheckErrorResult errorResult = createCheckErrorResult(
+              analysis.getPage(), beginIndex, endIndex);
+          if ((fromWiki != null) && (article.length() >0)) {
+            errorResult.addPossibleAction(
+                GT._("Check language links"),
+                new CheckLanguageLinkActionProvider(
+                    fromWiki, analysis.getWikipedia(),
+                    article, link.getText()));
+          }
+
+          // Use templates
+          if ((templatesList != null) &&
+              (templatesList.size() > 0) &&
+              (article.length() > 0)) {
+            for (String template : templatesList) {
+              String[] templateArgs = template.split("\\|");
+              if (templateArgs.length >= 5) {
+                String textPrefix =
+                  "{{" + templateArgs[0] + "|" + templateArgs[1] + "=";
+                String textSuffix =
+                  "|" + templateArgs[2] + "=" + prefix +
+                  "|" + templateArgs[3] + "=" + article +
+                  "|" + templateArgs[4] + "=" + ((link.getText() != null) ? link.getText() : article) +
+                  "}}";
+                String question = GT._("What is the title of the page on this wiki ?");
+                AddTextActionProvider action = null;
+                if ((link.getText() != null) && (!link.getText().equals(article))) {
+                  String[] possibleValues = { null, article, link.getText() };
+                  action = new AddTextActionProvider(
+                      textPrefix, textSuffix, null, question,
+                      possibleValues, false, null, checker);
+                } else {
+                  action = new AddTextActionProvider(
+                      textPrefix, textSuffix, null, question,
+                      article, checker);
+                }
+                errorResult.addPossibleAction(
+                    GT._("Replace using template {0}", "{{" + templateArgs[0] + "}}"),
+                    action);
+              }
+            }
+          }
+
+          // Create internal link
+          errorResult.addReplacement(
+              "[[:" + prefix + ":" + article + "|" + (link.getText() != null ? link.getText() : article) + "]]");
+          errors.add(errorResult);
+        }
       }
     }
-    if (!lowFound) {
-      return false;
-    }
 
-    // Searching a DEFAULTSORT tag
-    List<PageElementFunction> defaultSorts = pageAnalysis.getDefaultSorts();
-    if (defaultSorts.size() > 0) {
-      return false;
-    }
+    return result;
+  }
 
-    // Searching for Categories without a sort key
-    boolean categoriesWithoutSort = false;
-    List<PageElementCategory> categories = pageAnalysis.getCategories();
-    if ((categories == null) || (categories.isEmpty())) {
-      return false;
-    }
-    for (PageElementCategory category : categories) {
-      if ((category.getSort() == null) ||
-          (category.getSort().trim().length() == 0)) {
-        categoriesWithoutSort = true;
-      }
-    }
-    if (!categoriesWithoutSort) {
-      return false;
-    }
+  /**
+   * Bot fixing of all the errors in the page.
+   * 
+   * @param analysis Page analysis.
+   * @return Page contents after fix.
+   */
+  @Override
+  protected String internalBotFix(PageAnalysis analysis) {
+    return fix(globalFixes[0], analysis, null);
+  }
 
-    // Reporting error
-    if (errors == null) {
-      return true;
-    }
-    PageElementCategory category = categories.get(0);
-    CheckErrorResult errorResult = createCheckErrorResult(
-        pageAnalysis.getPage(), category.getBeginIndex(), category.getEndIndex());
-    String replacement =
-        createDefaultSort(pageAnalysis) + "\n" +
-    pageAnalysis.getContents().substring(category.getBeginIndex(), category.getEndIndex());
-    errorResult.addReplacement(replacement, GT._("Add DEFAULTSORT"));
-    errors.add(errorResult);
-    return true;
+  /**
+   * @return List of possible global fixes.
+   */
+  @Override
+  public String[] getGlobalFixes() {
+    return globalFixes;
+  }
+
+  /**
+   * Fix all the errors in the page.
+   * 
+   * @param fixName Fix name (extracted from getGlobalFixes()).
+   * @param analysis Page analysis.
+   * @param textPane Text pane.
+   * @return Page contents after fix.
+   */
+  @Override
+  public String fix(String fixName, PageAnalysis analysis, MWPane textPane) {
+    return fixUsingFirstReplacement(fixName, analysis);
+  }
+
+  /**
+   * @return Map of parameters (Name -> description).
+   * @see org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase#getParameters()
+   */
+  @Override
+  public Map<String, String> getParameters() {
+    Map<String, String> parameters = super.getParameters();
+    parameters.put("only_language", GT._("To report only links to an other language"));
+    parameters.put("only_local", GT._("To report only links to local wikis"));
+    return parameters;
   }
 }
---
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm091_Old.java
@@ -0,0 +1,101 @@
+/*
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2013  Nicolas Vervelle
+ *
+ *  See README.txt file for licensing information.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementCategory;
+import org.wikipediacleaner.api.data.PageElementFunction;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Algorithm for analyzing error 91 of check wikipedia project.
+ * Error 91: DEFAULTSORT is missing and title with lower case letters
+ */
+public class CheckErrorAlgorithm091_Old extends CheckErrorAlgorithmBase {
+
+  public CheckErrorAlgorithm091_Old() {
+    super("DEFAULTSORT is missing and title with lowercase letters");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param pageAnalysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis pageAnalysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (pageAnalysis == null) {
+      return false;
+    }
+
+    // Checking if title contains lower case letters
+    boolean lowFound = false;
+    boolean firstLetter = true;
+    String title = pageAnalysis.getPage().getTitle();
+    for (int currentPos = 0; currentPos < title.length(); currentPos++) {
+      if (Character.isLowerCase(title.charAt(currentPos))) {
+        if (firstLetter) {
+          lowFound = true;
+        }
+        firstLetter = false;
+      } else if (Character.isWhitespace(title.charAt(currentPos))) {
+        firstLetter = true;
+      } else {
+        firstLetter = false;
+      }
+    }
+    if (!lowFound) {
+      return false;
+    }
+
+    // Searching a DEFAULTSORT tag
+    List<PageElementFunction> defaultSorts = pageAnalysis.getDefaultSorts();
+    if (defaultSorts.size() > 0) {
+      return false;
+    }
+
+    // Searching for Categories without a sort key
+    boolean categoriesWithoutSort = false;
+    List<PageElementCategory> categories = pageAnalysis.getCategories();
+    if ((categories == null) || (categories.isEmpty())) {
+      return false;
+    }
+    for (PageElementCategory category : categories) {
+      if ((category.getSort() == null) ||
+          (category.getSort().trim().length() == 0)) {
+        categoriesWithoutSort = true;
+      }
+    }
+    if (!categoriesWithoutSort) {
+      return false;
+    }
+
+    // Reporting error
+    if (errors == null) {
+      return true;
+    }
+    PageElementCategory category = categories.get(0);
+    CheckErrorResult errorResult = createCheckErrorResult(
+        pageAnalysis.getPage(), category.getBeginIndex(), category.getEndIndex());
+    String replacement =
+        createDefaultSort(pageAnalysis) + "\n" +
+    pageAnalysis.getContents().substring(category.getBeginIndex(), category.getEndIndex());
+    errorResult.addReplacement(replacement, GT._("Add DEFAULTSORT"));
+    errors.add(errorResult);
+    return true;
+  }
+}
