07d59bf6352004f738e5203f138274886971712b
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-01-22 22:12:32 +0000

    Deactivate #62 and #89

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm062.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm062.java
@@ -7,77 +7,14 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.wikipediacleaner.api.check.CheckErrorResult;
-import org.wikipediacleaner.api.data.PageAnalysis;
-import org.wikipediacleaner.api.data.PageElementTitle;
-
 
 /**
  * Algorithm for analyzing error 62 of check wikipedia project.
- * Error 62: Headline alone
+ * Error 62: URL containing no http://
  */
-public class CheckErrorAlgorithm062 extends CheckErrorAlgorithmBase {
+public class CheckErrorAlgorithm062 extends CheckErrorAlgorithmUnavailable {
 
   public CheckErrorAlgorithm062() {
-    super("Headline alone");
-  }
-
-  /**
-   * Analyze a page to check if errors are present.
-   * 
-   * @param pageAnalysis Page analysis.
-   * @param errors Errors found in the page.
-   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
-   * @return Flag indicating if the error was found.
-   */
-  public boolean analyze(
-      PageAnalysis pageAnalysis,
-      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
-    if (pageAnalysis == null) {
-      return false;
-    }
-
-    // Retrieving titles
-    boolean result = false;
-    List<PageElementTitle> titles = pageAnalysis.getTitles();
-    if (titles == null) {
-      return false;
-    }
-    if (titles.size() < 5) {
-      return false;
-    }
-
-    // Analyzing titles
-    int previousLevel = 0;
-    for (int i = 0; i < titles.size(); i++) {
-      PageElementTitle title = titles.get(i);
-      if ((title.getLevel() > previousLevel) &&
-          (title.getLevel() > 2)) {
-        int j = i + 1;
-        int count = 0;
-        while ((j < titles.size()) &&
-               (titles.get(j).getLevel() >= title.getLevel())) {
-          if (titles.get(j).getLevel() == title.getLevel()) {
-            count++;
-          }
-          j++;
-        }
-        if (count == 0) {
-          if (errors == null) {
-            return true;
-          }
-          result = true;
-          CheckErrorResult errorResult = createCheckErrorResult(
-              pageAnalysis.getPage(), title.getBeginIndex(), title.getEndIndex());
-          errorResult.addEditTocAction();
-          errors.add(errorResult);
-        }
-      }
-      previousLevel = title.getLevel();
-    }
-    return result;
+    super("URL containing no http://");
   }
 }
--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm089.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm089.java
@@ -7,113 +7,14 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.wikipediacleaner.api.check.CheckErrorResult;
-import org.wikipediacleaner.api.data.PageAnalysis;
-import org.wikipediacleaner.api.data.PageElementFunction;
-import org.wikipediacleaner.gui.swing.component.MWPane;
-import org.wikipediacleaner.i18n.GT;
-
 
 /**
  * Algorithm for analyzing error 89 of check wikipedia project.
- * Error 89: DEFAULTSORT with capitalization in the middle of the word
+ * Error 89: DEFAULTSORT with no space after the comma
  */
-public class CheckErrorAlgorithm089 extends CheckErrorAlgorithmBase {
-
-  /**
-   * Possible global fixes.
-   */
-  private final static String[] globalFixes = new String[] {
-    GT._("Fix DEFAULTSORT"),
-  };
+public class CheckErrorAlgorithm089 extends CheckErrorAlgorithmUnavailable {
 
   public CheckErrorAlgorithm089() {
-    super("DEFAULTSORT with capitalization in the middle of the word");
-  }
-
-  /**
-   * Analyze a page to check if errors are present.
-   * 
-   * @param pageAnalysis Page analysis.
-   * @param errors Errors found in the page.
-   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
-   * @return Flag indicating if the error was found.
-   */
-  public boolean analyze(
-      PageAnalysis pageAnalysis,
-      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
-    if (pageAnalysis == null) {
-      return false;
-    }
-
-    // Check every DEFAULTSORT
-    List<PageElementFunction> defaultSorts = pageAnalysis.getDefaultSorts();
-    boolean result = false;
-    for (PageElementFunction defaultSort : defaultSorts) {
-
-      // Check if an upper case character is in the middle of a word
-      boolean firstLetter = true;
-      StringBuilder newText = null;
-      String text = (defaultSort.getParameterCount() > 0) ? defaultSort.getParameterValue(0) : "";
-      for (int index = 0; index < text.length(); index++) {
-        char currentChar = text.charAt(index);
-        if (Character.isUpperCase(currentChar)) {
-          if (!firstLetter) {
-            if (newText == null) {
-              newText = new StringBuilder(text.substring(0, index));
-            }
-            currentChar = Character.toLowerCase(currentChar);
-          }
-          firstLetter = false;
-        } else if (Character.isLowerCase(currentChar)) {
-          firstLetter = false;
-        } else {
-          firstLetter = true;
-        }
-        if (newText != null) {
-          newText.append(currentChar);
-        }
-      }
-
-      // Register error
-      if (newText != null) {
-        if (errors == null) {
-          return true;
-        }
-        result = true;
-        CheckErrorResult errorResult = createCheckErrorResult(
-            pageAnalysis.getPage(),
-            defaultSort.getBeginIndex(), defaultSort.getEndIndex());
-        errorResult.addReplacement(PageElementFunction.createFunction(
-            defaultSort.getFunctionName(), newText.toString()));
-        errors.add(errorResult);
-      }
-    }
-
-    return result;
-  }
-
-  /**
-   * @return List of possible global fixes.
-   */
-  @Override
-  public String[] getGlobalFixes() {
-    return globalFixes;
-  }
-
-  /**
-   * Fix all the errors in the page.
-   * 
-   * @param fixName Fix name (extracted from getGlobalFixes()).
-   * @param analysis Page analysis.
-   * @param textPane Text pane.
-   * @return Page contents after fix.
-   */
-  @Override
-  public String fix(String fixName, PageAnalysis analysis, MWPane textPane) {
-    return fixUsingFirstReplacement(fixName, analysis);
+    super("DEFAULTSORT with no space after the comma");
   }
 }
