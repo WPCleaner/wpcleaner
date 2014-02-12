1e378c53b5c3fb4310b03184f355c903ecb7da9a
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-02-12 15:30:19 +0000

    Error #89: DEFAULTSORT with no space after the comma

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm089.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm089.java
@@ -7,14 +7,77 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementFunction;
+
 
 /**
  * Algorithm for analyzing error 89 of check wikipedia project.
  * Error 89: DEFAULTSORT with no space after the comma
  */
-public class CheckErrorAlgorithm089 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm089 extends CheckErrorAlgorithmBase {
 
   public CheckErrorAlgorithm089() {
     super("DEFAULTSORT with no space after the comma");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (analysis == null) {
+      return false;
+    }
+
+    // Analyze each default sort
+    List<PageElementFunction> defaultSorts = analysis.getDefaultSorts();
+    if ((defaultSorts == null) || (defaultSorts.isEmpty())) {
+      return false;
+    }
+    boolean result = false;
+    for (PageElementFunction defaultSort : defaultSorts) {
+      if (defaultSort.getParameterCount() > 0) {
+        String value = defaultSort.getParameterValue(0);
+        if ((value != null) && (value.trim().length() > 0)) {
+          StringBuilder replacement = new StringBuilder();
+          value = value.trim();
+          for (int index = 0; index < value.length(); index++) {
+            char character = value.charAt(index);
+            replacement.append(character);
+            if ((character == ',') &&
+                (index + 1 < value.length()) &&
+                (Character.isLetter(value.charAt(index + 1)))) {
+              replacement.append(' ');
+            }
+          }
+          if (!value.equals(replacement.toString())) {
+            if (errors == null) {
+              return true;
+            }
+            result = true;
+            CheckErrorResult errorResult = createCheckErrorResult(
+                analysis.getPage(),
+                defaultSort.getBeginIndex(), defaultSort.getEndIndex());
+            errorResult.addReplacement(PageElementFunction.createFunction(
+                defaultSort.getFunctionName(),
+                replacement.toString()));
+            errors.add(errorResult);
+          }
+        }
+      }
+    }
+
+    return result;
   }
 }
