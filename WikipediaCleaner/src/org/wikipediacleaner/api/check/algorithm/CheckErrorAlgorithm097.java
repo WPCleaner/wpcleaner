a4ab944d90ceba772043bdb04617a48717ba9850
nicove <WPCleaner@users.noreply.github.com>
2014-02-15 15:58:33 +0000

    Table of Contents

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm097.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm097.java
@@ -7,14 +7,77 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElement;
+import org.wikipediacleaner.api.data.PageElementTitle;
+
 
 /**
  * Algorithm for analyzing error 97 of check wikipedia project.
  * Error 97: Material between TOC and first headline
  */
-public class CheckErrorAlgorithm097 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm097 extends CheckErrorAlgorithmTOC {
 
   public CheckErrorAlgorithm097() {
     super("Material between TOC and first headline");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (analysis == null) {
+      return false;
+    }
+
+    // Initialization
+    List<PageElementTitle> titles = analysis.getTitles();
+    int maxIndex = analysis.getContents().length();
+    if ((titles != null) && (!titles.isEmpty())) {
+      maxIndex = titles.get(0).getBeginIndex();
+    }
+    List<PageElement> tocs = getToCs(analysis, 0, maxIndex);
+    if ((tocs == null) || (tocs.isEmpty())) {
+      return false;
+    }
+
+    // Check every table of contents
+    boolean result = false;
+    String contents = analysis.getContents();
+    for (PageElement toc : tocs) {
+      if (toc.getBeginIndex() < maxIndex) {
+        boolean extraChar = false;
+        int index = toc.getEndIndex();
+        while (index < maxIndex) {
+          if (!Character.isWhitespace(contents.charAt(index))) {
+            extraChar = true;
+          }
+          index++;
+        }
+        if (extraChar) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              analysis.getPage(),
+              toc.getBeginIndex(), toc.getEndIndex());
+          errors.add(errorResult);
+        }
+      }
+    }
+
+    return result;
   }
 }
