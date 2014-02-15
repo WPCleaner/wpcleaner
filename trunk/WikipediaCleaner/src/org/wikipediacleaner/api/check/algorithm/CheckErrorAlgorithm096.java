d6e2ef40c936be4eb295a4fe996fd98f68f1314a
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-02-15 15:58:33 +0000

    Table of Contents

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm096.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm096.java
@@ -7,14 +7,66 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElement;
+import org.wikipediacleaner.api.data.PageElementTitle;
+
 
 /**
  * Algorithm for analyzing error 96 of check wikipedia project.
  * Error 96: TOC after first headline
  */
-public class CheckErrorAlgorithm096 extends CheckErrorAlgorithmUnavailable {
+public class CheckErrorAlgorithm096 extends CheckErrorAlgorithmTOC {
 
   public CheckErrorAlgorithm096() {
     super("TOC after first headline");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (analysis == null) {
+      return false;
+    }
+
+    // Initialization
+    List<PageElementTitle> titles = analysis.getTitles();
+    if ((titles == null) || (titles.isEmpty())) {
+      return false;
+    }
+    PageElementTitle firstTitle = titles.get(0);
+    List<PageElement> tocs = getToCs(analysis, firstTitle.getEndIndex(), Integer.MAX_VALUE);
+    if ((tocs == null) || (tocs.isEmpty())) {
+      return false;
+    }
+
+    // Check every table of contents
+    boolean result = false;
+    for (PageElement toc : tocs) {
+      if (toc.getBeginIndex() >= firstTitle.getEndIndex()) {
+        if (errors == null) {
+          return true;
+        }
+        result = true;
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis.getPage(),
+            toc.getBeginIndex(), toc.getEndIndex());
+        errors.add(errorResult);
+      }
+    }
+
+    return result;
   }
 }
---
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithmTOC.java
@@ -0,0 +1,104 @@
+/*
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2013  Nicolas Vervelle
+ *
+ *  See README.txt file for licensing information.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.data.MagicWord;
+import org.wikipediacleaner.api.data.Page;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElement;
+import org.wikipediacleaner.api.data.PageElementComparator;
+import org.wikipediacleaner.api.data.PageElementMagicWord;
+import org.wikipediacleaner.api.data.PageElementTemplate;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Base class for errors on table of contents.
+ */
+public abstract class CheckErrorAlgorithmTOC extends CheckErrorAlgorithmBase {
+
+  /**
+   * @param name Algorithm name.
+   */
+  public CheckErrorAlgorithmTOC(String name) {
+    super(name);
+  }
+
+  /**
+   * @param analysis Page analysis.
+   * @param beginIndex Begin index.
+   * @param endIndex End index.
+   * @return List of elements creating a table of contents.
+   */
+  protected List<PageElement> getToCs(
+      PageAnalysis analysis, int beginIndex, int endIndex) {
+    List<PageElement> result = new ArrayList<PageElement>();
+
+    // Magic words
+    List<PageElementMagicWord> magicWords = analysis.getMagicWords();
+    if (magicWords != null) {
+      for (PageElementMagicWord magicWord : magicWords) {
+        if ((magicWord.getBeginIndex() >= beginIndex) &&
+            (magicWord.getEndIndex() <= endIndex) &&
+            MagicWord.TOC.equals(magicWord.getMagicWord().getName())) {
+          result.add(magicWord);
+        }
+      }
+    }
+
+    // Templates
+    String templatesProp = getSpecificProperty("templates", true, true, false);
+    if (templatesProp != null) {
+      List<String> tmpTocTemplates = WPCConfiguration.convertPropertyToStringList(templatesProp);
+      if (tmpTocTemplates != null) {
+        List<String> tocTemplates = new ArrayList<String>();
+        for (String tocTemplate : tmpTocTemplates) {
+          tocTemplate = tocTemplate.toLowerCase();
+          tocTemplate = tocTemplate.replaceAll("\\[ \\]\\+", " ");
+          tocTemplates.add(tocTemplate);
+        }
+        List<PageElementTemplate> templates = analysis.getTemplates();
+        for (PageElementTemplate template : templates) {
+          if ((template.getBeginIndex() >= beginIndex) &&
+              (template.getEndIndex() <= endIndex)) {
+            boolean shouldAdd = false;
+            String templateName = template.getTemplateName().toLowerCase();
+            for (String tocTemplate : tocTemplates) {
+              if (Page.areSameTitle(templateName, tocTemplate)) {
+                shouldAdd = true;
+              }
+            }
+            if (shouldAdd) {
+              result.add(template);
+            }
+          }
+        }
+      }
+    }
+
+    Collections.sort(result, new PageElementComparator());
+    return result;
+  }
+
+  /**
+   * @return Map of parameters (Name -> description).
+   * @see org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase#getParameters()
+   */
+  @Override
+  public Map<String, String> getParameters() {
+    Map<String, String> parameters = super.getParameters();
+    parameters.put("templates", GT._("A list of templates resulting in the inclusion of a table of contents"));
+    return parameters;
+  }
+}
