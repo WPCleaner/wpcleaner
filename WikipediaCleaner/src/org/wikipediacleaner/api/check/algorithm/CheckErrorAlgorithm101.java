5fe1ce2dee01c3d68bcad7320fddeeec9c62be25
nicove <WPCleaner@users.noreply.github.com>
2014-04-23 21:41:43 +0000

    Error #100 and #101

---
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm101.java
@@ -0,0 +1,109 @@
+/*
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2013  Nicolas Vervelle
+ *
+ *  See README.txt file for licensing information.
+ */
+
+package org.wikipediacleaner.api.check.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementTag;
+import org.wikipediacleaner.i18n.GT;
+
+
+/**
+ * Algorithm for analyzing error 101 of check wikipedia project.
+ * Error 101: Ordinal numbers found inside &lt;sup&gt; tags
+ */
+public class CheckErrorAlgorithm101 extends CheckErrorAlgorithmBase {
+
+  public CheckErrorAlgorithm101() {
+    super("Ordinal numbers found inside <sup> tags");
+  }
+
+  /**
+   * Analyze a page to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param onlyAutomatic True if analysis could be restricted to errors automatically fixed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyze(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors, boolean onlyAutomatic) {
+    if (analysis == null) {
+      return false;
+    }
+
+    // Retrieve configuration
+    String suffixes = getSpecificProperty("templates", true, true, false);
+    List<String> listSuffixes = null;
+    if (suffixes != null) {
+      listSuffixes = WPCConfiguration.convertPropertyToStringList(suffixes);
+    } else {
+      listSuffixes = new ArrayList<String>();
+    }
+    listSuffixes.add("rd");
+    listSuffixes.add("th");
+    listSuffixes.add("st");
+
+    // Check every <sup> tag
+    List<PageElementTag> supTags = analysis.getCompleteTags(PageElementTag.TAG_HTML_SUP);
+    String contents = analysis.getContents();
+    boolean result = false;
+    for (PageElementTag supTag : supTags) {
+      if (supTag.isComplete() && !supTag.isFullTag()) {
+
+        // Check if digit is before supTag
+        boolean digitBefore = false;
+        int beginIndex = supTag.getBeginIndex();
+        if ((beginIndex > 0) &&
+            Character.isDigit(contents.charAt(beginIndex - 1))) {
+          digitBefore = true;
+        }
+
+        // Check if tag content is ordinal
+        boolean ordinal = false;
+        if (digitBefore) {
+          String value = contents.substring(supTag.getValueBeginIndex(), supTag.getValueEndIndex()).trim();
+          if (listSuffixes.contains(value)) {
+            ordinal = true;
+          }
+        }
+
+        // Report error
+        if (digitBefore && ordinal) {
+          if (errors == null) {
+            return true;
+          }
+          result = true;
+          CheckErrorResult errorResult = createCheckErrorResult(
+              analysis,
+              beginIndex, supTag.getCompleteEndIndex());
+          errors.add(errorResult);
+        }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * @return Map of parameters (Name -> description).
+   * @see org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase#getParameters()
+   */
+  @Override
+  public Map<String, String> getParameters() {
+    Map<String, String> parameters = super.getParameters();
+    parameters.put("templates", GT._("List of ordinal suffixes"));
+    return parameters;
+  }
+}
