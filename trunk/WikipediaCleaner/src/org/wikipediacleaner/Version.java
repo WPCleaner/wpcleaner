1b79f0f88a0d35ecd42cbf2d66a54777f0ba4245
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-04-26 22:58:36 +0000

    Update warnings

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/Version.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/Version.java
@@ -19,8 +19,8 @@ import org.wikipediacleaner.i18n.GT;
  */
 public final class Version {
 
-  public final static String VERSION = "1.31";
-  public final static Date   DATE = new GregorianCalendar(2014, Calendar.JANUARY, 22).getTime();
+  public final static String VERSION = "1.32";
+  public final static Date   DATE = new GregorianCalendar(2014, Calendar.APRIL, 27).getTime();
 
   public final static String PROGRAM = "WPCleaner";
 
