38d2b764a8a496a1b9eaa2117aa599758dff2379
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-05-16 04:50:28 +0000

    Error #11: HTML named entities Ignore Greek letters that look like Latin letters

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm011.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm011.java
@@ -41,7 +41,7 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_A_GRAVE_ACCENT);         // &agrave;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_A_GRAVE_ACCENT);       // &Agrave;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_ALPHA);                  // &alpha;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ALPHA);                // &Alpha;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ALPHA);                // &Alpha;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_A_RING);                 // &aring;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_A_RING);               // &Aring;
     htmlCharacters.add(HtmlCharacters.SYMBOL_ALMOST_EQUAL);                 // &asymp;
@@ -50,18 +50,18 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_A_UMLAUT_MARK);          // &auml;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_A_UMLAUT_MARK);        // &Auml;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_BETA);                   // &beta;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_BETA);                 // &Beta;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_BETA);                 // &Beta;
     htmlCharacters.add(HtmlCharacters.SYMBOL_BROKEN_VERTICAL_BAR);          // &brvbar;
     htmlCharacters.add(HtmlCharacters.SYMBOL_BULLET);                       // &bull;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_C_CEDILLA);              // &ccedil;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_C_CEDILLA);            // &Ccedil;
     htmlCharacters.add(HtmlCharacters.SYMBOL_CENT);                         // &cent;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_CHI);                    // &chi;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_CHI);                  // &Chi;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_CHI);                  // &Chi;
     htmlCharacters.add(HtmlCharacters.SYMBOL_CLUB);                         // &clubs;
     htmlCharacters.add(HtmlCharacters.SYMBOL_COPYRIGHT);                    // &copy;
     htmlCharacters.add(HtmlCharacters.SYMBOL_CARRIAGE_RETURN_ARROW);        // &crarr;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_DAGGER);                       // &dagger;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_DAGGER);                       // &dagger;
     htmlCharacters.add(HtmlCharacters.SYMBOL_DOUBLE_DAGGER);                // &Dagger;
     htmlCharacters.add(HtmlCharacters.SYMBOL_DOWN_ARROW);                   // &darr;
     htmlCharacters.add(HtmlCharacters.SYMBOL_DOWN_DOUBLE_ARROW);            // &dArr;
@@ -77,10 +77,10 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_E_GRAVE_ACCENT);         // &egrave;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_E_GRAVE_ACCENT);       // &Egrave;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_EPSILON);                // &epsilon;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_EPSILON);              // &Epsilon;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_EPSILON);              // &Epsilon;
     htmlCharacters.add(HtmlCharacters.SYMBOL_EQUIVALENT);                   // &equiv;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_ETA);                    // &eta;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ETA);                  // &Eta;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ETA);                  // &Eta;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_ETH);                    // &eth;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ETH);                  // &ETH;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_E_UMLAUT_MARK);          // &euml;
@@ -113,7 +113,7 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_I_UMLAUT_MARK);          // &iuml;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_I_UMLAUT_MARK);        // &Iuml;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_KAPPA);                  // &kappa;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_KAPPA);                // &Kappa;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_KAPPA);                // &Kappa;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_LAMBDA);                 // &lambda;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_LAMBDA);               // &Lambda;
     htmlCharacters.add(HtmlCharacters.SYMBOL_LEFT_ANGLE_QUOTATION_MARK);    // &laquo;
@@ -124,19 +124,19 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.SYMBOL_LOZENGE);                      // &loz;
     htmlCharacters.add(HtmlCharacters.SYMBOL_SINGLE_LEFT_ANGLE_QUOTATION);  // &lsaquo;
     htmlCharacters.add(HtmlCharacters.SYMBOL_LEFT_SINGLE_QUOTATION_MARK);   // &lsquo;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_EM_DASH);                      // &mdash;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_EM_DASH);                      // &mdash;
     htmlCharacters.add(HtmlCharacters.SYMBOL_MICRO);                        // &micro;
     htmlCharacters.add(HtmlCharacters.SYMBOL_MIDDLE_DOT);                   // &middot;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_MINUS);                        // &minus;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_MINUS);                        // &minus;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_MU);                     // &mu;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_MU);                   // &Mu;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_EN_DASH);                      // &ndash;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_MU);                   // &Mu;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_EN_DASH);                      // &ndash;
     htmlCharacters.add(HtmlCharacters.SYMBOL_NOT_EQUAL);                    // &ne;
     htmlCharacters.add(HtmlCharacters.SYMBOL_NEGATION);                     // &not;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_N_TILDE);                // &ntilde;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_N_TILDE);              // &Ntilde;
-    htmlCharacters.add(HtmlCharacters.LETTER_SMALL_NU);                     // &nu;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_NU);                   // &Nu;
+    // htmlCharacters.add(HtmlCharacters.LETTER_SMALL_NU);                     // &nu;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_NU);                   // &Nu;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_O_ACUTE_ACCENT);         // &oacute;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_O_ACUTE_ACCENT);       // &Oacute;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_O_CIRCUMFLEX_ACCENT);    // &ocirc;
@@ -148,8 +148,8 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.SYMBOL_OVERLINE);                     // &oline;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_OMEGA);                  // &omega;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_OMEGA);                // &Omega;
-    htmlCharacters.add(HtmlCharacters.LETTER_SMALL_OMICRON);                // &omicron;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_OMICRON);              // &Omicron;    
+    // htmlCharacters.add(HtmlCharacters.LETTER_SMALL_OMICRON);                // &omicron;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_OMICRON);              // &Omicron;    
     htmlCharacters.add(HtmlCharacters.SYMBOL_FEMININE_ORDINAL_INDICATOR);   // &ordf;
     htmlCharacters.add(HtmlCharacters.SYMBOL_MASCULINE_ORDINAL_INDICATOR);  // &ordm;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_O_SLASH);                // &oslash;
@@ -168,8 +168,8 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_PI_SYMBOL);                    // &piv;
     htmlCharacters.add(HtmlCharacters.SYMBOL_PLUS_OR_MINUS);                // &plusm;
     htmlCharacters.add(HtmlCharacters.SYMBOL_POUND);                        // &pound;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_MINUTES);                      // &prime;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_SECONDS);                      // &Prime;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_MINUTES);                      // &prime;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_SECONDS);                      // &Prime;
     htmlCharacters.add(HtmlCharacters.SYMBOL_PROD);                         // &prod;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_PSI);                    // &psi;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_PSI);                  // &Psi;
@@ -181,7 +181,7 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.SYMBOL_RIGHT_DOUBLE_QUOTATION_MARK);  // &rdquo;
     htmlCharacters.add(HtmlCharacters.SYMBOL_REGISTERED_TRADEMARK);         // &reg;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_RHO);                    // &rho;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_RHO);                  // &Rho;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_RHO);                  // &Rho;
     htmlCharacters.add(HtmlCharacters.SYMBOL_RIGHT_ANGLE_QUOTATION_MARK);   // &raquo;
     htmlCharacters.add(HtmlCharacters.SYMBOL_SINGLE_RIGHT_ANGLE_QUOTATION); // &rsaquo;
     htmlCharacters.add(HtmlCharacters.SYMBOL_RIGHT_SINGLE_QUOTATION_MARK);  // &rsquo;
@@ -198,14 +198,14 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.SYMBOL_SUPERSCRIPT_3);                // &sup3;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_SHARP_S);                // &szlig;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_TAU);                    // &tau;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_TAU);                  // &Tau;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_TAU);                  // &Tau;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_THETA);                  // &theta;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_THETA);                // &Theta;
     htmlCharacters.add(HtmlCharacters.LETTER_THETA_SYMBOL);                 // &thetasym;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_THORN);                  // &thorn;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_THORN);                // &THORN;
     htmlCharacters.add(HtmlCharacters.SYMBOL_SMALL_TILDE);                  // &tilde;
-    //htmlCharacters.add(HtmlCharacters.SYMBOL_MULTIPLICATION);               // &times;
+    // htmlCharacters.add(HtmlCharacters.SYMBOL_MULTIPLICATION);               // &times;
     htmlCharacters.add(HtmlCharacters.SYMBOL_TRADEMARK);                    // &trade;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_U_ACUTE_ACCENT);         // &uacute;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_U_ACUTE_ACCENT);       // &Uacute;
@@ -216,8 +216,8 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_U_GRAVE_ACCENT);         // &ugrave;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_U_GRAVE_ACCENT);       // &Ugrave;
     htmlCharacters.add(HtmlCharacters.LETTER_UPSILON_SYMBOL);               // &upsih;
-    htmlCharacters.add(HtmlCharacters.LETTER_SMALL_UPSILON);                // &upsilon;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_UPSILON);              // &Upsilon;
+    // htmlCharacters.add(HtmlCharacters.LETTER_SMALL_UPSILON);                // &upsilon;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_UPSILON);              // &Upsilon;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_U_UMLAUT_MARK);          // &uuml;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_U_UMLAUT_MARK);        // &Uuml;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_XI);                     // &xi;
@@ -228,7 +228,7 @@ public class CheckErrorAlgorithm011 extends CheckErrorAlgorithmHtmlNamedEntities
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_Y_UMLAUT_MARK);          // &yuml;
     htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_Y_UMLAUT_MARK);        // &Yuml;
     htmlCharacters.add(HtmlCharacters.LETTER_SMALL_ZETA);                   // &zeta;
-    htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ZETA);                 // &Zeta;
+    // htmlCharacters.add(HtmlCharacters.LETTER_CAPITAL_ZETA);                 // &Zeta;
   }
 
   /**
