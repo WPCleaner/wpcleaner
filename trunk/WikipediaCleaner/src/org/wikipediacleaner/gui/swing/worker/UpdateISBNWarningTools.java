b7b7ac4ce0002dbe490b377a1b8eb8e8da743df2
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-05-16 14:40:41 +0000

    Update warnings

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/gui/swing/worker/UpdateISBNWarningTools.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/gui/swing/worker/UpdateISBNWarningTools.java
@@ -140,7 +140,7 @@ public class UpdateISBNWarningTools extends UpdateWarningTools {
       }
       String error = analysis.getContents().substring(beginIndex, endIndex);
       error = error.replaceAll("\\=", "&#x3D;"); // Replace "=" by its HTML value
-      error = error.replaceAll("\\n", "\u21b5"); // Replacer \n by a visual character
+      error = error.replaceAll("\n", "\u21b5"); // Replacer \n by a visual character
       boolean keep = true;
       StringBuilder comment = new StringBuilder();
       while (pos < next) {
