eb2dfba5c9d39d11883d3b2e076e1320a4f89d31
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-08-31 16:06:22 +0000

    Error #59: Template value end with break

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm059.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm059.java
@@ -9,9 +9,12 @@ package org.wikipediacleaner.api.check.algorithm;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.CheckErrorResult.ErrorLevel;
+import org.wikipediacleaner.api.constants.WPCConfiguration;
+import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementComment;
 import org.wikipediacleaner.api.data.PageElementTag;
@@ -59,73 +62,94 @@ public class CheckErrorAlgorithm059 extends CheckErrorAlgorithmBase {
       return false;
     }
 
+    // List of templates to be ignored
+    List<String> ignoredTemplates = null;
+    String tmp = getSpecificProperty("templates", true, true, false);
+    if (tmp != null) {
+      ignoredTemplates = WPCConfiguration.convertPropertyToStringList(tmp);
+    }
+
     // Analyzing each template
     boolean result = false;
     for (PageElementTemplate template : analysis.getTemplates()) {
-      for (int paramNum = 0; paramNum < template.getParameterCount(); paramNum++) {
 
-        // Search for <br> at the end of the parameter
-        Parameter param = template.getParameter(paramNum);
-        String paramValue = param.getValue();
-        int paramValueStartIndex = param.getValueStartIndex();
-        boolean breakFound = false;
-        boolean tagAfter = false;
-        int currentValuePos = paramValue.length() - 1;
-        int beginError = -1;
-        int endError = -1;
-        boolean shouldStop = false;
-        String replacement = "";
-        while (!shouldStop) {
-          shouldStop = true;
-          currentValuePos = getLastIndexBeforeWhiteSpace(paramValue, currentValuePos);
-          if ((currentValuePos > 0) &&
-              (paramValue.charAt(currentValuePos) == '>')) {
-            PageElementTag tag = analysis.isInTag(
-                paramValueStartIndex +
-                currentValuePos);
-            if (tag != null) {
-              String name = tag.getNormalizedName();
-              if (PageElementTag.TAG_HTML_BR.equals(name)) {
-                breakFound = true;
-                shouldStop = false;
-                beginError = tag.getBeginIndex();
-                if (endError < 0) {
-                  endError = tag.getEndIndex();
-                }
-                currentValuePos -= tag.getEndIndex() - tag.getBeginIndex();
-              } else if (!breakFound) {
-                if (/*PageElementTag.TAG_WIKI_MATH.equals(name) ||*/
-                    PageElementTag.TAG_WIKI_HIERO.equals(name)) {
-                  tagAfter = true;
+      // Check if template should be analyzed
+      boolean analyzeTemplate = true;
+      if (ignoredTemplates != null) {
+        for (String ignoredTemplate : ignoredTemplates) {
+          if (Page.areSameTitle(ignoredTemplate, template.getTemplateName())) {
+            analyzeTemplate = false;
+          }
+        }
+      }
+
+      // Analyze template
+      if (analyzeTemplate) {
+        for (int paramNum = 0; paramNum < template.getParameterCount(); paramNum++) {
+  
+          // Search for <br> at the end of the parameter
+          Parameter param = template.getParameter(paramNum);
+          String paramValue = param.getValue();
+          int paramValueStartIndex = param.getValueStartIndex();
+          boolean breakFound = false;
+          boolean tagAfter = false;
+          int currentValuePos = paramValue.length() - 1;
+          int beginError = -1;
+          int endError = -1;
+          boolean shouldStop = false;
+          String replacement = "";
+          while (!shouldStop) {
+            shouldStop = true;
+            currentValuePos = getLastIndexBeforeWhiteSpace(paramValue, currentValuePos);
+            if ((currentValuePos > 0) &&
+                (paramValue.charAt(currentValuePos) == '>')) {
+              PageElementTag tag = analysis.isInTag(
+                  paramValueStartIndex +
+                  currentValuePos);
+              if (tag != null) {
+                String name = tag.getNormalizedName();
+                if (PageElementTag.TAG_HTML_BR.equals(name)) {
+                  breakFound = true;
                   shouldStop = false;
-                  endError = tag.getCompleteBeginIndex();
-                  currentValuePos -= tag.getEndIndex() - tag.getCompleteBeginIndex();
+                  beginError = tag.getBeginIndex();
+                  if (endError < 0) {
+                    endError = tag.getEndIndex();
+                  }
+                  currentValuePos -= tag.getEndIndex() - tag.getBeginIndex();
+                } else if (!breakFound) {
+                  if (/*PageElementTag.TAG_WIKI_MATH.equals(name) ||*/
+                      PageElementTag.TAG_WIKI_HIERO.equals(name)) {
+                    tagAfter = true;
+                    shouldStop = false;
+                    endError = tag.getCompleteBeginIndex();
+                    currentValuePos -= tag.getEndIndex() - tag.getCompleteBeginIndex();
+                  }
                 }
-              }
-            } else {
-              PageElementComment comment = analysis.isInComment(paramValueStartIndex + currentValuePos);
-              if (comment != null) {
-                replacement += analysis.getContents().substring(comment.getBeginIndex(), comment.getEndIndex());
-                shouldStop = false;
-                currentValuePos -= comment.getEndIndex() - comment.getBeginIndex();
+              } else {
+                PageElementComment comment = analysis.isInComment(paramValueStartIndex + currentValuePos);
+                if (comment != null) {
+                  replacement += analysis.getContents().substring(comment.getBeginIndex(), comment.getEndIndex());
+                  shouldStop = false;
+                  currentValuePos -= comment.getEndIndex() - comment.getBeginIndex();
+                }
               }
             }
           }
-        }
-
-        // Report error
-        if (breakFound) {
-          if (errors == null) {
-            return true;
+  
+          // Report error
+          if (breakFound) {
+            if (errors == null) {
+              return true;
+            }
+            result = true;
+            CheckErrorResult errorResult = createCheckErrorResult(
+                analysis, beginError, endError,
+                (tagAfter ? ErrorLevel.WARNING : ErrorLevel.ERROR));
+            if (!tagAfter) {
+              errorResult.addReplacement(replacement);
+            }
+            errors.add(errorResult);
           }
-          result = true;
-          CheckErrorResult errorResult = createCheckErrorResult(
-              analysis, beginError, endError,
-              (tagAfter ? ErrorLevel.WARNING : ErrorLevel.ERROR));
-          if (!tagAfter) {
-            errorResult.addReplacement(replacement);
-          }
-          errors.add(errorResult);
         }
       }
     }
@@ -134,16 +158,18 @@ public class CheckErrorAlgorithm059 extends CheckErrorAlgorithmBase {
   }
 
   /**
-   * Bot fixing of all the errors in the page.
+   * Return the parameters used to configure the algorithm.
    * 
-   * @param analysis Page analysis.
-   * @return Page contents after fix.
+   * @return Map of parameters (Name -> description).
    */
-  // No Bot fixing
-  // @Override
-  //public String botFix(PageAnalysis analysis) {
-  //  return fix(globalFixes[0], analysis, null);
-  //}
+  @Override
+  public Map<String, String> getParameters() {
+    Map<String, String> parameters = super.getParameters();
+    parameters.put(
+        "templates",
+        GT._("A list of that should be ignored"));
+    return parameters;
+  }
 
   /**
    * @return List of possible global fixes.
