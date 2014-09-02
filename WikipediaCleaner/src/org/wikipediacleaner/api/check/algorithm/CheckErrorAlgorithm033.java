a45e4516cb466c8c2c84060a032251da215bee17
nicove <WPCleaner@users.noreply.github.com>
2014-09-02 19:27:09 +0000

    Error #33: HTML text style element <u>

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm033.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm033.java
@@ -8,10 +8,14 @@
 package org.wikipediacleaner.api.check.algorithm;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.constants.WPCConfiguration;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementTag;
+import org.wikipediacleaner.i18n.GT;
 
 
 /**
@@ -38,8 +42,76 @@ public class CheckErrorAlgorithm033 extends CheckErrorAlgorithmBase {
     if (analysis == null) {
       return false;
     }
+
+    // List of templates for replacement
+    List<String> replacements = null;
+    String tmp = getSpecificProperty("templates", true, true, false);
+    if (tmp != null) {
+      replacements = WPCConfiguration.convertPropertyToStringList(tmp);
+    }
+
+    // Analyze each tag
     boolean result = false;
-    result = addTags(result, analysis, errors, PageElementTag.TAG_HTML_U);
+    Collection<PageElementTag> tags = analysis.getTags(PageElementTag.TAG_HTML_U);
+    String contents = analysis.getContents();
+    for (PageElementTag tag : tags) {
+      boolean shouldCount = true;
+
+      // Only take the first tag in a pair
+      if (shouldCount) {
+        if (!tag.isFullTag() && tag.isEndTag() && tag.isComplete()) {
+          shouldCount = false;
+        }
+      }
+
+      // Ignore tags inside nowiki
+      if (shouldCount) {
+        if (analysis.getSurroundingTag(
+            PageElementTag.TAG_WIKI_NOWIKI,
+            tag.getBeginIndex()) != null) {
+          shouldCount = false;
+        }
+      }
+
+      // Report error
+      if (shouldCount) {
+        if (errors == null) {
+          return true;
+        }
+        result = true;
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis,
+            tag.getCompleteBeginIndex(), tag.getCompleteEndIndex());
+        if (tag.isFullTag()) {
+          errorResult.addReplacement("");
+        } else if (tag.isComplete()) {
+          String value = contents.substring(tag.getValueBeginIndex(), tag.getValueEndIndex());
+          errorResult.addReplacement(value);
+          if (replacements != null) {
+            for (String replacement : replacements) {
+              errorResult.addReplacement("{{" + replacement + "|" + value + "}}");
+            }
+          }
+          errorResult.addReplacement("''" + value + "''");
+          errorResult.addReplacement("'''" + value + "'''");
+        }
+        errors.add(errorResult);
+      }
+    }
     return result;
+  }
+
+  /**
+   * Return the parameters used to configure the algorithm.
+   * 
+   * @return Map of parameters (Name -> description).
+   */
+  @Override
+  public Map<String, String> getParameters() {
+    Map<String, String> parameters = super.getParameters();
+    parameters.put(
+        "templates",
+        GT._("A list of templates that can be used to replace <u> tags"));
+    return parameters;
   }
 }
