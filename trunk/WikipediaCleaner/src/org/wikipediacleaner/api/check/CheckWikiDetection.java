e468e3392fe9482e6250f71214b33d5c5085ed25
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-09-09 17:50:16 +0000

    CheckWiki

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/CheckWikiDetection.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/check/CheckWikiDetection.java
@@ -8,11 +8,19 @@
 
 package org.wikipediacleaner.api.check;
 
+import org.wikipediacleaner.api.data.PageAnalysis;
+import org.wikipediacleaner.api.data.PageElementComment;
+
 
 /**
  * Bean for holding information about problems detected by Check Wiki.
  */
 public class CheckWikiDetection {
+
+  /**
+   * Method to interpret the location.
+   */
+  private final char locationMethod;
 
   /**
    * Error number (algorithm).
@@ -30,11 +38,15 @@ public class CheckWikiDetection {
   private final String detection;
 
   /**
+   * @param locationMethod Method to interprete the location.
    * @param errorNumber Error number (algorithm).
    * @param location Location of the error.
    * @param detection Text near the error.
    */
-  CheckWikiDetection(int errorNumber, int location, String detection) {
+  CheckWikiDetection(
+      char locationMethod, int errorNumber,
+      int location, String detection) {
+    this.locationMethod = locationMethod;
     this.errorNumber = errorNumber;
     this.location = location;
     this.detection = detection;
@@ -50,7 +62,32 @@ public class CheckWikiDetection {
   /**
    * @return Location of the error.
    */
-  public int getLocation() {
+  public int getLocation(PageAnalysis analysis) {
+    if (location < 0) {
+      return location;
+    }
+    if (locationMethod == '+') {
+      int currentIndex = 0;
+      int currentCount = 0;
+      String contents = analysis.getContents();
+      while ((currentIndex < contents.length()) &&
+             (currentCount < location)) {
+        boolean moved = false;
+        if (!moved) {
+          PageElementComment comment = analysis.isInComment(currentIndex);
+          if ((comment != null) && (comment.getBeginIndex() == currentIndex)) {
+            moved = true;
+            currentIndex = comment.getEndIndex();
+          }
+        }
+        // TODO: ignore other things
+        if (!moved) {
+          currentIndex++;
+          currentCount++;
+        }
+      }
+      return currentIndex;
+    }
     return location;
   }
 
