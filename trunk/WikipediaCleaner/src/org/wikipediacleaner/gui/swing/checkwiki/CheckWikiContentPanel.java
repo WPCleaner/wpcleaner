75213fad90a4e55f7f732095d40f8eb14c1c4bdd
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2014-09-12 20:14:49 +0000

    CheckWiki

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/gui/swing/checkwiki/CheckWikiContentPanel.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/gui/swing/checkwiki/CheckWikiContentPanel.java
@@ -52,6 +52,7 @@ import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.gui.swing.Controller;
 import org.wikipediacleaner.gui.swing.OnePageWindow;
+import org.wikipediacleaner.gui.swing.action.ActionCheckArticle;
 import org.wikipediacleaner.gui.swing.action.ActionExternalViewer;
 import org.wikipediacleaner.gui.swing.action.ActionFullAnalysis;
 import org.wikipediacleaner.gui.swing.action.ActionOccurrence;
@@ -194,6 +195,9 @@ public class CheckWikiContentPanel
     buttonMarkAsFixed.setActionCommand(ACTION_MARK_AS_FIXED);
     buttonMarkAsFixed.addActionListener(this);
     toolbarButtons.add(buttonMarkAsFixed);
+    ActionCheckArticle.addButton(
+        window.getParentComponent(), toolbarButtons,
+        getWiki(), page.getTitle(), true);
     toolbarButtons.addSeparator();
     if (Utilities.isDesktopSupported()) { // External Viewer
       ActionExternalViewer.addButton(
