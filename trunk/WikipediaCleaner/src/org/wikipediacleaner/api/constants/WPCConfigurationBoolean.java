e07697d13cf2f8164c124ce3de5231e690e23848
nicove <nicove@c0c10f81-528d-4954-a1a0-230323a19537>
2015-02-25 18:49:42 +0000

    Toolserver is down

--- trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WPCConfigurationBoolean.java
+++ trunk/WikipediaCleaner/src/org/wikipediacleaner/api/constants/WPCConfigurationBoolean.java
@@ -22,9 +22,9 @@ public enum WPCConfigurationBoolean {
   // Used to force the activation of Check Wiki project even without a project page.
   CW_FORCE("check_wiki_force", false, true, false),
   // Used to mark error fixed both on Tool Server and WMF Labs
-  CW_MARK_BOTH("check_wiki_mark_both", false, true, true),
+  // CW_MARK_BOTH("check_wiki_mark_both", false, true, true),
   // Used to decide if Check Wiki is on WMF Labs or on Tool Server.
-  CW_USE_LABS("check_wiki_use_labs", true, true, true),
+  // CW_USE_LABS("check_wiki_use_labs", true, true, true),
   // Allow use of __DISAMBIG__ magic word.
   DAB_USE_DISAMBIG_MAGIC_WORD("dab_use_disambig", true, true, false),
   // Allow disambiguation warning outside section 0 of talk page.
