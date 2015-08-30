8c1b63141c11d1d2fb04c2113d1eccb9ca102b73
Nicolas Vervelle <nicove@users.sourceforge.net>
2015-08-30 19:04:43 +0200

    Error 64: Link equal to link text

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm064.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm064.java
@@ -7,7 +7,9 @@
 
 package org.wikipediacleaner.api.check.algorithm;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.data.Page;
@@ -63,64 +65,92 @@ public class CheckErrorAlgorithm064 extends CheckErrorAlgorithmBase {
       String anchor = link.getAnchor();
       String linkName = link.getLink();
       String text = link.getText();
+      String cleanedText = (text != null) ? text.replaceAll("\\_", " ") : text;
       String paddingLeft = "";
-      String simplePaddingLeft = "";
       String paddingRight = "";
-      String simplePaddingRight = "";
       boolean same = false;
       boolean automatic = false;
       if (((anchor == null) || (anchor.trim().length() == 0)) &&
           (text != null)) {
-        if (!same && Page.areSameTitle(linkName, text)) {
-          same = true;
-          automatic = true;
+
+        // Check for exact same title
+        if (!same) {
+          if (Page.areSameTitle(linkName, text)) {
+            same = true;
+            automatic = true;
+          } else if (Page.areSameTitle(linkName, cleanedText)) {
+            same = true;
+          }
         }
-        if (!same && Page.areSameTitle(linkName, text.replaceAll("\\_", " "))) {
-          same = true;
-          automatic = false;
-        }
+
+        // Check for extra punctuation at the end
         if (!same) {
           int position = text.length();
           while ((position > 0) && (",.".indexOf(text.charAt(position - 1)) >= 0)) {
             position--;
           }
           if (position < text.length()) {
-            paddingRight = text.substring(position);
-            simplePaddingRight = paddingRight;
+            paddingRight = text.substring(position) + paddingRight;
             text = text.substring(0, position);
-            if (!same && Page.areSameTitle(linkName, text)) {
+            cleanedText = (text != null) ? text.replaceAll("\\_", " ") : text;
+            if (Page.areSameTitle(linkName, text)) {
               same = true;
               automatic = true;
-            }
-            if (!same && Page.areSameTitle(linkName, text.replaceAll("\\_", " "))) {
+            } else if (Page.areSameTitle(linkName, cleanedText)) {
               same = true;
-              automatic = false;
             }
           }
         }
-        if (!same) {
+
+        // Check for surrounding quotes
+        if (!same && (text != null)) {
           int countQuoteBefore = 0;
-          while ((countQuoteBefore < text.length()) && (text.charAt(countQuoteBefore) == '\'')) {
+          while ((countQuoteBefore < text.length()) &&
+                 (text.charAt(countQuoteBefore) == '\'')) {
             countQuoteBefore++;
           }
           int countQuoteAfter = 0;
-          while ((countQuoteAfter < text.length()) && (text.charAt(text.length() - countQuoteAfter - 1) == '\'')) {
+          while ((countQuoteAfter < text.length()) &&
+                 (text.charAt(text.length() - countQuoteAfter - 1) == '\'')) {
             countQuoteAfter++;
           }
-          if ((countQuoteBefore > 1) && (countQuoteAfter == countQuoteBefore) && (text.length() > countQuoteBefore)) {
+          if ((countQuoteBefore > 1) &&
+              (text.length() > countQuoteBefore)) {
             paddingLeft = paddingLeft + text.substring(0, countQuoteBefore);
             paddingRight = text.substring(text.length() - countQuoteAfter) + paddingRight;
             text = text.substring(countQuoteBefore, text.length() - countQuoteAfter);
-            if (Page.areSameTitle(linkName, text) || Page.areSameTitle(linkName, text.replaceAll("\\_", " "))) {
+            cleanedText = (text != null) ? text.replaceAll("\\_", " ") : text;
+            if (Page.areSameTitle(linkName, text)) {
               same = true;
-              automatic = true;
-              if ((link.getBeginIndex() > 0) && (content.charAt(link.getBeginIndex() - 1) == '\'')) {
-                automatic = false;
+              if ((link.getBeginIndex() <= 0) ||
+                  (content.charAt(link.getBeginIndex() - 1) != '\'')) {
+                if ((link.getEndIndex() >= content.length()) ||
+                    (content.charAt(link.getEndIndex()) != '\'')) {
+                  automatic = true;
+                }
               }
-              if ((link.getEndIndex() < content.length()) && (content.charAt(link.getEndIndex()) == '\'')) {
-                automatic = false;
-              }
+            } else if (Page.areSameTitle(linkName, cleanedText)) {
+              same = true;
             }
+          }
+        }
+      }
+
+      // Check for extra punctuation at the end (again, in case punctuation removed)
+      if (!same && (text != null)) {
+        int position = text.length();
+        while ((position > 0) && (",.".indexOf(text.charAt(position - 1)) >= 0)) {
+          position--;
+        }
+        if (position < text.length()) {
+          paddingRight = text.substring(position) + paddingRight;
+          text = text.substring(0, position);
+          cleanedText = (text != null) ? text.replaceAll("\\_", " ") : text;
+          if (Page.areSameTitle(linkName, text)) {
+            same = true;
+            automatic = true;
+          } else if (Page.areSameTitle(linkName, cleanedText)) {
+            same = true;
           }
         }
       }
@@ -131,26 +161,88 @@ public class CheckErrorAlgorithm064 extends CheckErrorAlgorithmBase {
           return true;
         }
         result = true;
-        String cleanedText = text.replaceAll("\\_", " ");
-        CheckErrorResult errorResult = createCheckErrorResult(
-            analysis,
-            link.getBeginIndex(),
-            link.getEndIndex());
-        errorResult.addReplacement(
-            paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight,
-            automatic);
-        if (!text.equals(cleanedText)) {
-          errorResult.addReplacement(
-              paddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + paddingRight,
-              false);
-        }
-        if (!simplePaddingLeft.equals(paddingLeft) || !simplePaddingRight.equals(paddingRight)) {
-          errorResult.addReplacement(
-              simplePaddingLeft + PageElementInternalLink.createInternalLink(text, null) + simplePaddingRight);
-          if (!text.equals(cleanedText)) {
-            errorResult.addReplacement(
-                simplePaddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + simplePaddingRight);
+
+        // Analyze for possible extra modifications
+        int beginIndex = link.getBeginIndex();
+        int endIndex = link.getEndIndex();
+        String extraRight = "";
+        String extraFullRight = "";
+        if ((endIndex < content.length()) &&
+            (content.charAt(endIndex) == '\'')) {
+          int countQuoteBefore = 0;
+          int tmpIndex = endIndex;
+          while ((tmpIndex < content.length() &&
+                 (content.charAt(tmpIndex) == '\''))) {
+            tmpIndex++;
+            countQuoteBefore++;
           }
+          while ((tmpIndex < content.length()) &&
+                 (" ,.".indexOf(content.charAt(tmpIndex)) >= 0)) {
+            tmpIndex++;
+          }
+          int countQuoteAfter = 0;
+          while ((tmpIndex < content.length() &&
+                 (content.charAt(tmpIndex) == '\''))) {
+            tmpIndex++;
+            countQuoteAfter++;
+          }
+          if (countQuoteBefore == countQuoteAfter) {
+            extraRight = content.substring(endIndex + countQuoteBefore, tmpIndex - countQuoteAfter);
+            extraFullRight = content.substring(endIndex, tmpIndex);
+            endIndex = tmpIndex;
+          }
+        }
+
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis, beginIndex, endIndex);
+        List<String> replacements = new ArrayList<>();
+        String replacement = null;
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight + extraRight;
+        if (!automatic && !replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight + extraFullRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement, automatic);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight + extraRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + paddingRight + extraFullRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + paddingRight + extraRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        paddingLeft = paddingLeft.replaceAll("\'", "");
+        paddingRight = paddingRight.replaceAll("\'", "");
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight + extraFullRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(text, null) + paddingRight + extraRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + paddingRight + extraFullRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
+        }
+        replacement = paddingLeft + PageElementInternalLink.createInternalLink(cleanedText, null) + paddingRight + extraRight;
+        if (!replacements.contains(replacement)) {
+          errorResult.addReplacement(replacement);
+          replacements.add(replacement);
         }
         errors.add(errorResult);
       }
