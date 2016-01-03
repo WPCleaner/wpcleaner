fe2f057d9a99e2444e6a6c32efc4c26ebfea2933
Nicolas Vervelle <nicove@users.sourceforge.net>
2016-01-03 13:15:19 +0100

    Version 1.38

--- WikipediaCleaner/src/org/wikipediacleaner/Version.java
+++ WikipediaCleaner/src/org/wikipediacleaner/Version.java
@@ -19,8 +19,8 @@ import org.wikipediacleaner.i18n.GT;
  */
 public final class Version {
 
-  public final static String VERSION = "1.37";
-  public final static Date   DATE = new GregorianCalendar(2015, Calendar.OCTOBER, 14).getTime();
+  public final static String VERSION = "1.38";
+  public final static Date   DATE = new GregorianCalendar(2016, Calendar.JANUARY, 3).getTime();
 
   public final static String PROGRAM = "WPCleaner";
 
