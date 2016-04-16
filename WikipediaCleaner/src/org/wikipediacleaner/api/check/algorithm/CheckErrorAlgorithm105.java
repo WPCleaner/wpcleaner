d3298b26c0cab4238ede1372a59ac412050c9847
Nicolas Vervelle <nicove@users.sourceforge.net>
2016-04-16 14:25:56 +0200

    Error 105: Headline should start with "=" Ignore inside {{#tag:timeline|...}}

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm105.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm105.java
@@ -10,8 +10,10 @@ package org.wikipediacleaner.api.check.algorithm;
 import java.util.Collection;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.data.MagicWord;
 import org.wikipediacleaner.api.data.PageAnalysis;
 import org.wikipediacleaner.api.data.PageElementExternalLink;
+import org.wikipediacleaner.api.data.PageElementFunction;
 import org.wikipediacleaner.api.data.PageElementTag;
 import org.wikipediacleaner.api.data.PageElementTemplate;
 import org.wikipediacleaner.api.data.PageElementTemplate.Parameter;
@@ -88,6 +90,54 @@ public class CheckErrorAlgorithm105 extends CheckErrorAlgorithmBase {
             (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_SYNTAXHIGHLIGHT, currentIndex) != null) ||
             (analysis.getSurroundingTag(PageElementTag.TAG_WIKI_TIMELINE, currentIndex) != null)) {
           errorFound = false;
+        }
+      }
+      if (errorFound) {
+        // Functions used instead of some tags
+        PageElementFunction function = analysis.isInFunction(currentIndex);
+        if ((function != null) &&
+            (function.getMagicWord() != null) &&
+            (MagicWord.TAG.equals(function.getMagicWord().getName()))) {
+          int functionIndex = function.getBeginIndex();
+          while ((functionIndex < contents.length()) &&
+                 (contents.charAt(functionIndex) == '{')) {
+            functionIndex++;
+          }
+          while ((functionIndex < contents.length()) &&
+                 (Character.isWhitespace(contents.charAt(functionIndex)))) {
+            functionIndex++;
+          }
+          if ((functionIndex < contents.length()) &&
+              (contents.startsWith(function.getFunctionName(), functionIndex))) {
+            functionIndex += function.getFunctionName().length();
+            while ((functionIndex < contents.length()) &&
+                   (Character.isWhitespace(contents.charAt(functionIndex)))) {
+              functionIndex++;
+            }
+            while ((functionIndex < contents.length()) &&
+                   (contents.charAt(functionIndex) == ':')) {
+              functionIndex++;
+            }
+            while ((functionIndex < contents.length()) &&
+                   (Character.isWhitespace(contents.charAt(functionIndex)))) {
+              functionIndex++;
+            }
+            int endIndex = -1;
+            int pipeIndex = contents.indexOf('|', functionIndex);
+            if ((pipeIndex > 0) && ((endIndex < 0) || (pipeIndex < endIndex))) {
+              endIndex = pipeIndex;
+            }
+            int curlyIndex = contents.indexOf('}', functionIndex);
+            if ((curlyIndex > 0) && ((endIndex < 0) || (curlyIndex < endIndex))) {
+              endIndex = curlyIndex;
+            }
+            if (endIndex > 0) {
+              String tagName = contents.substring(functionIndex, endIndex);
+              if (tagName.equalsIgnoreCase(PageElementTag.TAG_WIKI_TIMELINE)) {
+                errorFound = false;
+              }
+            }
+          }
         }
       }
       if (errorFound && (tmpIndex == currentIndex + 1)) {
