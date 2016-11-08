67daac87c6fb00542d24e0b23d6bd5761685c2ff
Nicolas Vervelle <nicove@users.sourceforge.net>
2016-11-08 21:07:34 +0100

    Error 64: Link equal to link text More extra characters

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm064.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/CheckErrorAlgorithm064.java
@@ -34,10 +34,10 @@ public class CheckErrorAlgorithm064 extends CheckErrorAlgorithmBase {
   };
 
   /** Possible quotes before text */
-  private final static String POSSIBLE_QUOTES_BEFORE = "«'`‘\"„“” .,(";
+  private final static String POSSIBLE_QUOTES_BEFORE = "«'`‘\"„“” .,(–־—";
 
   /** Possible quotes after text */
-  private final static String POSSIBLE_QUOTES_AFTER = "»'`‘\"„“” .,);:";
+  private final static String POSSIBLE_QUOTES_AFTER = "»'`‘\"„“” .,);:–־—";
 
   public CheckErrorAlgorithm064() {
     super("Link equal to linktext");
