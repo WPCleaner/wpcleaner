9d5ac10adc9e123aa65b1d9976711d8c2685c99a
Nicolas <Nicolas@NICO-ZEN3D>
2019-08-11 17:40:38 +0200

    Refactoring: creation of PanelWindow

---
+++ WikipediaCleaner/src/org/wikipediacleaner/gui/swing/basic/PanelWindow.java
@@ -0,0 +1,156 @@
+/*
+ *  WPCleaner: A tool to help on Wikipedia maintenance tasks.
+ *  Copyright (C) 2013  Nicolas Vervelle
+ *
+ *  See README.txt file for licensing information.
+ */
+
+package org.wikipediacleaner.gui.swing.basic;
+
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import org.wikipediacleaner.api.constants.EnumWikipedia;
+import org.wikipediacleaner.gui.swing.action.ActionDispose;
+import org.wikipediacleaner.gui.swing.basic.BasicWindow;
+import org.wikipediacleaner.gui.swing.basic.DefaultBasicWindowListener;
+
+
+/**
+ * A basic window displaying a JPanl and a close button. 
+ */
+public abstract class PanelWindow extends BasicWindow {
+
+  /** Title of the window */
+  private String title;
+
+  /** Panel to use inside the window */
+  private JPanel panel;
+
+  /** Close button */
+  private JButton buttonClose;
+
+  /**
+   * Create and display a PanelWindow.
+   * 
+   * @param name Window name.
+   * @param wiki Wiki.
+   * @param title Title of the window.
+   * @param panel JPanel to use inside the window.
+   */
+  protected static void createPanelWindow(
+      String name,
+      EnumWikipedia wiki,
+      Class<? extends PanelWindow> windowClass,
+      final String title,
+      final JPanel panel) {
+    createWindow(
+        name, wiki,
+        WindowConstants.DISPOSE_ON_CLOSE,
+        windowClass,
+        new DefaultBasicWindowListener() {
+          
+          @Override
+          public void initializeWindow(BasicWindow window) {
+            PanelWindow.initializeWindow(window, title, panel);
+          }
+        });
+  }
+
+  /**
+   * Initialize window.
+   * 
+   * @param window Window.
+   * @param title Title of the window.
+   * @param panel JPanel to use inside the window.
+   */
+  static void initializeWindow(
+      BasicWindow window,
+      String title,
+      JPanel panel) {
+    if (window instanceof PanelWindow) {
+      PanelWindow panelWindow = (PanelWindow) window;
+      panelWindow.setPanel(panel);
+      panelWindow.setTitle(title);
+    }
+  }
+
+  /**
+   * @param title Title of the window.
+   */
+  protected void setTitle(String title) {
+    this.title = title;
+  }
+
+  /* (non-Javadoc)
+   * @see org.wikipediacleaner.gui.swing.basic.BasicWindow#getTitle()
+   */
+  @Override
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * @param panel JPanel to use inside the window.
+   */
+  protected void setPanel(JPanel panel) {
+    this.panel = panel;
+  }
+
+  /**
+   * @return Window components.
+   */
+  @Override
+  protected Component createComponents() {
+    JPanel bigPanel = new JPanel(new GridBagLayout());
+
+    // Initialize constraints
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.gridheight = 1;
+    constraints.gridwidth = 1;
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    constraints.insets = new Insets(2, 2, 2, 2);
+    constraints.ipadx = 0;
+    constraints.ipady = 0;
+    constraints.weightx = 1;
+    constraints.weighty = 0;
+
+    // Panel
+    constraints.fill = GridBagConstraints.BOTH;
+    constraints.weightx = 1;
+    constraints.weighty = 1;
+    bigPanel.add(panel, constraints);
+    constraints.gridy++;
+
+    // Commands
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.weighty = 0;
+    bigPanel.add(createCommandComponents(), constraints);
+    constraints.gridy++;
+
+    return bigPanel;
+  }
+
+  /**
+   * @return Command components.
+   */
+  private Component createCommandComponents() {
+    JPanel commandPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    //panel.setBorder(BorderFactory.createEtchedBorder());
+
+    // Close button
+    buttonClose = ActionDispose.createButton(getParentComponent(), true, false);
+    commandPanel.add(buttonClose);
+
+    return commandPanel;
+  }
+}
--- WikipediaCleaner/src/org/wikipediacleaner/gui/swing/linter/LinterErrorWindow.java
+++ WikipediaCleaner/src/org/wikipediacleaner/gui/swing/linter/LinterErrorWindow.java
@@ -7,39 +7,21 @@
 
 package org.wikipediacleaner.gui.swing.linter;
 
-import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 import javax.swing.text.JTextComponent;
 
 import org.wikipediacleaner.api.constants.EnumWikipedia;
 import org.wikipediacleaner.api.linter.LinterError;
-import org.wikipediacleaner.gui.swing.action.ActionDispose;
-import org.wikipediacleaner.gui.swing.basic.BasicWindow;
-import org.wikipediacleaner.gui.swing.basic.DefaultBasicWindowListener;
+import org.wikipediacleaner.gui.swing.basic.PanelWindow;
 import org.wikipediacleaner.i18n.GT;
 
 
 /**
  * Linter errors. 
  */
-public class LinterErrorWindow extends BasicWindow {
-
-  /** Linter errors to be displayed */
-  private List<LinterError> errors;
-
-  /** Close button */
-  private JButton buttonClose;
-
-  /** Text pane */
-  private JTextComponent textPane;
+public class LinterErrorWindow extends PanelWindow {
 
   /**
    * Create and display an LinterErrorWindow.
@@ -52,105 +34,7 @@ public class LinterErrorWindow extends BasicWindow {
       EnumWikipedia wiki,
       final List<LinterError> errors,
       final JTextComponent textPane) {
-    createWindow(
-        "LinterWindow", wiki,
-        WindowConstants.DISPOSE_ON_CLOSE,
-        LinterErrorWindow.class,
-        new DefaultBasicWindowListener() {
-          
-          @Override
-          public void initializeWindow(BasicWindow window) {
-            LinterErrorWindow.initializeWindow(window, errors, textPane);
-          }
-        });
-  }
-
-  /**
-   * Initialize window.
-   * 
-   * @param window Window.
-   * @param errors List of Linter errors.
-   * @param textPane Text pane.
-   */
-  static void initializeWindow(
-      BasicWindow window,
-      List<LinterError> errors,
-      JTextComponent textPane) {
-    if (window instanceof LinterErrorWindow) {
-      LinterErrorWindow linterError = (LinterErrorWindow) window;
-      linterError.setErrors(errors);
-      linterError.setTextPane(textPane);
-    }
-  }
-
-  /* (non-Javadoc)
-   * @see org.wikipediacleaner.gui.swing.basic.BasicWindow#getTitle()
-   */
-  @Override
-  public String getTitle() {
-    return GT._T("Errors"); 
-  }
-
-  /**
-   * @param errors List of linter errors.
-   */
-  protected void setErrors(List<LinterError> errors) {
-    this.errors = errors;
-  }
-
-  /**
-   * @param textPane Text pane.
-   */
-  protected void setTextPane(JTextComponent textPane) {
-    this.textPane = textPane;
-  }
-
-  /**
-   * @return Window components.
-   */
-  @Override
-  protected Component createComponents() {
-    JPanel panel = new JPanel(new GridBagLayout());
-
-    // Initialize constraints
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.gridheight = 1;
-    constraints.gridwidth = 1;
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    constraints.insets = new Insets(2, 2, 2, 2);
-    constraints.ipadx = 0;
-    constraints.ipady = 0;
-    constraints.weightx = 1;
-    constraints.weighty = 0;
-
-    // About
-    constraints.fill = GridBagConstraints.BOTH;
-    constraints.weighty = 1;
-    panel.add(new LinterErrorPanel(getWiki(), errors, textPane), constraints);
-    constraints.gridy++;
-
-    // Commands
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.weighty = 0;
-    panel.add(createCommandComponents(), constraints);
-    constraints.gridy++;
-
-    return panel;
-  }
-
-  /**
-   * @return Command components.
-   */
-  private Component createCommandComponents() {
-    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    //panel.setBorder(BorderFactory.createEtchedBorder());
-
-    // Close button
-    buttonClose = ActionDispose.createButton(getParentComponent(), true, false);
-    panel.add(buttonClose);
-
-    return panel;
+    JPanel panel = new LinterErrorPanel(wiki, errors, textPane);
+    createPanelWindow("LinterWindow", wiki, LinterErrorWindow.class, GT._T("Errors"), panel);
   }
 }
