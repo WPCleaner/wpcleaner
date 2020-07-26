3920856e1f6441b5bee402ec110b9267c3dc5d51
Nicolas <Nicolas@NICO-ZEN3D>
2020-07-26 13:13:53 +0200

    T258642: Unit tests for parser

--- WikipediaCleaner/test/org/wikipediacleaner/api/data/analysis/PageAnalysisTest.java
+++ WikipediaCleaner/test/org/wikipediacleaner/api/data/analysis/PageAnalysisTest.java
@@ -14,7 +14,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -22,6 +25,8 @@ import org.wikipediacleaner.api.constants.EnumCaseSensitiveness;
 import org.wikipediacleaner.api.constants.EnumWikipedia;
 import org.wikipediacleaner.api.data.DataManager;
 import org.wikipediacleaner.api.data.Interwiki;
+import org.wikipediacleaner.api.data.Language;
+import org.wikipediacleaner.api.data.MagicWord;
 import org.wikipediacleaner.api.data.Namespace;
 import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageElementTag;
@@ -52,20 +57,20 @@ public class PageAnalysisTest {
     checkImages(analysis, 2);
     checkCategories(analysis, 2);
     checkInterwikiLinks(analysis, 2);
-    checkLanguageLinks(analysis, 0);
-    checkFunctions(analysis, 0);
-    checkMagicWords(analysis, 0);
-    checkTemplates(analysis, 0);
-    checkParameters(analysis, 0);
-    checkTitles(analysis, 0);
-    checkExternalLinks(analysis, 0);
-    checkISBN(analysis, 0);
-    checkISSN(analysis, 0);
-    checkPMID(analysis, 0);
-    checkRFC(analysis, 0);
-    checkTables(analysis, 0);
-    checkListItems(analysis, 0);
-    checkParagraphs(analysis, 8);
+    checkLanguageLinks(analysis, 1);
+    checkFunctions(analysis, 1);
+    checkMagicWords(analysis, 1);
+    checkTemplates(analysis, 3);
+    checkParameters(analysis, 2);
+    checkTitles(analysis, 2);
+    checkExternalLinks(analysis, 2);
+    checkISBN(analysis, 1);
+    checkISSN(analysis, 1);
+    checkPMID(analysis, 1);
+    checkRFC(analysis, 1);
+    checkTables(analysis, 1);
+    checkListItems(analysis, 3);
+    checkParagraphs(analysis, 17);
   }
 
   /**
@@ -111,20 +116,40 @@ public class PageAnalysisTest {
 
     // Configure wiki
     EnumWikipedia wiki = EnumWikipedia.EN;
+
     List<Namespace> namespaces = new ArrayList<>();
-    namespaces.add(new Namespace(
+    Namespace categoryNS = new Namespace(
         Integer.toString(Namespace.CATEGORY),
         "Category", "Category",
-        EnumCaseSensitiveness.FIRST_LETTER, true));
-    namespaces.add(new Namespace(
+        EnumCaseSensitiveness.FIRST_LETTER, true);
+    namespaces.add(categoryNS);
+    Namespace fileNS = new Namespace(
         Integer.toString(Namespace.IMAGE),
         "File", "File",
-        EnumCaseSensitiveness.FIRST_LETTER, true));
+        EnumCaseSensitiveness.FIRST_LETTER, true);
+    fileNS.addAlias("Image");
+    namespaces.add(fileNS);
     wiki.getWikiConfiguration().setNamespaces(namespaces);
+
     List<Interwiki> interwikis = new ArrayList<>();
-    interwikis.add(new Interwiki("en", true, "en", "https://en.wikipedia.org"));
-    interwikis.add(new Interwiki("fr", true, "fr", "https://fr.wikipedia.org"));
+    interwikis.add(new Interwiki("en", true, "English", "https://en.wikipedia.org/wiki/$1"));
+    interwikis.add(new Interwiki("fr", true, "français", "https://fr.wikipedia.org/wiki/$1"));
     wiki.getWikiConfiguration().setInterwikis(interwikis);
+
+    List<Language> languages = new ArrayList<>();
+    languages.add(new Language("en", "English"));
+    languages.add(new Language("fr", "français"));
+    wiki.getWikiConfiguration().setLanguages(languages);
+
+    Map<String, MagicWord> magicWords = new HashMap<>();
+    magicWords.put(MagicWord.PAGE_NAME, new MagicWord(MagicWord.PAGE_NAME, Collections.singletonList("PAGENAME"), true));
+    magicWords.put(MagicWord.REDIRECT, new MagicWord(MagicWord.REDIRECT, Collections.singletonList("#REDIRECT"), false));
+    List<String> thumbAliases = new ArrayList<>();
+    thumbAliases.add("thumb");
+    thumbAliases.add("thumbnail");
+    magicWords.put(MagicWord.IMG_THUMBNAIL, new MagicWord(MagicWord.IMG_THUMBNAIL, thumbAliases, true));
+    magicWords.put(MagicWord.TOC,  new MagicWord(MagicWord.TOC, Collections.singletonList("__TOC__"), false));
+    wiki.getWikiConfiguration().setMagicWords(magicWords);
 
     // Create contents and analysis
     String text = readFile(fileName + ".txt");
@@ -369,7 +394,7 @@ public class PageAnalysisTest {
         "List of " + name + " is null",
         list);
     assertEquals(
-        "List of " + name + " doesn't have" + expectedCount + " " + name,
+        "List of " + name + " doesn't have " + expectedCount + " " + name,
         expectedCount, list.size());
   }
 
