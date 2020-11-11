a51935b005dfe8b3a940d83ec909b9e94dabcf9c
Nicolas <Nicolas@NICO-ZEN3D>
2020-11-11 14:58:21 +0100

    T256456: Switch to Java 8

--- WikipediaCleaner/src/org/wikipediacleaner/Version.java
+++ WikipediaCleaner/src/org/wikipediacleaner/Version.java
@@ -19,8 +19,8 @@ import org.wikipediacleaner.i18n.GT;
  */
 public final class Version {
 
-  public final static String VERSION = "2.03";
-  public final static Date   DATE = new GregorianCalendar(2020, Calendar.JULY, 10).getTime();
+  public final static String VERSION = "2.04";
+  public final static Date   DATE = new GregorianCalendar(2020, Calendar.NOVEMBER, 11).getTime();
 
   public final static String PROGRAM = "WPCleaner";
 
