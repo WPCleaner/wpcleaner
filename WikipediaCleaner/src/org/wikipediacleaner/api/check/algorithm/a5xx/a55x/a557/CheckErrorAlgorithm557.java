40a6361cdbad997b3f32ce97be257e8589b38bde
Nicolas <Nicolas@NICO-ZEN3D>
2020-12-16 21:07:01 +0100

    CW#557: Improvements for automatic fixing and suggestions

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a55x/a557/CheckErrorAlgorithm557.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a55x/a557/CheckErrorAlgorithm557.java
@@ -14,16 +14,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
+
 import org.apache.commons.lang3.StringUtils;
 import org.wikipediacleaner.api.algorithm.AlgorithmParameter;
 import org.wikipediacleaner.api.algorithm.AlgorithmParameterElement;
 import org.wikipediacleaner.api.check.BasicActionProvider;
 import org.wikipediacleaner.api.check.CheckErrorResult;
+import org.wikipediacleaner.api.check.SpecialCharacters;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase;
 import org.wikipediacleaner.api.configuration.WPCConfiguration;
+import org.wikipediacleaner.api.constants.EnumWikipedia;
+import org.wikipediacleaner.api.data.CharacterUtils;
 import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageElementInternalLink;
 import org.wikipediacleaner.api.data.PageElementTag;
+import org.wikipediacleaner.api.data.PageElementTemplate;
 import org.wikipediacleaner.api.data.analysis.PageAnalysis;
 import org.wikipediacleaner.api.data.contents.ilink.InternalLinkBuilder;
 import org.wikipediacleaner.gui.swing.action.ActionExternalViewer;
@@ -146,6 +152,7 @@ public class CheckErrorAlgorithm557 extends CheckErrorAlgorithmBase {
     CheckErrorResult errorResult = createCheckErrorResult(analysis, beginIndex, endIndex);
 
     String replacement = null;
+    boolean automaticUsed = displayedText.isEmpty() || CharacterUtils.isPunctuation(displayedText.charAt(0));
     if ((displayedText.length() > 0) &&
         (" \u00A0;'’".indexOf(displayedText.charAt(0)) >= 0)) {
 
@@ -181,7 +188,8 @@ public class CheckErrorAlgorithm557 extends CheckErrorAlgorithmBase {
             automatic = false;
           }
         }
-        errorResult.addReplacement(replacement, automatic);
+        errorResult.addReplacement(replacement, !automaticUsed && automatic);
+        automaticUsed |= automatic;
 
         // Add a white space before the internal link
         if (!automatic) {
@@ -198,31 +206,48 @@ public class CheckErrorAlgorithm557 extends CheckErrorAlgorithmBase {
     } else {
 
       // Include the text before the internal link
+      EnumWikipedia wiki = analysis.getWikipedia();
       replacement = InternalLinkBuilder
           .from(link.getLink())
           .withAnchor(link.getAnchor())
           .withText(prefix + displayedText)
           .toString();
+      boolean automatic = isSafeLink(link, prefix + displayedText, wiki);
       errorResult.addReplacement(
           replacement,
-          areIdentical(link.getLink(), prefix + displayedText, true));
+          !automaticUsed && automatic);
+      automaticUsed |= automatic;
 
       // Extract the beginning of the internal link
       int spaceIndex = displayedText.indexOf(' ');
       while ((spaceIndex > 0) && (spaceIndex < displayedText.length() - 1)) {
         String fullPrefix = prefix + displayedText.substring(0, spaceIndex + 1);
+        String text = displayedText.substring(spaceIndex + 1);
         replacement =
             fullPrefix +
             InternalLinkBuilder
                 .from(link.getLink())
                 .withAnchor(link.getAnchor())
-                .withText(displayedText.substring(spaceIndex + 1))
+                .withText(text)
                 .toString();
-        boolean automatic = areIdentical(link.getLink(), displayedText.substring(spaceIndex), true);
-        automatic &= (spaceIndex <= 2) || (prefixes_exclude.contains(fullPrefix.trim()));
+        automatic = false;
+        if (link.getText() != null) {
+          automatic = isSafeLink(link, text, wiki);
+          if (automatic &&
+              (beginIndex > 0) &&
+              (analysis.isInInternalLink(beginIndex - 1) != null)) {
+            automatic = false;
+          }
+        } else {
+          automatic = areIdentical(link.getLink(), text, true);
+        }
+        automatic &=
+            (fullPrefix.length() <= 3) ||
+            (prefixes_exclude.contains(fullPrefix.trim()));
         errorResult.addReplacement(
             replacement,
-            automatic);
+            !automaticUsed && automatic);
+        automaticUsed |= automatic;
         spaceIndex = displayedText.indexOf(' ', spaceIndex + 1);
       }
 
@@ -258,6 +283,21 @@ public class CheckErrorAlgorithm557 extends CheckErrorAlgorithmBase {
     // Remove internal link
     errorResult.addReplacement(prefix + link.getDisplayedText());
 
+    // Missing = in a template argument
+    PageElementTemplate template = analysis.isInTemplate(link.getBeginIndex());
+    if (template != null) {
+      PageElementTemplate.Parameter param = template.getParameterAtIndex(link.getBeginIndex());
+      if ((param != null) &&
+          StringUtils.isEmpty(param.getName()) &&
+          (beginIndex == param.getValueStartIndex())) {
+        replacement =
+            contents.substring(beginIndex, link.getBeginIndex()) +
+            "=" +
+            contents.substring(link.getBeginIndex(), endIndex);
+        errorResult.addReplacement(replacement);
+      }
+    }
+
     // View internal link
     errorResult.addPossibleAction(
         GT._T("External Viewer"),
@@ -286,6 +326,96 @@ public class CheckErrorAlgorithm557 extends CheckErrorAlgorithmBase {
       }
     }
     return false;
+  }
+
+  /**
+   * Tells if it is safe to make a link.
+   * 
+   * @param link Link.
+   * @param text Suggested text for the link.
+   * @param wiki Wiki.
+   * @return True if it is safe to make a link.
+   */
+  private boolean isSafeLink(PageElementInternalLink link, String text, EnumWikipedia wiki) {
+    if (isSafeLink(link.getFullLink(), text, wiki)) {
+      return true;
+    }
+    if (link.getAnchor() != null) {
+      if (isSafeLink(link.getLink(), text, wiki)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Tells if it is safe to make a link.
+   * 
+   * @param link Link target.
+   * @param text Suggested text for the link.
+   * @param wiki Wiki.
+   * @return True if it is safe to make a link.
+   */
+  private boolean isSafeLink(String link, String text, EnumWikipedia wiki) {
+    if ((link == null) || (text == null)) {
+      return false;
+    }
+    if (Page.areSameTitle(link, text)) {
+      return true;
+    }
+    if (Page.areSameTitle(
+        cleanLink(link, wiki),
+        cleanLink(text, wiki))) {
+      return true;
+    }
+
+    // Check if the link can be a disambiguation link (parenthesis or comma)
+    int parenthesis = link.indexOf('(');
+    if ((parenthesis > 0) && (isSafeLink(link.substring(0, parenthesis), text, wiki))) {
+      return true;
+    }
+    int comma = link.indexOf(',');
+    if ((comma > 0) && (isSafeLink(link.substring(0, comma), text, wiki))) {
+      return true;
+    }
+
+    // Check if the text is a part of the link
+    if ((parenthesis < 0) &&
+        (comma < 0) &&
+        (text.length() > 4)) {
+      int spaceIndex = 0;
+      while (spaceIndex >= 0) {
+        while ((spaceIndex < link.length()) &&
+              (link.charAt(spaceIndex) == ' ')) {
+          spaceIndex++;
+        }
+        if (link.length() >= spaceIndex + text.length()) {
+          String subLink = link.substring(spaceIndex, spaceIndex + text.length());
+          if (Page.areSameTitle(cleanLink(subLink, wiki), cleanLink(text, wiki))) {
+            return true;
+          }
+          spaceIndex = link.indexOf(' ', spaceIndex);
+        } else {
+          spaceIndex = -1;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Clean a link to compare between text and link.
+   * 
+   * @param link Link to be cleaned.
+   * @param wiki Wiki.
+   * @return Cleaned link.
+   */
+  private String cleanLink(@Nonnull String link, EnumWikipedia wiki) {
+    return SpecialCharacters
+        .replaceAllSpecialCharacters(link.toUpperCase(), wiki)
+        .replaceAll("-", " ")
+        .replaceAll("’", "'")
+        .replaceAll("  ++", " ");
   }
 
   /**
