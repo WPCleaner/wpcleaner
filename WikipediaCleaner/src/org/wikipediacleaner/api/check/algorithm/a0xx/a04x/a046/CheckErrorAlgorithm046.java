143821e365868d8bab397e647e11640cd7ad8ea9
Nicolas <Nicolas@NICO-ZEN3D>
2021-09-24 20:32:20 +0200

    CW#46: Automatic fixing in gallery tags

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a04x/a046/CheckErrorAlgorithm046.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a04x/a046/CheckErrorAlgorithm046.java
@@ -55,168 +55,179 @@ public class CheckErrorAlgorithm046 extends CheckErrorAlgorithmBase {
     int currentIndex = contents.indexOf("]]");
     boolean result = false;
     while (currentIndex > 0) {
-      boolean shouldCount = true;
-      if (shouldCount) {
-        PageElementInternalLink link = analysis.isInInternalLink(currentIndex);
-        if ((link != null) &&
-            ((link.getEndIndex() == currentIndex + 2) ||
-             (link.getEndIndex() == currentIndex + 3))) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementImage image = analysis.isInImage(currentIndex);
-        if ((image != null) &&
-            ((image.getEndIndex() == currentIndex + 2) ||
-             (image.getEndIndex() == currentIndex + 3))) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementCategory category = analysis.isInCategory(currentIndex);
-        if ((category != null) && (category.getEndIndex() == currentIndex + 2)) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementLanguageLink link = analysis.isInLanguageLink(currentIndex);
-        if ((link != null) && (link.getEndIndex() == currentIndex + 2)) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementInterwikiLink link = analysis.isInInterwikiLink(currentIndex);
-        if ((link != null) && (link.getEndIndex() == currentIndex + 2)) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        if (analysis.comments().isAt(currentIndex) ||
-            (analysis.getSurroundingTag(WikiTagType.NOWIKI, currentIndex) != null) ||
-            (analysis.getSurroundingTag(HtmlTagType.CODE, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.MAPFRAME, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.MATH, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.MATH_CHEM, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.PRE, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.SCORE, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.SOURCE, currentIndex) != null) ||
-            (analysis.getSurroundingTag(WikiTagType.SYNTAXHIGHLIGHT, currentIndex) != null) ||
-            (analysis.isInTag(currentIndex) != null)) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementTemplate template = analysis.isInTemplate(currentIndex - 1);
-        if ((template != null) &&
-            (template.getEndIndex() == currentIndex) &&
-            (contents.startsWith("[[", template.getBeginIndex() - 2))) {
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        PageElementExternalLink link = analysis.isInExternalLink(currentIndex);
-        if ((link != null) &&
-            (link.getEndIndex() == currentIndex + 1)) {
-          if ((link.getBeginIndex() == 0) ||
-              (contents.charAt(link.getBeginIndex() - 1) != '[')) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            boolean automatic = true;
-            if (contents.substring(link.getBeginIndex() + 1, currentIndex).indexOf('[') >= 0) {
-              automatic = false;
-            }
-            if (automatic) {
-              int tmpIndex = link.getBeginIndex();
-              while ((tmpIndex > 0) && (contents.charAt(tmpIndex - 1) != '\n')) {
-                tmpIndex--;
-                if (contents.charAt(tmpIndex) == '[') {
-                  PageElementExternalLink eLinkTmp = analysis.isInExternalLink(tmpIndex);
-                  PageElementInternalLink iLinkTmp = analysis.isInInternalLink(tmpIndex);
-                  if ((eLinkTmp == null) && (iLinkTmp == null)) {
-                    automatic = false;
-                  }
-                }
-              }
-            }
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, currentIndex, currentIndex + 2);
-            errorResult.addReplacement("]", automatic);
-            errors.add(errorResult);
-          }
-          shouldCount = false;
-        }
-      }
-      if (shouldCount) {
-        if (errors == null) {
-          return true;
-        }
-        result = true;
-
-        // Check if there is a potential beginning
-        int tmpIndex = currentIndex - 1;
-        boolean errorReported = false;
-        boolean finished = false;
-        while (!finished && tmpIndex >= 0) {
-          char tmpChar = contents.charAt(tmpIndex);
-          if ((tmpChar == '\n') ||
-              (tmpChar == ']') ||
-              (tmpChar == '}')) {
-            finished = true;
-          } else if (tmpChar == '[') {
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, tmpIndex, currentIndex + 2);
-
-            // Check if the situation is something like [http://....]] (replacement: [http://....])
-            boolean protocolFound = PageElementExternalLink.isPossibleProtocol(contents, tmpIndex + 1);
-            if (protocolFound) {
-              errorResult.addReplacement(contents.substring(tmpIndex, currentIndex + 1));
-            }
-
-            errorResult.addReplacement("[" + contents.substring(tmpIndex, currentIndex + 2));
-            errors.add(errorResult);
-            errorReported = true;
-            finished = true;
-          } else if (tmpChar == '{') {
-            int firstChar = tmpIndex;
-            if ((firstChar > 0) && (contents.charAt(firstChar - 1) == '{')) {
-              firstChar--;
-            }
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, firstChar, currentIndex + 2);
-            errorResult.addReplacement("[[" + contents.substring(tmpIndex + 1, currentIndex + 2));
-            errorResult.addReplacement("{{" + contents.substring(tmpIndex + 1, currentIndex) + "}}");
-            errors.add(errorResult);
-            errorReported = true;
-            finished = true;
-          }
-          tmpIndex--;
-        }
-
-        // Default
-        if (!errorReported) {
-          boolean automatic = false;
-          if ((currentIndex >= 2) && (contents.startsWith("]]", currentIndex - 2))) {
-            PageElementCategory category = analysis.isInCategory(currentIndex - 2);
-            if ((category != null) && (category.getEndIndex() == currentIndex)) {
-              automatic = true;
-            }
-            PageElementInternalLink link = analysis.isInInternalLink(currentIndex - 2);
-            if ((link != null) && (link.getEndIndex() == currentIndex)) {
-              automatic = true;
-            }
-          }
-          CheckErrorResult errorResult = createCheckErrorResult(
-              analysis, currentIndex, currentIndex + 2);
-          errorResult.addReplacement("", GT._T("Delete"), automatic);
-          errors.add(errorResult);
-        }
-      }
+      result |= analyzeIndex(analysis, errors, currentIndex);
       currentIndex = contents.indexOf("]]", currentIndex + 2);
     }
 
     return result;
+  }
+
+  /**
+   * Analyze a page to check if errors are present at a given index.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param currentIndex Index to analyze.
+   * @return Flag indicating if the error was found.
+   */
+  private boolean analyzeIndex(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      int currentIndex) {
+    
+    // Ignore some situations
+    PageElementInternalLink iLink = analysis.isInInternalLink(currentIndex);
+    if ((iLink != null) &&
+        ((iLink.getEndIndex() == currentIndex + 2) ||
+         (iLink.getEndIndex() == currentIndex + 3))) {
+      return false;
+    }
+    PageElementImage image = analysis.isInImage(currentIndex);
+    if ((image != null) &&
+        ((image.getEndIndex() == currentIndex + 2) ||
+         (image.getEndIndex() == currentIndex + 3))) {
+      return false;
+    }
+    PageElementCategory category = analysis.isInCategory(currentIndex);
+    if ((category != null) && (category.getEndIndex() == currentIndex + 2)) {
+      return false;
+    }
+    PageElementLanguageLink langLink = analysis.isInLanguageLink(currentIndex);
+    if ((langLink != null) && (langLink.getEndIndex() == currentIndex + 2)) {
+      return false;
+    }
+    PageElementInterwikiLink interLink = analysis.isInInterwikiLink(currentIndex);
+    if ((interLink != null) && (interLink.getEndIndex() == currentIndex + 2)) {
+      return false;
+    }
+    if (analysis.comments().isAt(currentIndex) ||
+        (analysis.getSurroundingTag(WikiTagType.NOWIKI, currentIndex) != null) ||
+        (analysis.getSurroundingTag(HtmlTagType.CODE, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.MAPFRAME, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.MATH, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.MATH_CHEM, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.PRE, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.SCORE, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.SOURCE, currentIndex) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.SYNTAXHIGHLIGHT, currentIndex) != null) ||
+        (analysis.isInTag(currentIndex) != null)) {
+      return false;
+    }
+    String contents = analysis.getContents();
+    PageElementTemplate template = analysis.isInTemplate(currentIndex - 1);
+    if ((template != null) &&
+        (template.getEndIndex() == currentIndex) &&
+        (contents.startsWith("[[", template.getBeginIndex() - 2))) {
+      return false;
+    }
+
+    // Handle specific case inside external link
+    PageElementExternalLink eLink = analysis.isInExternalLink(currentIndex);
+    if ((eLink != null) &&
+        (eLink.getEndIndex() == currentIndex + 1)) {
+      if ((eLink.getBeginIndex() != 0) &&
+          (contents.charAt(eLink.getBeginIndex() - 1) == '[')) {
+        return false;
+      }
+      if (errors == null) {
+        return true;
+      }
+
+      boolean automatic = true;
+      if (contents.substring(eLink.getBeginIndex() + 1, currentIndex).indexOf('[') >= 0) {
+        automatic = false;
+      }
+      if (automatic) {
+        int tmpIndex = eLink.getBeginIndex();
+        while ((tmpIndex > 0) && (contents.charAt(tmpIndex - 1) != '\n')) {
+          tmpIndex--;
+          if (contents.charAt(tmpIndex) == '[') {
+            PageElementExternalLink eLinkTmp = analysis.isInExternalLink(tmpIndex);
+            PageElementInternalLink iLinkTmp = analysis.isInInternalLink(tmpIndex);
+            if ((eLinkTmp == null) && (iLinkTmp == null)) {
+              automatic = false;
+            }
+          }
+        }
+      }
+      CheckErrorResult errorResult = createCheckErrorResult(
+          analysis, currentIndex, currentIndex + 2);
+      errorResult.addReplacement("]", automatic);
+      errors.add(errorResult);
+      return true;
+    }
+
+    // Report error
+    if (errors == null) {
+      return true;
+    }
+
+    // Check if there is a potential beginning
+    int tmpIndex = currentIndex - 1;
+    boolean errorReported = false;
+    boolean finished = false;
+    while (!finished && tmpIndex >= 0) {
+      char tmpChar = contents.charAt(tmpIndex);
+      if ((tmpChar == '\n') ||
+          (tmpChar == ']') ||
+          (tmpChar == '}')) {
+        finished = true;
+      } else if (tmpChar == '[') {
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis, tmpIndex, currentIndex + 2);
+
+        // Check if the situation is something like [http://....]] (replacement: [http://....])
+        boolean protocolFound = PageElementExternalLink.isPossibleProtocol(contents, tmpIndex + 1);
+        if (protocolFound) {
+          errorResult.addReplacement(contents.substring(tmpIndex, currentIndex + 1));
+        }
+
+        errorResult.addReplacement("[" + contents.substring(tmpIndex, currentIndex + 2));
+        errors.add(errorResult);
+        errorReported = true;
+        finished = true;
+      } else if (tmpChar == '{') {
+        int firstChar = tmpIndex;
+        if ((firstChar > 0) && (contents.charAt(firstChar - 1) == '{')) {
+          firstChar--;
+        }
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis, firstChar, currentIndex + 2);
+        errorResult.addReplacement("[[" + contents.substring(tmpIndex + 1, currentIndex + 2));
+        errorResult.addReplacement("{{" + contents.substring(tmpIndex + 1, currentIndex) + "}}");
+        errors.add(errorResult);
+        errorReported = true;
+        finished = true;
+      }
+      tmpIndex--;
+    }
+    if (errorReported) {
+      return true;
+    }
+
+    // Default
+    boolean automatic = false;
+    if ((currentIndex >= 2) && (contents.startsWith("]]", currentIndex - 2))) {
+      PageElementCategory tmpCategory = analysis.isInCategory(currentIndex - 2);
+      if ((tmpCategory != null) && (tmpCategory.getEndIndex() == currentIndex)) {
+        automatic = true;
+      }
+      PageElementInternalLink link = analysis.isInInternalLink(currentIndex - 2);
+      if ((link != null) && (link.getEndIndex() == currentIndex)) {
+        automatic = true;
+      }
+    }
+    if (analysis.getSurroundingTag(WikiTagType.GALLERY, currentIndex) != null) {
+      if (contents.startsWith("\n", currentIndex + 2)) {
+        automatic = true;
+      }
+    }
+    CheckErrorResult errorResult = createCheckErrorResult(
+        analysis, currentIndex, currentIndex + 2);
+    errorResult.addReplacement("", GT._T("Delete"), automatic);
+    errors.add(errorResult);
+
+    return true;
   }
 
   /**
