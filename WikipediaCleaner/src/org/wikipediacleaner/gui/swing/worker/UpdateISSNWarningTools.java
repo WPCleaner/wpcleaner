4026a57c0de97702e5df6d0546552bb1001a45f7
Nicolas <Nicolas@NICO-ZEN3D>
2022-03-12 20:20:45 +0100

    Fix ISSN warning with pipe

--- WikipediaCleaner/src/org/wikipediacleaner/gui/swing/worker/UpdateISSNWarningTools.java
+++ WikipediaCleaner/src/org/wikipediacleaner/gui/swing/worker/UpdateISSNWarningTools.java
@@ -149,6 +149,7 @@ public class UpdateISSNWarningTools extends UpdateWarningTools {
       error = error.replaceAll("\\[", "&#x5B;"); // Replace "[" by its HTML value
       error = error.replaceAll("\\]", "&#x5D;"); // Replace "]" by its HTML value
       error = error.replaceAll("\\{", "&#x7B;"); // Replace "{" by its HTML value
+      error = error.replaceAll("\\|", "&#x7C;"); // Replace "|" by its HTML value
       error = error.replaceAll("\\}", "&#x7D;"); // Replace "}" by its HTML value
       boolean keep = true;
       StringBuilder comment = new StringBuilder();
