438766445754ac920403104c0ba036904494e14a
Nicolas <Nicolas@NICO-ZEN3D>
2022-06-09 08:11:38 +0200

    CW#106: Refactoring

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a1xx/a10x/a106/CheckErrorAlgorithm106.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a1xx/a10x/a106/CheckErrorAlgorithm106.java
@@ -19,6 +19,7 @@ import org.wikipediacleaner.api.configuration.WPCConfigurationStringList;
 import org.wikipediacleaner.api.data.Namespace;
 import org.wikipediacleaner.api.data.Page;
 import org.wikipediacleaner.api.data.PageElementFunction;
+import org.wikipediacleaner.api.data.PageElementISBN;
 import org.wikipediacleaner.api.data.PageElementISSN;
 import org.wikipediacleaner.api.data.PageElementTemplate;
 import org.wikipediacleaner.api.data.PageElementTemplate.Parameter;
@@ -57,83 +58,97 @@ public class CheckErrorAlgorithm106 extends CheckErrorAlgorithmISSN {
     boolean result = false;
     List<PageElementISSN> issns = analysis.getISSNs();
     for (PageElementISSN issn : issns) {
-      boolean isError = false;
-      if (!issn.isCorrect() && issn.isValid()) {
-        isError = true;
-      }
+      result |= analyzeISSN(analysis, errors, issn);
+    }
 
-      // Exclude special configured values for ISSN
-      if (isError &&
-          issn.isTemplateParameter()) {
-        WPCConfiguration config = analysis.getWPCConfiguration();
-        List<String[]> specialValues = config.getStringArrayList(
-            WPCConfigurationStringList.ISSN_SPECIAL_VALUES);
-        if ((specialValues != null) && !specialValues.isEmpty()) {
-          PageElementTemplate template = analysis.isInTemplate(issn.getBeginIndex());
-          if (template != null) {
-            Parameter param = template.getParameterAtIndex(issn.getBeginIndex());
-            if ((param != null) &&
-                (param.getName() != null) &&
-                (param.getName().trim().length() > 0)) {
-              String name = param.getName().trim();
-              for (String[] specialValue : specialValues) {
-                if ((specialValue.length > 2) &&
-                    (Page.areSameTitle(template.getTemplateName(), specialValue[0])) &&
-                    (name.equals(specialValue[1])) &&
-                    (issn.getISSNNotTrimmed().equals(specialValue[2]))) {
-                  isError = false;
-                }
-              }
-            }
-          }
-        }
-      }
+    return result;
+  }
 
-      // Exclude parameters in templates
-      if (isError &&
-          issn.isTemplateParameter() &&
-          analysis.isInNamespace(Namespace.TEMPLATE)) {
+  /**
+   * Analyze an ISSN to check if has an error.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @return issn ISSN to be checked.
+   */
+  private boolean analyzeISSN(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      PageElementISSN issn) {
+
+    // Check if an error is detected in the ISBN
+    if (issn.isCorrect() || !issn.isValid()) {
+      return false;
+    }
+
+    // Exclude special configured values for ISSN if inside a template parameter
+    if (issn.isTemplateParameter()) {
+      WPCConfiguration config = analysis.getWPCConfiguration();
+      List<String[]> specialValues = config.getStringArrayList(
+          WPCConfigurationStringList.ISSN_SPECIAL_VALUES);
+      if ((specialValues != null) && !specialValues.isEmpty()) {
         PageElementTemplate template = analysis.isInTemplate(issn.getBeginIndex());
         if (template != null) {
           Parameter param = template.getParameterAtIndex(issn.getBeginIndex());
-          if (param != null) {
-            List<PageElementFunction> functions = analysis.getFunctions();
-            if (functions != null) {
-              for (PageElementFunction function : functions) {
-                int functionIndex = function.getBeginIndex();
-                if ((template == analysis.isInTemplate(functionIndex)) &&
-                    (param == template.getParameterAtIndex(functionIndex))) {
-                  isError = false;
-                }
+          if ((param != null) &&
+              (param.getName() != null) &&
+              (param.getName().trim().length() > 0)) {
+            String name = param.getName().trim();
+            for (String[] specialValue : specialValues) {
+              if ((specialValue.length > 2) &&
+                  (Page.areSameTitle(template.getTemplateName(), specialValue[0])) &&
+                  (name.equals(specialValue[1])) &&
+                  (issn.getISSNNotTrimmed().equals(specialValue[2]))) {
+                return false;
               }
-            }
-          }
-        }
-      }
-
-      // Report error
-      if (isError) {
-        if (errors == null) {
-          return true;
-        }
-        result = true;
-        CheckErrorResult errorResult = createCheckErrorResult(analysis, issn, false);
-        addSuggestions(analysis, errorResult, issn);
-        errors.add(errorResult);
-        List<Replacement> replacements = issn.getCorrectISSN();
-        if (replacements != null) {
-          for (Replacement replacement : replacements) {
-            String newText = replacement.replacement;
-            if ((newText != null) &&
-                !newText.equals(analysis.getContents().substring(issn.getBeginIndex(), issn.getEndIndex()))) {
-              errorResult.addReplacement(replacement);
             }
           }
         }
       }
     }
 
-    return result;
+    // Exclude parameters in templates
+    if (issn.isTemplateParameter() &&
+        analysis.isInNamespace(Namespace.TEMPLATE)) {
+      PageElementTemplate template = analysis.isInTemplate(issn.getBeginIndex());
+      if (template != null) {
+        Parameter param = template.getParameterAtIndex(issn.getBeginIndex());
+        if (param != null) {
+          List<PageElementFunction> functions = analysis.getFunctions();
+          if (functions != null) {
+            for (PageElementFunction function : functions) {
+              int functionIndex = function.getBeginIndex();
+              if ((template == analysis.isInTemplate(functionIndex)) &&
+                  (param == template.getParameterAtIndex(functionIndex))) {
+                return false;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // Report error
+    if (errors == null) {
+      return true;
+    }
+
+    // Report error
+    CheckErrorResult errorResult = createCheckErrorResult(analysis, issn, false);
+    addSuggestions(analysis, errorResult, issn);
+    errors.add(errorResult);
+    List<Replacement> replacements = issn.getCorrectISSN();
+    if (replacements != null) {
+      for (Replacement replacement : replacements) {
+        String newText = replacement.replacement;
+        if ((newText != null) &&
+            !newText.equals(analysis.getContents().substring(issn.getBeginIndex(), issn.getEndIndex()))) {
+          errorResult.addReplacement(replacement);
+        }
+      }
+    }
+
+    return true;
   }
 
   /**
