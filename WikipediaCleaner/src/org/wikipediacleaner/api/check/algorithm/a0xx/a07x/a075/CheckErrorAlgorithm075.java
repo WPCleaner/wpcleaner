627dab7e0a639365ecb4ab708a4f252567714c3f
Nicolas <Nicolas@192.168.1.107>
2022-08-22 21:03:49 +0200

    CW#75: Refactoring and consistency with CW detection

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a07x/a075/CheckErrorAlgorithm075.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a07x/a075/CheckErrorAlgorithm075.java
@@ -8,9 +8,11 @@
 package org.wikipediacleaner.api.check.algorithm.a0xx.a07x.a075;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase;
+import org.wikipediacleaner.api.data.PageElementListItem;
 import org.wikipediacleaner.api.data.analysis.PageAnalysis;
 
 
@@ -43,66 +45,70 @@ public class CheckErrorAlgorithm075 extends CheckErrorAlgorithmBase {
       return false;
     }
 
-    // Analyzing the text from the beginning
-    boolean result = false;
-    int startIndex = -1;
-    String contents = analysis.getContents();
-    String previousLine = "";
-    String incorrectLine = null;
-    while (startIndex < contents.length()) {
-      if (startIndex < 0) {
-        startIndex = 0;
-      } else {
-        startIndex = contents.indexOf('\n', startIndex);
-        if (startIndex >= 0) {
-          startIndex++;
-        }
-      }
-      if ((startIndex < 0) || startIndex >= contents.length()) {
-        startIndex = contents.length();
-      } else {
-        if (contents.charAt(startIndex) == ':') {
-          int endIndex = startIndex;
-          boolean otherThanColon = false;
-          while ((startIndex < contents.length()) &&
-                 (":-*#".indexOf(contents.charAt(endIndex)) >= 0)) {
-            if (contents.charAt(endIndex) != ':') {
-              otherThanColon = true;
-            }
-            endIndex++;
-          }
-          String newLine = contents.substring(startIndex, endIndex);
-          int newLength = newLine.length();
-          if ((newLength > 1) && otherThanColon &&
-              ((newLength > previousLine.length() + 1) ||
-               (!previousLine.startsWith(newLine.substring(0, newLength - 1))))) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, startIndex, endIndex);
-            char lastChar = newLine.charAt(newLine.length() - 1);
-            if (incorrectLine == null) {
-              incorrectLine = newLine;
-            }
-            if (newLine.equals(incorrectLine)) {
-              if (newLine.length() > previousLine.length() + 1) {
-                errorResult.addReplacement(previousLine + lastChar);
-              } else {
-                errorResult.addReplacement(previousLine.substring(0, newLength - 1) + lastChar);
-              }
-            }
-            errors.add(errorResult);
-          } else {
-            previousLine = newLine;
-            incorrectLine = null;
-          }
-        } else {
-          previousLine = "";
-        }
-      }
+    // Analyze each list item
+    List<PageElementListItem> listItems = analysis.getListItems();
+    if (listItems == null) {
+      return false;
     }
+    boolean result = false;
+    for (int index = 0; index < listItems.size(); index++) {
+      result |= analyzeListItem(analysis, errors, listItems, index);
+    }
+    return result;
+  }
+
+  /**
+   * Analyze a list item to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param listItems List items
+   * @param initialIndex Index of the initial item to analyze.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyzeListItem(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      List<PageElementListItem> listItems,
+      int initialIndex) {
+    PageElementListItem initialItem = listItems.get(initialIndex);
+    String initialIndicators = initialItem.getIndicators();
+    if (!initialIndicators.startsWith("*") && !initialIndicators.startsWith("#")) {
+      return false;
+    }
+
+    int currentIndex = initialIndex + 1;
+    boolean result = false;
+    while (currentIndex < listItems.size()) {
+      PageElementListItem currentItem = listItems.get(currentIndex);
+      String currentIndicators = currentItem.getIndicators();
+      if (currentIndicators.startsWith(":*") || currentIndicators.startsWith(":#")) {
+        if (errors == null) {
+          return true;
+        }
+        result = true;
+        String contents = analysis.getContents();
+        int beginIndex = currentItem.getBeginIndex();
+        int endIndex = currentItem.getEndIndex();
+        CheckErrorResult errorResult = createCheckErrorResult(analysis, beginIndex, endIndex);
+        boolean automatic =
+            (initialIndicators.length() == 1) &&
+            (currentIndicators.charAt(0) == initialIndicators.charAt(0));
+        String replacement =
+            initialIndicators.substring(0, 1) +
+            contents.substring(beginIndex + 1, endIndex);
+        String text =
+            initialIndicators.substring(0, 1) +
+            currentIndicators.substring(1) +
+            " ...";
+        errorResult.addReplacement(replacement, text, automatic);
+        errors.add(errorResult);
+      } else {
+        return result;
+      }
+      currentIndex++;
+    }
+
     return result;
   }
 }
