00ca178b15ba3dd4183bcf633a09cdf37d756c1e
Nicolas <Nicolas@192.168.1.107>
2023-05-02 10:54:43 +0200

    CW#93: Automatic fixing

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a09x/a093/CheckErrorAlgorithm093.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a09x/a093/CheckErrorAlgorithm093.java
@@ -9,6 +9,7 @@ package org.wikipediacleaner.api.check.algorithm.a0xx.a09x.a093;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase;
@@ -50,93 +51,136 @@ public class CheckErrorAlgorithm093 extends CheckErrorAlgorithmBase {
     // Analyze each external link
     List<PageElementExternalLink> links = analysis.getExternalLinks();
     boolean result = false;
-    String contents = analysis.getContents();
     for (PageElementExternalLink link : links) {
+      result |= analyzeExternalLink(analysis, errors, link);
+    }
 
-      // Initialization
-      boolean errorFound = false;
-      int beginIndex = link.getBeginIndex();
-      String dest = link.getLink();
-      String prefix1 = null;
-      String prefix2 = null;
-      String withoutPrefix = null;
+    return result;
+  }
 
-      // Check if there's a double prefix inside the link
-      if (!errorFound) {
-        for (String tmp1 : possiblePrefixes) {
-          if ((prefix1 == null) &&
-              (tmp1.length() < dest.length()) &&
-              (tmp1.equalsIgnoreCase(dest.substring(0, tmp1.length())))) {
-            for (String tmp2 : possiblePrefixes) {
-              if ((prefix2 == null) &&
-                  (tmp1.length() + tmp2.length() < dest.length()) &&
-                  tmp2.equalsIgnoreCase(dest.substring(
-                      tmp1.length(),
-                      tmp1.length() + tmp2.length()))) {
-                errorFound = true;
-                prefix1 = tmp1;
-                prefix2 = tmp2;
-                withoutPrefix = dest.substring(tmp1.length() + tmp2.length());
-              }
-            }
-          }
-        }
+  /**
+   * Analyze an external to check if errors are present.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @param link External link to be analyzed.
+   * @return Flag indicating if the error was found.
+   */
+  public boolean analyzeExternalLink(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      PageElementExternalLink link) {
 
-        // Check if there's a second prefix before the link
-        if (!errorFound && !link.hasSquare()) {
-          for (String tmp2 : possiblePrefixes) {
-            if ((prefix2 == null) &&
-                (tmp2.length() < dest.length()) &&
-                (tmp2.equalsIgnoreCase(dest.substring(0, tmp2.length())))) {
-              prefix2 = tmp2;
-            }
-          }
-          for (String tmp1 : possiblePrefixes) {
-            if ((prefix1 == null) &&
-                (beginIndex >= tmp1.length()) &&
-                (tmp1.equalsIgnoreCase(contents.substring(beginIndex - tmp1.length(), beginIndex)))) {
-              prefix1 = tmp1;
-            }
-          }
-          if ((prefix1 != null) && (prefix2 != null)) {
+    // Initialization
+    int beginIndex = link.getBeginIndex();
+    String dest = link.getLink();
+
+    // Check if there's a double prefix inside the link
+    boolean errorFound = false;
+    String prefix1 = null;
+    String prefix2 = null;
+    String withoutPrefix = null;
+    for (String tmp1 : possiblePrefixes) {
+      if ((prefix1 == null) &&
+          (tmp1.length() < dest.length()) &&
+          (tmp1.equalsIgnoreCase(dest.substring(0, tmp1.length())))) {
+        for (String tmp2 : possiblePrefixes) {
+          if ((prefix2 == null) &&
+              (tmp1.length() + tmp2.length() < dest.length()) &&
+              tmp2.equalsIgnoreCase(dest.substring(
+                  tmp1.length(),
+                  tmp1.length() + tmp2.length()))) {
             errorFound = true;
-            beginIndex -= prefix1.length();
-            withoutPrefix = dest.substring(prefix2.length());
+            prefix1 = tmp1;
+            prefix2 = tmp2;
+            withoutPrefix = dest.substring(tmp1.length() + tmp2.length());
           }
-        }
-
-        // Raise error
-        if (errorFound && (prefix1 != null) && (prefix2 != null)) {
-          if (errors == null) {
-            return true;
-          }
-          result = true;
-          CheckErrorResult errorResult = createCheckErrorResult(
-              analysis, beginIndex, link.getEndIndex());
-          if (prefix2.endsWith("//")) {
-            String newDest = prefix2 + withoutPrefix;
-            if (link.hasSquare()) {
-              errorResult.addReplacement(PageElementExternalLink.createExternalLink(
-                  newDest, link.getText()));
-            } else {
-              errorResult.addReplacement(newDest);
-            }
-          }
-          if (!prefix1.equalsIgnoreCase(prefix2) &&
-              (prefix1.endsWith("//"))) {
-            String newDest = prefix1 + withoutPrefix;
-            if (link.hasSquare()) {
-              errorResult.addReplacement(PageElementExternalLink.createExternalLink(
-                  newDest, link.getText()));
-            } else {
-              errorResult.addReplacement(newDest);
-            }
-          }
-          errors.add(errorResult);
         }
       }
     }
 
-    return result;
+    // Check if there's a second prefix before the link
+    if (!errorFound && !link.hasSquare()) {
+      for (String tmp2 : possiblePrefixes) {
+        if ((prefix2 == null) &&
+            (tmp2.length() < dest.length()) &&
+            (tmp2.equalsIgnoreCase(dest.substring(0, tmp2.length())))) {
+          prefix2 = tmp2;
+        }
+      }
+      String contents = analysis.getContents();
+      for (String tmp1 : possiblePrefixes) {
+        if ((prefix1 == null) &&
+            (beginIndex >= tmp1.length()) &&
+            (tmp1.equalsIgnoreCase(contents.substring(beginIndex - tmp1.length(), beginIndex)))) {
+          prefix1 = tmp1;
+        }
+      }
+      if ((prefix1 != null) && (prefix2 != null)) {
+        errorFound = true;
+        beginIndex -= prefix1.length();
+        withoutPrefix = dest.substring(prefix2.length());
+      }
+    }
+
+    if (!errorFound || (prefix1 == null) || (prefix2 == null)) {
+      return false;
+    }
+    if (errors == null) {
+      return true;
+    }
+
+    // Raise error
+    CheckErrorResult errorResult = createCheckErrorResult(analysis, beginIndex, link.getEndIndex());
+    if (prefix1.equalsIgnoreCase(prefix2)) {
+      addReplacement(
+          errorResult,
+          link,
+          prefix1.toLowerCase(Locale.ROOT) + withoutPrefix,
+          prefix1.endsWith("//"));
+    } else {
+      if (prefix2.endsWith("//")) {
+        addReplacement(
+            errorResult,
+            link,
+            prefix2.toLowerCase(Locale.ROOT) + withoutPrefix,
+            prefix2.equalsIgnoreCase("https://") && prefix1.equalsIgnoreCase("http://"));
+      }
+      if (prefix1.endsWith("//")) {
+        addReplacement(
+            errorResult,
+            link,
+            prefix1.toLowerCase(Locale.ROOT) + withoutPrefix,
+            prefix1.equalsIgnoreCase("https://") && prefix2.equalsIgnoreCase("http://"));
+      }
+    }
+    errors.add(errorResult);
+    return true;
+  }
+
+  private void addReplacement(
+      CheckErrorResult errorResult, PageElementExternalLink link,
+      String dest, boolean automatic) {
+    if (link.hasSquare()) {
+      errorResult.addReplacement(
+          PageElementExternalLink.createExternalLink(dest, link.getText()),
+          automatic);
+    } else {
+      errorResult.addReplacement(dest, automatic);
+    }
+  }
+
+  /**
+   * Automatic fixing of all the errors in the page.
+   * 
+   * @param analysis Page analysis.
+   * @return Page contents after fix.
+   */
+  @Override
+  protected String internalAutomaticFix(PageAnalysis analysis) {
+    if (!analysis.getPage().isArticle()) {
+      return analysis.getContents();
+    }
+    return fixUsingAutomaticReplacement(analysis);
   }
 }
