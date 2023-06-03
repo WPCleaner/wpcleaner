c38be3b2db6041a0f4b25e7252cc06bf38c88a15
Nicolas <Nicolas@192.168.1.112>
2023-06-03 20:45:47 +0200

    CW#81: Improve selection of the name of the reference

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a08x/a081/RefTagSelector.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a0xx/a08x/a081/RefTagSelector.java
@@ -22,6 +22,7 @@ import org.wikipediacleaner.api.data.PageElementTag;
 import org.wikipediacleaner.api.data.PageElementTagRef;
 import org.wikipediacleaner.api.data.PageElementTemplate;
 import org.wikipediacleaner.api.data.analysis.PageAnalysis;
+import org.wikipediacleaner.api.data.contents.ContentsUtil;
 import org.wikipediacleaner.api.data.contents.tag.WikiTagType;
 
 class RefTagSelector {
@@ -52,21 +53,21 @@ class RefTagSelector {
     }
 
     // Keep only tags with a name and a value
-    List<PageElementTag> namedAndValuedTags = tags.stream()
+    List<PageElementTag> selectedTags = tags.stream()
         .filter(this::hasName)
         .filter(tag -> hasValue(analysis, tag))
         .collect(Collectors.toList());
-    if (namedAndValuedTags.isEmpty()) {
+    if (selectedTags.isEmpty()) {
       return null;
     }
-    if (namedAndValuedTags.size() == 1) {
-      return namedAndValuedTags.get(0);
+    if (selectedTags.size() == 1) {
+      return selectedTags.get(0);
     }
 
-    // Prefer a named tag inside a <references />
+    // Prefer a tag inside a <references />
     final List<PageElementTag> references = analysis.getCompleteTags(WikiTagType.REFERENCES);
     if (!references.isEmpty()) {
-      for (PageElementTag tag : namedAndValuedTags) {
+      for (PageElementTag tag : selectedTags) {
         for (PageElementTag reference : references) {
           if ((tag.getCompleteBeginIndex() > reference.getCompleteBeginIndex()) &&
               (tag.getCompleteEndIndex() < reference.getCompleteEndIndex())) {
@@ -76,9 +77,9 @@ class RefTagSelector {
       }
     }
 
-    // Prefer a named tag inside a references template
+    // Prefer a tag inside a references template
     if (!referencesTemplates.isEmpty()) {
-      for (PageElementTag tag : namedAndValuedTags) {
+      for (PageElementTag tag : selectedTags) {
         PageElementTemplate template = analysis.isInTemplate(tag.getCompleteBeginIndex());
         if ((template != null) && referencesTemplates.contains(template.getTemplateName())) {
           return tag;
@@ -86,21 +87,75 @@ class RefTagSelector {
       }
     }
 
-    // TODO: better heuristics (like based on name to handle extra 2 added by VE)
+    // Filter tags in a template
+    List<PageElementTag> tagsNotInTemplate = selectedTags.stream()
+        .filter(tag -> analysis.isInTemplate(tag.getBeginIndex()) == null)
+        .collect(Collectors.toList());
+    if (!tagsNotInTemplate.isEmpty()) {
+      selectedTags = tagsNotInTemplate;
+    }
 
-    // Return first named tag not in a template
-    for (PageElementTag namedAndValuedTag : namedAndValuedTags) {
-      if (analysis.isInTemplate(namedAndValuedTag.getBeginIndex()) == null) {
-        return namedAndValuedTag;
-      }
+    // Filter name starting with ":" followed by digits
+    List<PageElementTag> tagsWithRealName = selectedTags.stream()
+        .filter(this::hasRealName)
+        .collect(Collectors.toList());
+    if (!tagsWithRealName.isEmpty()) {
+      selectedTags = tagsWithRealName;
+    }
+
+    // Filter name equal to another name with number after
+    final List<PageElementTag> compareTags = selectedTags;
+    List<PageElementTag> tagsWithLowerName = selectedTags.stream()
+        .filter(tag -> hasNoLowerName(tag, compareTags))
+        .collect(Collectors.toList());
+    if (!tagsWithLowerName.isEmpty()) {
+      selectedTags = tagsWithLowerName;
     }
 
     // Return first named tag
-    return namedAndValuedTags.get(0);
+    return selectedTags.get(0);
   }
 
   private boolean hasName(PageElementTag tag) {
     return PageElementTagRef.getName(tag) != null;
+  }
+
+  private boolean hasRealName(PageElementTag tag) {
+    final String name = PageElementTagRef.getName(tag);
+    if (name == null) {
+      return false;
+    }
+    if (!name.startsWith(":")) {
+      return true;
+    }
+    if (name.length() < 2) {
+      return false;
+    }
+    if (ContentsUtil.moveIndexForwardWhileFound(name, 1, "0123456789") == name.length()) {
+      return false;
+    }
+    return true;
+  }
+
+  private boolean hasNoLowerName(PageElementTag tag, List<PageElementTag> tags) {
+    final String name = PageElementTagRef.getName(tag);
+    if (name == null) {
+      return false;
+    }
+    return tags.stream()
+        .map(PageElementTagRef::getName)
+        .filter(testName -> !name.equals(testName))
+        .noneMatch(testName -> isLowerName(name, testName));
+  }
+
+  private boolean isLowerName(String name, String compareName) {
+    if (!name.startsWith(compareName)) {
+      return false;
+    }
+    if (name.length() <= compareName.length()) {
+      return false;
+    }
+    return ContentsUtil.moveIndexForwardWhileFound(name, compareName.length(), "0123456789") == name.length();
   }
 
   private boolean hasValue(PageAnalysis analysis, PageElementTag tag) {
