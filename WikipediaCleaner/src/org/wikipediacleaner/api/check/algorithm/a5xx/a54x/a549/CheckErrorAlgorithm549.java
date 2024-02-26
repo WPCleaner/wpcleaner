40b1752dc891272b3295b5c32c4b040e623f355e
Nicolas <Nicolas@192.168.1.107>
2024-02-26 16:17:50 +0100

    CW#549: Refactoring

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a54x/a549/CheckErrorAlgorithm549.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a54x/a549/CheckErrorAlgorithm549.java
@@ -10,6 +10,7 @@ package org.wikipediacleaner.api.check.algorithm.a5xx.a54x.a549;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase;
@@ -74,187 +75,204 @@ public class CheckErrorAlgorithm549 extends CheckErrorAlgorithmBase {
     if (links == null) {
       return false;
     }
-    String contents = analysis.getContents();
     boolean result = false;
-    int linkNum = 0;
+    AtomicInteger linkNum = new AtomicInteger(0);
+    while (linkNum.get() < links.size()) {
+      result |= analyzeInternalLink(analysis, errors, links, linkNum);
+    }
+    return result;
+  }
+
+  /**
+   * Analyze a page to check if errors are present in internal links.
+   * 
+   * @param analysis Page analysis.
+   * @param errors Errors found in the page.
+   * @return Flag indicating if the error was found.
+   */
+  private boolean analyzeInternalLink(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      List<PageElementInternalLink> links,
+      AtomicInteger linkNum) {
+
+    // Check if next link has the same target
+    PageElementInternalLink firstLink = links.get(linkNum.get());
+    if (linkNum.addAndGet(1) >= links.size()) {
+      return false;
+    }
+    String fullLink = firstLink.getFullLink();
+    if (!fullLink.equals(links.get(linkNum.get()).getFullLink())) {
+      return false;
+    }
+    int endIndex = firstLink.getEndIndex();
+
+    // Check for consecutive links with the same target
+    String contents = analysis.getContents();
     List<PageElementInternalLink> tmpLinks = new ArrayList<>();
-    tmpLinks.add(null);
+    tmpLinks.add(firstLink);
     List<String> tmpBetweenList = new ArrayList<>();
-    StringBuilder tmpBetween = new StringBuilder();
-    while (linkNum < links.size()) {
+    boolean finished = false;
+    while (!finished && (linkNum.get() < links.size())) {
 
-      // Check for consecutive links with the same target
-      PageElementInternalLink firstLink = links.get(linkNum);
-      tmpLinks.set(0, firstLink);
-      tmpBetweenList.clear();
-      String fullLink = firstLink.getFullLink();
-      int endIndex = firstLink.getEndIndex();
-      linkNum++;
-      boolean finished = false;
-      while (!finished && (linkNum < links.size())) {
-
-        // Ignore special characters after the link
-        tmpBetween.setLength(0);
-        int tmpIndex = endIndex;
-        while (!finished && (tmpIndex < contents.length())) {
-          char tmpChar = contents.charAt(tmpIndex);
-          if (tmpChar == '<') {
-            PageElementTag tag = analysis.isInTag(tmpIndex);
-            if ((tag != null) &&
-                (tag.getBeginIndex() == tmpIndex) &&
-                (WikiTagType.NOWIKI.equals(tag.getType()))) {
-              tmpIndex = tag.getEndIndex();
-            } else {
-              finished = true;
-            }
-          } else if (CharacterUtils.isWhitespace(tmpChar) ||
-                     (tmpChar == '\'')) {
-            tmpBetween.append(tmpChar);
-            tmpIndex++;
+      // Ignore special characters after the link
+      StringBuilder tmpBetween = new StringBuilder();
+      int tmpIndex = endIndex;
+      while (!finished && (tmpIndex < contents.length())) {
+        char tmpChar = contents.charAt(tmpIndex);
+        if (tmpChar == '<') {
+          PageElementTag tag = analysis.isInTag(tmpIndex);
+          if ((tag != null) &&
+              (tag.getBeginIndex() == tmpIndex) &&
+              (WikiTagType.NOWIKI.equals(tag.getType()))) {
+            tmpIndex = tag.getEndIndex();
           } else {
             finished = true;
           }
-        }
-
-        // Check if the next link is just after the current link
-        finished = false;
-        if ((links.get(linkNum).getBeginIndex() == tmpIndex) &&
-            (fullLink.equals(links.get(linkNum).getFullLink()))) {
-          tmpBetweenList.add(tmpBetween.toString());
-          PageElementInternalLink lastLink = links.get(linkNum);
-          tmpLinks.add(lastLink);
-          endIndex = lastLink.getEndIndex();
-          linkNum++;
+        } else if (CharacterUtils.isWhitespace(tmpChar) ||
+                   (tmpChar == '\'')) {
+          tmpBetween.append(tmpChar);
+          tmpIndex++;
         } else {
           finished = true;
         }
       }
 
-      // Report error
-      if (tmpLinks.size() > 1) {
-        if (errors == null) {
-          return true;
-        }
-        result = true;
-
-        // Construct possible replacement
-        StringBuilder buffer = new StringBuilder();
-        String previousText = "";
-        boolean automatic = true;
-        for (int tmpLinkNum = 0; tmpLinkNum < tmpLinks.size(); tmpLinkNum++) {
-          PageElementInternalLink link = tmpLinks.get(tmpLinkNum);
-          String text = link.getDisplayedTextNotTrimmed();
-          boolean nowiki = false;
-          boolean punctuation = false;
-          boolean otherChar = false;
-          if (link.getTextOffset() > 0) {
-            int charNum = 0;
-            while (charNum < text.length()) {
-              int currentIndex = link.getBeginIndex() + link.getTextOffset() + charNum;
-              char currentChar = contents.charAt(currentIndex);
-              if (currentChar == '<') {
-                PageElementTag tag = analysis.isInTag(currentIndex);
-                if ((tag != null) &&
-                    (tag.getBeginIndex() == currentIndex) &&
-                    (WikiTagType.NOWIKI.equals(tag.getType()))) {
-                  nowiki = true;
-                  charNum += tag.getEndIndex() - tag.getBeginIndex() - 1;
-                } else {
-                  otherChar = true;
-                }
-              } else if (PUNCTUATIONS.indexOf(currentChar) >= 0) {
-                punctuation = true;
-              } else if (!CharacterUtils.isWhitespace(currentChar)) {
-                otherChar = true;
-              }
-              charNum++;
-            }
-          } else {
-            otherChar = true;
-          }
-          boolean textEquals = text.equals(previousText);
-          boolean textEqualsCase = text.equalsIgnoreCase(previousText);
-          previousText = text;
-          if (textEqualsCase) {
-            automatic = false;
-          }
-          if (tmpLinkNum > 0) {
-            String between = tmpBetweenList.get(tmpLinkNum - 1);
-            if (!between.isEmpty()) {
-              buffer.append(between);
-              if (between.startsWith("'") || between.endsWith("'")) {
-                automatic = false;
-              }
-            }
-          }
-          if (!textEquals && (!nowiki || otherChar || punctuation)) {
-            if (text.startsWith("'") &&
-                buffer.toString().endsWith("'")) {
-              automatic = false;
-            }
-            buffer.append(text);
-            if (nowiki) {
-              automatic = false;
-            }
-            if (punctuation && !otherChar) {
-              automatic = false;
-            } 
-          }
-        }
-        if (buffer.length() == 0) {
-          automatic = false;
-        }
-
-        // Report error
-        PageElementInternalLink lastLink = tmpLinks.get(tmpLinks.size() - 1);
-        int beginIndex = firstLink.getBeginIndex();
+      // Check if the next link is just after the current link
+      finished = false;
+      if ((links.get(linkNum.get()).getBeginIndex() == tmpIndex) &&
+          (fullLink.equals(links.get(linkNum.get()).getFullLink()))) {
+        tmpBetweenList.add(tmpBetween.toString());
+        PageElementInternalLink lastLink = links.get(linkNum.get());
+        tmpLinks.add(lastLink);
         endIndex = lastLink.getEndIndex();
-        CheckErrorResult errorResult = createCheckErrorResult(
-            analysis, beginIndex, endIndex);
-
-        // Suggestion to group the links
-        errorResult.addReplacement(
-            InternalLinkBuilder.from(fullLink).withText(buffer.toString()).toString(),
-            automatic);
-
-        // Suggestion to keep only the text for the last link
-        boolean lastPunctuationOnly = false;
-        String lastText = lastLink.getDisplayedText();
-        if (lastText != null) {
-          lastPunctuationOnly = true;
-          for (int charIndex = 0; charIndex < lastText.length(); charIndex++) {
-            if (PUNCTUATIONS.indexOf(lastText.charAt(charIndex)) < 0) {
-              lastPunctuationOnly = false;
-            }
-          }
-        }
-        errorResult.addReplacement(
-            contents.substring(beginIndex, lastLink.getBeginIndex()) + lastText,
-            !automatic && lastPunctuationOnly && !fullLink.endsWith(lastText));
-
-        // Suggestion to keep only the text for the first link
-        errorResult.addReplacement(
-            firstLink.getDisplayedText() + contents.substring(firstLink.getEndIndex(), endIndex),
-            false);
-
-        // Suggestion to remove the last link entirely
-        errorResult.addReplacement(
-            contents.substring(beginIndex, lastLink.getBeginIndex()),
-            false);
-
-        // Suggestion to remove the first link entirely
-        errorResult.addReplacement(
-            contents.substring(firstLink.getEndIndex(), endIndex),
-            false);
-
-        errors.add(errorResult);
-
-        // Restore the temporary list to one element
-        tmpLinks.clear();
-        tmpLinks.add(null);
+        linkNum.addAndGet(1);
+      } else {
+        finished = true;
       }
     }
-    return result;
+
+    // Check if there's an error
+    if (tmpLinks.size() <= 1) {
+      return false;
+    }
+    if (errors == null) {
+      return true;
+    }
+
+    // Report error
+    // Construct possible replacement
+    StringBuilder buffer = new StringBuilder();
+    String previousText = "";
+    boolean automatic = true;
+    for (int tmpLinkNum = 0; tmpLinkNum < tmpLinks.size(); tmpLinkNum++) {
+      PageElementInternalLink link = tmpLinks.get(tmpLinkNum);
+      String text = link.getDisplayedTextNotTrimmed();
+      boolean nowiki = false;
+      boolean punctuation = false;
+      boolean otherChar = false;
+      if (link.getTextOffset() > 0) {
+        int charNum = 0;
+        while (charNum < text.length()) {
+          int currentIndex = link.getBeginIndex() + link.getTextOffset() + charNum;
+          char currentChar = contents.charAt(currentIndex);
+          if (currentChar == '<') {
+            PageElementTag tag = analysis.isInTag(currentIndex);
+            if ((tag != null) &&
+                (tag.getBeginIndex() == currentIndex) &&
+                (WikiTagType.NOWIKI.equals(tag.getType()))) {
+              nowiki = true;
+              charNum += tag.getEndIndex() - tag.getBeginIndex() - 1;
+            } else {
+              otherChar = true;
+            }
+          } else if (PUNCTUATIONS.indexOf(currentChar) >= 0) {
+            punctuation = true;
+          } else if (!CharacterUtils.isWhitespace(currentChar)) {
+            otherChar = true;
+          }
+          charNum++;
+        }
+      } else {
+        otherChar = true;
+      }
+      boolean textEquals = text.equals(previousText);
+      boolean textEqualsCase = text.equalsIgnoreCase(previousText);
+      previousText = text;
+      if (textEqualsCase) {
+        automatic = false;
+      }
+      if (tmpLinkNum > 0) {
+        String between = tmpBetweenList.get(tmpLinkNum - 1);
+        if (!between.isEmpty()) {
+          buffer.append(between);
+          if (between.startsWith("'") || between.endsWith("'")) {
+            automatic = false;
+          }
+        }
+      }
+      if (!textEquals && (!nowiki || otherChar || punctuation)) {
+        if (text.startsWith("'") &&
+            buffer.toString().endsWith("'")) {
+          automatic = false;
+        }
+        buffer.append(text);
+        if (nowiki) {
+          automatic = false;
+        }
+        if (punctuation && !otherChar) {
+          automatic = false;
+        } 
+      }
+    }
+    if (buffer.length() == 0) {
+      automatic = false;
+    }
+
+    // Report error
+    PageElementInternalLink lastLink = tmpLinks.get(tmpLinks.size() - 1);
+    int beginIndex = firstLink.getBeginIndex();
+    endIndex = lastLink.getEndIndex();
+    CheckErrorResult errorResult = createCheckErrorResult(
+        analysis, beginIndex, endIndex);
+
+    // Suggestion to group the links
+    errorResult.addReplacement(
+        InternalLinkBuilder.from(fullLink).withText(buffer.toString()).toString(),
+        automatic);
+
+    // Suggestion to keep only the text for the last link
+    boolean lastPunctuationOnly = false;
+    String lastText = lastLink.getDisplayedText();
+    if (lastText != null) {
+      lastPunctuationOnly = true;
+      for (int charIndex = 0; charIndex < lastText.length(); charIndex++) {
+        if (PUNCTUATIONS.indexOf(lastText.charAt(charIndex)) < 0) {
+          lastPunctuationOnly = false;
+        }
+      }
+    }
+    errorResult.addReplacement(
+        contents.substring(beginIndex, lastLink.getBeginIndex()) + lastText,
+        !automatic && lastPunctuationOnly && !fullLink.endsWith(lastText));
+
+    // Suggestion to keep only the text for the first link
+    errorResult.addReplacement(
+        firstLink.getDisplayedText() + contents.substring(firstLink.getEndIndex(), endIndex),
+        false);
+
+    // Suggestion to remove the last link entirely
+    errorResult.addReplacement(
+        contents.substring(beginIndex, lastLink.getBeginIndex()),
+        false);
+
+    // Suggestion to remove the first link entirely
+    errorResult.addReplacement(
+        contents.substring(firstLink.getEndIndex(), endIndex),
+        false);
+
+    errors.add(errorResult);
+    return true;
   }
 
   /**
