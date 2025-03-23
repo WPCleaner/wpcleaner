25ccedadef73a2cc2ce2b1bca6244ddea79dae83
Nicolas VERVELLE <nicolas.vervelle@quicksign.com>
2025-03-23 21:26:26 +0100

    CW#524: Fix identical parameters

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a52x/a524/CheckErrorAlgorithm524.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a52x/a524/CheckErrorAlgorithm524.java
@@ -8,9 +8,13 @@
 package org.wikipediacleaner.api.check.algorithm.a5xx.a52x.a524;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.wikipediacleaner.api.API;
 import org.wikipediacleaner.api.APIException;
@@ -20,7 +24,9 @@ import org.wikipediacleaner.api.algorithm.AlgorithmParameterElement;
 import org.wikipediacleaner.api.check.CheckErrorResult;
 import org.wikipediacleaner.api.check.CheckErrorResult.ErrorLevel;
 import org.wikipediacleaner.api.check.algorithm.CheckErrorAlgorithmBase;
+import org.wikipediacleaner.api.check.algorithm.a5xx.TemplateConfigurationGroup;
 import org.wikipediacleaner.api.configuration.WPCConfiguration;
+import org.wikipediacleaner.api.configuration.WPCConfigurationStringList;
 import org.wikipediacleaner.api.constants.EnumWikipedia;
 import org.wikipediacleaner.api.data.DataManager;
 import org.wikipediacleaner.api.data.Namespace;
@@ -78,7 +84,7 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
     for (PageElementTemplate template : templates) {
       boolean shouldCheck = true;
       int nbParam = template.getParameterCount();
-      if (shouldCheck && (nbParam < 1)) {
+      if (nbParam < 1) {
         shouldCheck = false;
       }
       if (shouldCheck) {
@@ -123,39 +129,42 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
               paramEnd = tmpIndex;
             }
 
+
             // Create error
             CheckErrorResult errorResult = createCheckErrorResult(
                 analysis, paramBegin, paramEnd);
             String existingValue = existingParam.param.getValue();
             String value = param.getValue();
+            boolean identical = identicalParameters.stream()
+                .filter(identicalParameter -> Page.areSameTitle(identicalParameter.templateName, template.getTemplateName()))
+                .filter(identicalParameter -> Objects.equals(identicalParameter.parameterName, paramName))
+                .filter(identicalParameter -> identicalParameter.values.contains(existingValue))
+                .anyMatch(identicalParameter -> identicalParameter.values.contains(value));
 
             // Display the value of the existing parameter
             if (value.equals(existingValue)) {
               errorResult.addText(GT._T("Both values are equal"));
             } else if (value.isEmpty()) {
               errorResult.addText(GT._T("Other value is empty"));
+            } else if (identical) {
+              errorResult.addText(GT._T("Both values are identical"));
             } else {
               errorResult.addText(GT._T("Other value: {0}", value));
             }
 
-            boolean automatic = true;
-            if (automatic) {
-              if (paramName.trim().length() == 0) {
-                automatic = false;
-              }
-            }
+            boolean automatic = !paramName.trim().isEmpty();
             if (automatic) {
               // Detect special cases: first parameter unnamed, second one explicitly named
               boolean special = false;
               String existingName = existingParam.param.getName();
-              if (((existingName == null) || (existingName.trim().length() == 0)) &&
+              if (((existingName == null) || existingName.trim().isEmpty()) &&
                   (param.getName() != null) &&
-                  (param.getName().trim().length() > 0)) {
+                  !param.getName().trim().isEmpty()) {
                 special = true;
                 // Avoid unnamed parameter in between
                 for (int numParam2 = existingParam.numParam + 1; numParam2 < numParam; numParam2++) {
                   String name2 = template.getParameter(numParam2).getName();
-                  if ((name2 == null) || (name2.trim().length() == 0)) {
+                  if ((name2 == null) || name2.trim().isEmpty()) {
                     special = false;
                   }
                 }
@@ -182,6 +191,7 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
                   for (int pos = 2; pos < ignoreElement.length; pos++) {
                     if (ignoreElement[pos].equals(existingValue)) {
                       ignored = true;
+                      break;
                     }
                   }
                 }
@@ -208,12 +218,14 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
               errorResult.addReplacement("", automatic);
             } else if (("".equals(existingValue))) {
               errorResult.addReplacement("", automatic);
+            } else if (identical) {
+              errorResult.addReplacement("", automatic);
             } else if (ignored) {
               errorResult.addReplacement("", automatic);
             }
 
             // Suggestions to comment the parameter
-            if ((existingValue != null) && (!"".equals(existingValue))) {
+            if ((existingValue != null) && !existingValue.isEmpty()) {
               boolean numericName = true;
               for (int i = 0; i < paramName.length(); i++) {
                 if (!Character.isDigit(paramName.charAt(i))) {
@@ -223,16 +235,16 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
               if (!numericName) {
                 StringBuilder replacement = new StringBuilder();
                 if (paramBegin < pipeBefore) {
-                  replacement.append(contents.substring(paramBegin, pipeBefore));
+                  replacement.append(contents, paramBegin, pipeBefore);
                 }
                 tmpIndex = paramEnd;
                 while ((tmpIndex > pipeBefore) &&
                        Character.isWhitespace(contents.charAt(tmpIndex - 1))) {
                   tmpIndex--;
                 }
-                replacement.append(CommentBuilder.from(contents.substring(pipeBefore, tmpIndex)).toString());
+                replacement.append(CommentBuilder.from(contents.substring(pipeBefore, tmpIndex)));
                 if (paramEnd > tmpIndex) {
-                  replacement.append(contents.substring(tmpIndex, paramEnd));
+                  replacement.append(contents, tmpIndex, paramEnd);
                 }
                 errorResult.addReplacement(replacement.toString(), GT._T("Comment"));
               }
@@ -287,8 +299,7 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
     if (categoryName != null) {
       return categoryName;
     }
-    if ((trackingCategory != null) &&
-        (trackingCategory.trim().length() > 0)) {
+    if ((trackingCategory != null) && !trackingCategory.trim().isEmpty()) {
       return trackingCategory;
     }
     return null;
@@ -336,13 +347,7 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
   /**
    * Bean for holding information about a parameter
    */
-  private static class ParameterInfo {
-    public final int numParam;
-    public final Parameter param;
-    public ParameterInfo(int numParam, Parameter param) {
-      this.numParam = numParam;
-      this.param = param;
-    }
+  private record ParameterInfo(int numParam, Parameter param) {
   }
 
   /* ====================================================================== */
@@ -354,6 +359,9 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
 
   /** Values that can be safely ignored */
   private static final String PARAMETER_IGNORE = "ignore";
+
+  /** Values that are to be considered identical */
+  private static final String PARAMETER_IDENTICAL = "identical";
 
   /**
    * Initialize settings for the algorithm.
@@ -370,10 +378,30 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
         ignore.addAll(tmpList);
       }
     }
+    tmp = getSpecificProperty(PARAMETER_IDENTICAL, true, true, false);
+    identicalParameters.clear();
+    if (tmp != null) {
+      TemplateConfigurationGroup group = new TemplateConfigurationGroup();
+      List<String[]> generalList = getWPCConfiguration().getStringArrayList(WPCConfigurationStringList.TEMPLATE_GROUPS);
+      if (generalList != null) {
+        group.addGroups(generalList);
+      }
+      List<String[]> tmpList = WPCConfiguration.convertPropertyToStringArrayList(tmp);
+      if (tmpList != null) {
+        for (String[] params : tmpList) {
+          if (params.length > 3) {
+            for (String templateName : group.getTemplateNames(params[0])) {
+              for (String paramName : params[1].split(",")) {
+                identicalParameters.add(new IdenticalParameters(templateName, paramName, Arrays.stream(params).skip(2).collect(Collectors.toSet())));
+              }
+            }
+          }
+        }
+      }
+    }
     tmp = getSpecificProperty(PARAMETER_CATEGORY, true, true, false);
     categoryName = null;
-    if ((tmp != null) &&
-        (tmp.trim().length() > 0)) {
+    if ((tmp != null) && !tmp.trim().isEmpty()) {
       categoryName = tmp.trim();
     }
   }
@@ -383,6 +411,11 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
 
   /** Values that can be safely ignored */
   private final List<String[]> ignore = new ArrayList<>();
+
+  private record IdenticalParameters(String templateName, String parameterName, Set<String> values) {}
+
+  /** Values that are to be considered identical */
+  private final List<IdenticalParameters> identicalParameters = new ArrayList<>();
 
   /**
    * Build the list of parameters for this algorithm.
@@ -397,6 +430,27 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
             "category name",
             GT._T("A category containing the list of pages in error"))));
     addParameter(new AlgorithmParameter(
+        PARAMETER_IDENTICAL,
+        GT._T("Values that are to be considered identical"),
+        new AlgorithmParameterElement[] {
+            new AlgorithmParameterElement(
+                "template names",
+                GT._T("Comma separated list of template names")
+            ),
+            new AlgorithmParameterElement(
+                "parameter names",
+                GT._T("Comma separated list of parameter names")
+            ),
+            new AlgorithmParameterElement(
+                "parameter value",
+                GT._T("Parameter value"),
+                false,
+                true
+            )
+        },
+        true
+    ));
+    addParameter(new AlgorithmParameter(
         PARAMETER_IGNORE,
         GT._T("Values that can be safely ignored for a given template and argument"),
         new AlgorithmParameterElement[] {
@@ -408,7 +462,7 @@ public class CheckErrorAlgorithm524 extends CheckErrorAlgorithmBase {
                 GT._T("Name of the parameter for which some values can be safely ignored")),
             new AlgorithmParameterElement(
                 "value",
-                GT._T("Value of the paramter which can be safely ignored"),
+                GT._T("Value of the parameter which can be safely ignored"),
                 false,
                 true),
         },
