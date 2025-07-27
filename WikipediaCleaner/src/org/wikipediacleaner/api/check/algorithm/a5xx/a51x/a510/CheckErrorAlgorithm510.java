034b3dbe97ff18ec9ac38f5bd3be6fb762acb50a
Nicolas VERVELLE <nicolas.vervelle@quicksign.com>
2025-07-27 15:48:20 +0200

    CW#510: Refactor and handle gallery tags

--- WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a51x/a510/CheckErrorAlgorithm510.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/check/algorithm/a5xx/a51x/a510/CheckErrorAlgorithm510.java
@@ -20,7 +20,7 @@ import org.wikipediacleaner.api.data.contents.tag.WikiTagType;
 
 /**
  * Algorithm for analyzing error 510 of check wikipedia project.
- * Error 510: Non working pipe trick
+ * Error 510: Non-working pipe trick
  */
 public class CheckErrorAlgorithm510 extends CheckErrorAlgorithmBase {
 
@@ -48,104 +48,113 @@ public class CheckErrorAlgorithm510 extends CheckErrorAlgorithmBase {
     boolean result = false;
     List<PageElementInternalLink> links = analysis.getInternalLinks();
     for (PageElementInternalLink link : links) {
-      if ((link.getText() != null) &&
-          (link.getText().length() == 0) &&
-          (link.getFullLink() != null)) {
-        boolean errorFound = false;
-        String target = link.getFullLink().trim();
-        if (analysis.getSurroundingTag(WikiTagType.REF, link.getBeginIndex()) != null) {
+      result |= analyzeInternalLink(analysis, errors, link);
+    }
+    return result;
+  }
 
-          // Check for namespace at the beginning
-          int beginIndex = 0;
-          if (target.length() > 1) {
-            int tmpIndex = target.indexOf(':');
-            if (tmpIndex > 0) {
-              beginIndex = tmpIndex + 1;
-            }
-          }
+  private boolean analyzeInternalLink(
+      PageAnalysis analysis,
+      Collection<CheckErrorResult> errors,
+      PageElementInternalLink link) {
+    if ((link.getText() == null) ||
+        (link.getText().isEmpty()) ||
+        (link.getFullLink() != null)) {
+      return false;
+    }
 
-          // Check for parenthesis or commas (remove the end part)
-          int endIndex = target.length();
-          if (endIndex > 0) {
-            if (target.charAt(endIndex - 1) == ')') {
-              int tmpIndex = target.lastIndexOf('(');
-              if (tmpIndex > 0) {
-                endIndex = tmpIndex;
-              }
-            }
-            if (endIndex == target.length()) {
-              int tmpIndex = target.indexOf(',');
-              if (tmpIndex > 0) {
-                endIndex = tmpIndex;
-              }
-            }
-          }
+    if ((analysis.getSurroundingTag(WikiTagType.GALLERY, link.getBeginIndex()) == null) ||
+        (analysis.getSurroundingTag(WikiTagType.REF, link.getBeginIndex()) == null)) {
+      return false;
+    }
 
-          // Report error
-          if ((beginIndex > 0) || (endIndex < target.length())) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            errorFound = true;
-            if (beginIndex >= endIndex) {
-              beginIndex = 0;
-            }
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, link.getBeginIndex(), link.getEndIndex());
-            String replacement = InternalLinkBuilder
-                .from(target)
-                .withText(target.substring(beginIndex, endIndex))
-                .toString();
-            errorResult.addReplacement(replacement);
-            replacement = InternalLinkBuilder.from(target).toString();
-            errorResult.addReplacement(replacement);
-            errors.add(errorResult);
+    String target = link.getFullLink().trim();
+    if ((analysis.getSurroundingTag(WikiTagType.GALLERY, link.getBeginIndex()) != null) ||
+        (analysis.getSurroundingTag(WikiTagType.REF, link.getBeginIndex()) != null)) {
+
+      // Check for namespace at the beginning
+      int beginIndex = 0;
+      if (target.length() > 1) {
+        int tmpIndex = target.indexOf(':');
+        if (tmpIndex > 0) {
+          beginIndex = tmpIndex + 1;
+        }
+      }
+
+      // Check for parenthesis or commas (remove the end part)
+      int endIndex = target.length();
+      if (endIndex > 0) {
+        if (target.charAt(endIndex - 1) == ')') {
+          int tmpIndex = target.lastIndexOf('(');
+          if (tmpIndex > 0) {
+            endIndex = tmpIndex;
           }
         }
-
-        // Incorrect slash trick
-        if (!errorFound) {
-          int endIndex = target.length();
-          if ((endIndex > 1) && (target.charAt(0) == '/')) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            errorFound = true;
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, link.getBeginIndex(), link.getEndIndex());
-            String replacement = InternalLinkBuilder.from(target + "/").toString();
-            errorResult.addReplacement(replacement);
-            replacement = InternalLinkBuilder.from(target).toString();
-            errorResult.addReplacement(replacement);
-            errors.add(errorResult);
-          }
-        }
-
-        // Link to section
-        if (!errorFound) {
-          int index = target.indexOf('#');
-          if ((index >= 0) && (index < target.length() - 1)) {
-            if (errors == null) {
-              return true;
-            }
-            result = true;
-            errorFound = true;
-            CheckErrorResult errorResult = createCheckErrorResult(
-                analysis, link.getBeginIndex(), link.getEndIndex());
-            String replacement = InternalLinkBuilder
-                .from(target)
-                .withText(target.substring(index + 1))
-                .toString();
-            errorResult.addReplacement(replacement);
-            replacement = InternalLinkBuilder.from(target).toString();
-            errorResult.addReplacement(replacement);
-            errors.add(errorResult);
+        if (endIndex == target.length()) {
+          int tmpIndex = target.indexOf(',');
+          if (tmpIndex > 0) {
+            endIndex = tmpIndex;
           }
         }
       }
+
+      // Report error
+      if ((beginIndex > 0) || (endIndex < target.length())) {
+        if (errors == null) {
+          return true;
+        }
+        if (beginIndex >= endIndex) {
+          beginIndex = 0;
+        }
+        CheckErrorResult errorResult = createCheckErrorResult(
+            analysis, link.getBeginIndex(), link.getEndIndex());
+        String replacement = InternalLinkBuilder
+            .from(target)
+            .withText(target.substring(beginIndex, endIndex))
+            .toString();
+        errorResult.addReplacement(replacement);
+        replacement = InternalLinkBuilder.from(target).toString();
+        errorResult.addReplacement(replacement);
+        errors.add(errorResult);
+        return true;
+      }
     }
-    return result;
+
+    // Incorrect slash trick
+    int endIndex = target.length();
+    if ((endIndex > 1) && (target.charAt(0) == '/')) {
+      if (errors == null) {
+        return true;
+      }
+      CheckErrorResult errorResult = createCheckErrorResult(
+          analysis, link.getBeginIndex(), link.getEndIndex());
+      String replacement = InternalLinkBuilder.from(target + "/").toString();
+      errorResult.addReplacement(replacement);
+      replacement = InternalLinkBuilder.from(target).toString();
+      errorResult.addReplacement(replacement);
+      errors.add(errorResult);
+      return true;
+    }
+
+    // Link to section
+    int index = target.indexOf('#');
+    if ((index >= 0) && (index < target.length() - 1)) {
+      if (errors == null) {
+        return true;
+      }
+      CheckErrorResult errorResult = createCheckErrorResult(
+          analysis, link.getBeginIndex(), link.getEndIndex());
+      String replacement = InternalLinkBuilder
+          .from(target)
+          .withText(target.substring(index + 1))
+          .toString();
+      errorResult.addReplacement(replacement);
+      replacement = InternalLinkBuilder.from(target).toString();
+      errorResult.addReplacement(replacement);
+      errors.add(errorResult);
+      return true;
+    }
+
+    return false;
   }
 }
