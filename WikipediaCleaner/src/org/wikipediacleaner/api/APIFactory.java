51e7303bf75c4d7f3a16e4210a77916d193103a7
Nicolas VERVELLE <nicolas.vervelle@quicksign.com>
2025-08-09 19:51:34 +0200

    Add version to user agent

--- WikipediaCleaner/src/org/wikipediacleaner/api/APIFactory.java
+++ WikipediaCleaner/src/org/wikipediacleaner/api/APIFactory.java
@@ -11,6 +11,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpConnectionManager;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.wikipediacleaner.Version;
 import org.wikipediacleaner.api.check.CheckWiki;
 import org.wikipediacleaner.api.http.HttpServer;
 import org.wikipediacleaner.api.http.hc3.Hc3HttpServer;
@@ -23,13 +24,13 @@ import org.wikipediacleaner.api.impl.MediaWikiAPI;
 public class APIFactory {
 
   /** MediaWiki API */
-  private static API api;
+  private static final API api;
 
   /** MediaWiki REST API */
-  private static MediaWikiRESTAPI restApi;
+  private static final MediaWikiRESTAPI restApi;
 
   /** Check Wiki project */
-  private static CheckWiki checkWiki;
+  private static final CheckWiki checkWiki;
 
   // Initialize static members
   static {
@@ -84,17 +85,15 @@ public class APIFactory {
   /**
    * Create an HTTP connection.
    * 
-   * @return A HTTP connection.
+   * @return An HTTP connection.
    */
   private static HttpClient createHttpClient(HttpConnectionManager manager) {
     HttpClient client = new HttpClient(manager);
-    client.getParams().setParameter(
-        HttpMethodParams.USER_AGENT,
-        "WPCleaner (+http://en.wikipedia.org/wiki/User:NicoV/Wikipedia_Cleaner/Documentation)");
+    client.getParams().setParameter(HttpMethodParams.USER_AGENT, userAgent());
     return client;
   }
 
-  /**
+  /*
    * Create an HTTP client.
    * 
    * @param manager Connection manager.
@@ -103,7 +102,11 @@ public class APIFactory {
   /*private static CloseableHttpClient createHttpClient(HttpClientConnectionManager manager) {
     return HttpClients.custom().
         setConnectionManager(manager).
-        setUserAgent("WPCleaner (+http://en.wikipedia.org/wiki/User:NicoV/Wikipedia_Cleaner/Documentation)").
+        setUserAgent(userAgent()).
         build();
   }*/
+
+  private static String userAgent() {
+    return "%s %s (+%s)".formatted(Version.PROGRAM, Version.VERSION, "https://en.wikipedia.org/wiki/User:NicoV/Wikipedia_Cleaner/Documentation");
+  }
 }
